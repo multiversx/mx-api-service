8b951ffa905542e9c29cdf6cc970ee629f02fe3e
Traian Anghel <tanghel@live.com>
2021-05-30 12:41:46 +0300

    keep list of pending requests to re-use long-lasting pending connections

--- src/interceptors/caching.interceptor.ts
+++ src/interceptors/caching.interceptor.ts
@@ -1,11 +1,13 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
 import { HttpAdapterHost } from "@nestjs/core";
-import { Observable, of } from "rxjs";
-import { tap } from 'rxjs/operators';
+import { Observable, of, throwError } from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
 import { CachingService } from "src/helpers/caching.service";
 
 @Injectable()
 export class CachingInterceptor implements NestInterceptor {
+  private pendingRequestsDictionary: { [ key: string]: any; } = {};
+
   constructor(
     private readonly cachingService: CachingService,
     private readonly httpAdapterHost: HttpAdapterHost,
@@ -14,19 +16,44 @@ export class CachingInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
     let cacheKey = this.getCacheKey(context);
     if (cacheKey) {
+      let pendingRequest = this.pendingRequestsDictionary[cacheKey];
+      if (pendingRequest) {
+        let result = await pendingRequest;
+        return of(result);
+      }
+
       let cachedValue = await this.cachingService.getCacheLocal(cacheKey);
       if (cachedValue) {
         return of(cachedValue);
       }
 
+      let pendingRequestResolver: (value: any) => null;
+      let pendingRequestReject: (value: any) => null;
+      this.pendingRequestsDictionary[cacheKey] = new Promise((resolve, reject) => {
+        // @ts-ignore
+        pendingRequestResolver = resolve;
+
+        // @ts-ignore
+        pendingRequestReject = reject;
+      });
+
       return next
         .handle()
         .pipe(
           tap(async (result) => {
+            delete this.pendingRequestsDictionary[cacheKey ?? ''];
+            pendingRequestResolver(result);
+
             let ttl = await this.cachingService.getSecondsRemainingUntilNextRound();
 
             await this.cachingService.setCacheLocal(cacheKey!!, result, ttl);
           }),
+          catchError(err => {
+            delete this.pendingRequestsDictionary[cacheKey ?? ''];
+            pendingRequestReject(err);
+
+            return throwError(err);
+          })
         );
     }
 
