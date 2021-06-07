f97bc9c53dfb78331e21f44ba0fc891f710155cf
Traian Anghel <tanghel@live.com>
2021-06-07 22:01:12 +0300

    fixed issue regarding blocks info in previous epochs when filtering by validator/proposer, and also provide extra relevant info in block details

--- src/endpoints/blocks/block.service.ts
+++ src/endpoints/blocks/block.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ElasticPagination } from "src/helpers/entities/elastic.pagination";
 import { ElasticService } from "src/helpers/elastic.service";
-import { mergeObjects, oneMinute } from "src/helpers/helpers";
+import { mergeObjects, oneMinute, oneWeek } from "src/helpers/helpers";
 import { Block } from "./entities/block";
 import { BlockDetailed } from "./entities/block.detailed";
 import { CachingService } from "src/helpers/caching.service";
@@ -22,8 +22,44 @@ export class BlockService {
   }
 
   async getBlocks(shard: number | undefined, proposer: string | undefined, validator: string | undefined, epoch: number | undefined, from: number, size: number): Promise<Block[]> {
+    let totalBlocks: Block[] = [];
+
+    let totalIterations = 0;
+
+    // We get valid proposers only within one epoch
+    // meaning we need to fetch epoch by epoch until we reached the total size of the result set
+    while (true) {
+      let remainingSize = size - totalBlocks.length;
+
+      // we reached total blocks => out
+      if (remainingSize <= 0) {
+        return totalBlocks;
+      }
+
+      let blocksForEpoch = await this.getBlocksForEpoch(shard, proposer, validator, epoch, from, size);
+      
+      totalBlocks.push(...blocksForEpoch);
+
+      // no epoch filter, no reason to decrement it
+      if (!epoch) {
+        return totalBlocks;
+      }
+
+      epoch--;
+
+      totalIterations++;
+
+      // general safety measure (max 10 requests towards elastic)
+      if (totalIterations >= 10) {
+        return totalBlocks;
+      }
+    }
+  }
+
+  async getBlocksForEpoch(shard: number | undefined, proposer: string | undefined, validator: string | undefined, epoch: number | undefined, from: number, size: number): Promise<Block[]> {
     let query: any = {
       shardId: shard,
+      epoch: epoch
     };
 
     if (proposer && shard && epoch) {
@@ -51,8 +87,34 @@ export class BlockService {
       item.shard = item.shardId;
     }
 
-    return result.map(item => mergeObjects(new Block(), item));
+    let finalResult = [];
+
+    for (let item of result) {
+      let transformedItem = await this.transformItem(item);
+
+      finalResult.push(transformedItem);
+    }
+
+    return finalResult.map(item => mergeObjects(new Block(), item));
   }
+
+  async transformItem(item: any) {
+    // eslint-disable-next-line no-unused-vars
+    let { shardId: shard, epoch, proposer, validators, searchOrder, ...rest } = item;
+
+    let key = `${shard}_${epoch}`;
+    let blses: any = await this.cachingService.getCacheLocal(key);
+    if (!blses) {
+      blses = await this.elasticService.getBlses(shard, epoch);
+
+      await this.cachingService.setCacheLocal(key, blses, oneWeek());
+    }
+  
+    proposer = blses[proposer];
+    validators = validators.map((index: number) => blses[index]);
+  
+    return { shard, epoch, proposer, validators, ...rest };
+  };
 
   async getBlock(hash: string): Promise<BlockDetailed> {
     let result = await this.elasticService.getItem('blocks', 'hash', hash);
