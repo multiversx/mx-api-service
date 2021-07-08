99026a6d7957d0732e64fee8342cb8ac44138a14
Traian Anghel <tanghel@live.com>
2021-07-08 15:03:01 +0300

    changed indentation for data api service

--- src/helpers/data.api.service.ts
+++ src/helpers/data.api.service.ts
@@ -5,52 +5,51 @@ import { ApiService } from "./api.service";
 
 @Injectable()
 export class DataApiService {
-    private readonly quotesHistoricalUrl: string;
-    private readonly stakingHistoricalUrl: string;
-    private readonly stakingUsersHistoricalUrl: string;
-    private readonly transactionsHistoricalUrl: string;
-    private readonly accountsHistoricalUrl: string
+  private readonly quotesHistoricalUrl: string;
+  private readonly stakingHistoricalUrl: string;
+  private readonly stakingUsersHistoricalUrl: string;
+  private readonly transactionsHistoricalUrl: string;
+  private readonly accountsHistoricalUrl: string
 
-    constructor(
-        private readonly apiConfigService: ApiConfigService,
-        private readonly apiService: ApiService,
-    ) {
-        this.quotesHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/quoteshistorical/egld`;
-        this.stakingHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/stakinghistorical/total`;
-        this.stakingUsersHistoricalUrl = `${this.apiConfigService.getDataLatestUrl()}/stakinghistorical/total`;
-        this.transactionsHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/transactionshistorical/transactions`;
-        this.accountsHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/accountshistorical/accounts`;
-    };
-    
-    async getQuotesHistorical(quoteUrl: string): Promise<Data[]> {
-        const { data } = await this.apiService.get(`${this.quotesHistoricalUrl}/${quoteUrl}`);
+  constructor(
+    private readonly apiConfigService: ApiConfigService,
+    private readonly apiService: ApiService,
+  ) {
+    this.quotesHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/quoteshistorical/egld`;
+    this.stakingHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/stakinghistorical/total`;
+    this.stakingUsersHistoricalUrl = `${this.apiConfigService.getDataLatestUrl()}/stakinghistorical/total`;
+    this.transactionsHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/transactionshistorical/transactions`;
+    this.accountsHistoricalUrl = `${this.apiConfigService.getDataLatestCompleteUrl()}/accountshistorical/accounts`;
+  };
+  
+  async getQuotesHistorical(quoteUrl: string): Promise<Data[]> {
+    const { data } = await this.apiService.get(`${this.quotesHistoricalUrl}/${quoteUrl}`);
 
-        return data;
-    }
+    return data;
+  }
 
-    async getStakingHistorical(stakeUrl: string): Promise<Data[]> {
-        const { data } = await this.apiService.get(`${this.stakingHistoricalUrl}/${stakeUrl}`);
+  async getStakingHistorical(stakeUrl: string): Promise<Data[]> {
+    const { data } = await this.apiService.get(`${this.stakingHistoricalUrl}/${stakeUrl}`);
 
-        return data;
-    }
+    return data;
+  }
 
-    async getStakingUsersHistorical(stakeUrl: string): Promise<number> {
-        const { data } = await this.apiService.get(`${this.stakingUsersHistoricalUrl}/${stakeUrl}`);
+  async getStakingUsersHistorical(stakeUrl: string): Promise<number> {
+    const { data } = await this.apiService.get(`${this.stakingUsersHistoricalUrl}/${stakeUrl}`);
 
-        return data;
-    }
+    return data;
+  }
 
-    async getTransactionsHistorical(transactionsUrl: string): Promise<Data[]> {
-        const { data } = await this.apiService.get(`${this.transactionsHistoricalUrl}/${transactionsUrl}`);
+  async getTransactionsHistorical(transactionsUrl: string): Promise<Data[]> {
+    const { data } = await this.apiService.get(`${this.transactionsHistoricalUrl}/${transactionsUrl}`);
 
-        return data;
-    }
+    return data;
+  }
 
 
-    async getAccountsHistorical(accountsUrl: string): Promise<Data[]> {
-        const { data } = await this.apiService.get(`${this.accountsHistoricalUrl}/${accountsUrl}`);
+  async getAccountsHistorical(accountsUrl: string): Promise<Data[]> {
+    const { data } = await this.apiService.get(`${this.accountsHistoricalUrl}/${accountsUrl}`);
 
-        return data;
-    }
-
+    return data;
+  }
 }
