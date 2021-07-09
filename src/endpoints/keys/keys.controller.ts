57ecd149879781460c784ccf9254faf9716380c6
Traian Anghel <tanghel@live.com>
2021-07-09 14:13:12 +0300

    endpoint for getting key unbond period (#14)

---
+++ src/endpoints/keys/keys.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, HttpException, HttpStatus, Param } from "@nestjs/common";
+import { ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { KeyUnbondPeriod } from "./entities/key.unbond.period";
+import { KeysService } from "./keys.service";
+
+@Controller()
+@ApiTags('keys')
+export class KeysController {
+	constructor(private readonly keysService: KeysService) {}
+
+	@Get("/keys/:key/unbond-period")
+	@ApiResponse({
+		status: 200,
+		description: 'The unbonding period remaining for the given BLS key',
+		type: KeyUnbondPeriod
+	})
+	@ApiQuery({ name: 'key', description: 'The BLS key of the node', required: true })
+	async getKeyUnbondPeriod(
+		@Param('key') key: string
+	): Promise<KeyUnbondPeriod> {
+    let result = await this.keysService.getKeyUnbondPeriod(key);
+    if (!result) {
+      throw new HttpException('Key not found', HttpStatus.NOT_FOUND);
+    }
+
+    return result;
+	}
+}
