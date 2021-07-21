8b28db989327598e8584f36107ab4dffe669c317
Traian Anghel <tanghel@live.com>
2021-07-21 17:07:22 +0300

    initial implementation

--- src/cache.warmer.module.ts
+++ src/cache.warmer.module.ts
@@ -24,7 +24,12 @@ import { PublicAppModule } from './public.app.module';
         let clientOptions: ClientOptions = {
           transport: Transport.REDIS,
           options: {
-            url: `redis://${apiConfigService.getRedisUrl()}:6379`
+            url: `redis://${apiConfigService.getRedisUrl()}:6379`,
+            retryDelay: 1000,
+            retryAttempts: 10,
+            retry_strategy: function(_: any) {
+              return 1000;
+            },
           }
         };
 
