5fa97e4838c70d64900c3227d3106b13fec56444
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-07-27 15:22:09 +0300

    modify wildcard query structure

--- src/helpers/entities/elastic/wildcard.query.ts
+++ src/helpers/entities/elastic/wildcard.query.ts
@@ -1,20 +1,7 @@
 import { AbstractQuery } from "./abstract.query";
-import { QueryOperator } from "./query.operator";
-
 export class WildcardQuery extends AbstractQuery {
 
-  buildQuery(key: string, value: any, operator: QueryOperator | undefined): any {
-    if (!operator) {
-      return { wildcard: { [key]: value } };
-    }
-
-    return {
-      wildcard: {
-        [key]: {
-        query: value,
-        operator: operator
-        }
-      }
-    }
+  buildQuery(key: string, value: any): any {
+    return { wildcard: { [key]: value } };
   }
 }
