3a38f70dad1eca0c494af82a0818912ebe2535a3
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-02 10:14:30 +0300

    add epoch filter on for rounds

--- src/endpoints/rounds/round.service.ts
+++ src/endpoints/rounds/round.service.ts
@@ -27,6 +27,11 @@ export class RoundService {
       const shardIdQuery = QueryType.Match('shardId', filter.shard);
       queries.push(shardIdQuery);
     }
+
+    if (filter.epoch !== undefined) {
+      const epochQuery = QueryType.Match('epoch', filter.epoch);
+      queries.push(epochQuery);
+    }
     
     if (filter.validator !== undefined && filter.shard !== undefined && filter.epoch !== undefined) {
       const index = await this.blsService.getBlsIndex(filter.validator, filter.shard, filter.epoch);
