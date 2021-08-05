6e5b496f7103afae39575f6f0ab7bdfb0a1245e9
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-05 17:08:16 +0300

    lock cache warmer invalidations

--- src/crons/cache.warmer.service.ts
+++ src/crons/cache.warmer.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, Logger } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { Cron } from "@nestjs/schedule";
 import { IdentitiesService } from "src/endpoints/identities/identities.service";
@@ -6,18 +6,11 @@ import { NodeService } from "src/endpoints/nodes/node.service";
 import { ProviderService } from "src/endpoints/providers/provider.service";
 import { TokenService } from "src/endpoints/tokens/token.service";
 import { CachingService } from "src/helpers/caching.service";
-import { oneHour, oneMinute } from "src/helpers/helpers";
+import { lock, oneHour, oneMinute } from "src/helpers/helpers";
 import { KeybaseService } from "src/helpers/keybase.service";
-import { PerformanceProfiler } from "src/helpers/performance.profiler";
 
 @Injectable()
 export class CacheWarmerService {
-  isRunningNodeInvalidations: boolean = false;
-  isRunningTokenInvalidations: boolean = false;
-  isRunningIdentitiesInvalidations: boolean = false;
-  isRunningProviderInvalidations: boolean = false;
-  isRunningKeybaseInvalidations: boolean = false;
-  private readonly logger: Logger
 
   constructor(
     private readonly nodeService: NodeService,
@@ -27,103 +20,47 @@ export class CacheWarmerService {
     private readonly providerService: ProviderService,
     private readonly keybaseService: KeybaseService,
     @Inject('PUBSUB_SERVICE') private client: ClientProxy,
-  ) {
-    this.logger = new Logger(CacheWarmerService.name);
-  }
+  ) { }
 
   @Cron('* * * * *')
   async handleNodeInvalidations() {
-    if (this.isRunningNodeInvalidations) {
-      return;
-    }
-
-    this.isRunningNodeInvalidations = true;
-    let profiler = new PerformanceProfiler();
-    try {
+    await lock('Nodes invalidations', async () => {
       let nodes = await this.nodeService.getAllNodesRaw();
       await this.cachingService.setCache('nodes', nodes, oneHour());
       await this.deleteCacheKey('nodes');
-    } catch(error) {
-      this.logger.error('Error running node invalidations');
-      this.logger.error(error);
-    } finally {
-      profiler.stop('Running node invalidations', true);
-      this.isRunningNodeInvalidations = false;
-    }
+    }, true);
   }
 
   @Cron('* * * * *')
   async handleTokenInvalidations() {
-    if (this.isRunningTokenInvalidations) {
-      return;
-    }
-
-    this.isRunningTokenInvalidations = true;
-    let profiler = new PerformanceProfiler();
-    try {
+    await lock('Tokens invalidations', async () => {
       let tokens = await this.tokenService.getAllTokensRaw();
       await this.cachingService.setCache('allTokens', tokens, oneHour());
       await this.deleteCacheKey('allTokens');
-    } catch(error) {
-      this.logger.error('Error running token invalidations');
-      this.logger.error(error);
-    } finally {
-      profiler.stop('Running token invalidations', true);
-      this.isRunningTokenInvalidations = false;
-    }
+    }, true);
   }
 
   @Cron('*/7 * * * *')
   async handleIdentityInvalidations() {
-    if (this.isRunningIdentitiesInvalidations) {
-      return;
-    }
-
-    this.isRunningIdentitiesInvalidations = true;
-    let profiler = new PerformanceProfiler();
-    try {
+    await lock('Identities invalidations', async () => {
       let identities = await this.identitiesService.getAllIdentitiesRaw();
       await this.cachingService.setCache('identities', identities, oneMinute() * 15);
       await this.deleteCacheKey('identities');
-    } catch(error) {
-      this.logger.error('Error running identities invalidations');
-      this.logger.error(error);
-    } finally {
-      profiler.stop('Running identities invalidations', true);
-      this.isRunningIdentitiesInvalidations = false;
-    }
+    }, true);
   }
 
   @Cron('*/30 * * * *')
   async handleProviderInvalidations() {
-    if (this.isRunningProviderInvalidations) {
-      return;
-    }
-
-    this.isRunningProviderInvalidations = true;
-    let profiler = new PerformanceProfiler();
-    try {
+    await lock('Providers invalidations', async () => {
       let providers = await this.providerService.getAllProvidersRaw();
       await this.cachingService.setCache('providers', providers, oneHour());
       await this.deleteCacheKey('providers');
-    } catch(error) {
-      this.logger.error('Error running provider invalidations');
-      this.logger.error(error);
-    } finally {
-      profiler.stop('Running provider invalidations', true);
-      this.isRunningProviderInvalidations = false;
-    }
+    }, true);
   }
 
   @Cron('*/30 * * * *')
   async handleKeybaseInvalidations() {
-    if (this.isRunningKeybaseInvalidations) {
-      return;
-    }
-
-    this.isRunningKeybaseInvalidations = true;
-    let profiler = new PerformanceProfiler();
-    try {
+    await lock('Keybase invalidations', async () => {
       let nodeKeybases = await this.keybaseService.confirmKeybaseNodesAgainstKeybasePub();
       await this.cachingService.setCache('nodeKeybases', nodeKeybases, oneHour());
       await this.deleteCacheKey('nodeKeybases');
@@ -131,13 +68,7 @@ export class CacheWarmerService {
       let providerKeybases = await this.keybaseService.confirmKeybaseProvidersAgainstKeybasePub();
       await this.cachingService.setCache('providerKeybases', providerKeybases, oneHour());
       await this.deleteCacheKey('providerKeybases');
-    } catch(error) {
-      this.logger.error('Error running keybase invalidations');
-      this.logger.error(error);
-    } finally {
-      profiler.stop('Running keybase invalidations', true);
-      this.isRunningKeybaseInvalidations = false;
-    }
+    }, true);
   }
 
   private async deleteCacheKey(key: string) {
