be9eb0541aaa5a42becca30c24e0aa111fb49abd
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-06 09:22:38 +0300

    set node identity against keybase

--- src/endpoints/nodes/node.service.ts
+++ src/endpoints/nodes/node.service.ts
@@ -222,10 +222,6 @@ export class NodeService {
         if (provider) {
           node.provider = provider.provider;
           node.owner = provider.owner ?? '';
-
-          if (provider.identity) {
-            node.identity = provider.identity;
-          }
         }
       }
     });
