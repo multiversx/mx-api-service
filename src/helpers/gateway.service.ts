8de4fcaab546eee54f2fc073a4eb877003093788
Traian Anghel <tanghel@live.com>
2021-08-11 13:48:31 +0300

    monitor all api calls in a more unified manner

--- src/helpers/gateway.service.ts
+++ src/helpers/gateway.service.ts
@@ -1,15 +1,12 @@
 import { forwardRef, Inject, Injectable } from "@nestjs/common";
-import { MetricsService } from "src/endpoints/metrics/metrics.service";
 import { ApiConfigService } from "./api.config.service";
 import { ApiService } from "./api.service";
-import { PerformanceProfiler } from "./performance.profiler";
 
 @Injectable()
 export class GatewayService {
   constructor(
     private readonly apiConfigService: ApiConfigService,
-    @Inject(forwardRef(() => MetricsService))
-    private readonly metricsService: MetricsService,
+    @Inject(forwardRef(() => ApiService))
     private readonly apiService: ApiService
   ) {}
 
@@ -19,13 +16,7 @@ export class GatewayService {
   }
 
   async getRaw(url: string): Promise<any> {
-    let profiler = new PerformanceProfiler();
-    let result = await this.apiService.get(`${this.apiConfigService.getGatewayUrl()}/${url}`);
-    profiler.stop();
-
-    this.metricsService.setExternalCall('gateway', profiler.duration);
-
-    return result;
+    return await this.apiService.get(`${this.apiConfigService.getGatewayUrl()}/${url}`);
   }
 
   async create(url: string, data: any): Promise<any> {
@@ -34,12 +25,6 @@ export class GatewayService {
   }
 
   async createRaw(url: string, data: any): Promise<any> {
-    let profiler = new PerformanceProfiler();
-    let result = await this.apiService.post(`${this.apiConfigService.getGatewayUrl()}/${url}`, data);
-    profiler.stop();
-
-    this.metricsService.setExternalCall('gateway', profiler.duration);
-
-    return result;
+    return await this.apiService.post(`${this.apiConfigService.getGatewayUrl()}/${url}`, data);
   }
 }
