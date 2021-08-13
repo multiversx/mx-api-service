d603a9e78e7e95ee04108c6dab6e5b95eed6521d
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-13 15:11:50 +0300

    var => let

--- src/utils/binary.utils.ts
+++ src/utils/binary.utils.ts
@@ -12,8 +12,8 @@ export class BinaryUtils {
   }
   
   static hexToString(hex: string): string {
-    var str = '';
-    for (var n = 0; n < hex.length; n += 2) {
+    let str = '';
+    for (let n = 0; n < hex.length; n += 2) {
       str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
     }
     
