73803453becf357943454c09c26f442118576db3
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-13 13:50:37 +0300

    add file utils

--- src/helpers/helpers.ts
+++
@@ -1,7 +0,0 @@
-const { readdirSync } = require('fs');
-
-export function getDirectories(source: string) {
-  return readdirSync(source, { withFileTypes: true })
-    .filter((dirent: any) => dirent.isDirectory())
-    .map((dirent: any) => dirent.name);
-}
