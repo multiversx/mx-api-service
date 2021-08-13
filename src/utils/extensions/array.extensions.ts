0cc18fc554453fbac19e2f93953fbf08a5253f79
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-13 13:48:39 +0300

    add array extensions

---
+++ src/utils/extensions/array.extensions.ts
@@ -0,0 +1,58 @@
+declare interface Array<T> {
+    groupBy(predicate: (item: T) => any): any;
+    selectMany(predicate: (item: T) => T[]): T[];
+    firstOrUndefined(predicate: (item: T) => boolean): T | undefined;
+    zip<TSecond, TResult>(second: TSecond[], predicate: (first: T, second: TSecond) => TResult): TResult[];
+    remove(element: T): number;
+}
+
+Array.prototype.groupBy = function(predicate: Function, asArray = false) {
+  let result = this.reduce(function(rv, x) {
+      (rv[predicate(x)] = rv[predicate(x)] || []).push(x);
+      return rv;
+  }, {});
+
+  if (asArray === true) {
+      result = Object.keys(result).map(key => {
+          return {
+              key: key,
+              values: result[key]
+          };
+      });
+  }
+
+  return result;
+};
+
+Array.prototype.selectMany = function(predicate: Function) {
+  let result = [];
+
+  for (let item of this) {
+      result.push(...predicate(item));
+  }
+
+  return result;
+};
+
+Array.prototype.firstOrUndefined = function(predicate: Function) {
+  let result = this.filter(x => predicate(x));
+
+  if (result.length > 0) {
+    return result[0];
+  }
+
+  return undefined;
+};
+
+Array.prototype.zip = function<TSecond, TResult>(second: TSecond[], predicate: Function): TResult[] {
+  return this.map((element: any, index: number) => predicate(element, second[index]));
+};
+
+Array.prototype.remove = function<T>(element: T): number {
+  let index = this.indexOf(element);
+  if (index >= 0) {
+    this.splice(index, 1);
+  }
+
+  return index;
+}
