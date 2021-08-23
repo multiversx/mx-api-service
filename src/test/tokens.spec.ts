784b6257678748f5850f9645928fc8cac3fa5be8
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-08-23 12:09:35 +0300

    test token service

--- src/test/tokens.spec.ts
+++ src/test/tokens.spec.ts
@@ -1,9 +1,10 @@
 import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
-import { TokenController } from 'src/endpoints/tokens/token.controller';
 import { TokenDetailed } from 'src/endpoints/tokens/entities/token.detailed';
 import { Nft } from 'src/endpoints/tokens/entities/nft';
 import { NftType } from 'src/endpoints/tokens/entities/nft.type';
+import { TokenService } from 'src/endpoints/tokens/token.service';
+import { NftFilter } from 'src/endpoints/tokens/entities/nft.filter';
 
 expect.extend({
     toHaveStructure(received: any, keys: string[]) {
@@ -26,8 +27,8 @@ expect.extend({
     },
 });
 
-describe('Token Controller', () => {
-  let tokenController: TokenController;
+describe('Token Service', () => {
+  let tokenService: TokenService;
   let tokenName: string;
   let tokenIdentifier: string;
   let nftCreator: string;
@@ -38,16 +39,16 @@ describe('Token Controller', () => {
       imports: [PublicAppModule],
     }).compile();
 
-    tokenController = moduleRef.get<TokenController>(TokenController);
+    tokenService = moduleRef.get<TokenService>(TokenService);
 
-    let nfts = await tokenController.getNfts(0, 1, undefined, undefined, undefined, undefined, undefined, undefined);
+    let nfts = await tokenService.getNfts({from: 0, size: 1}, new NftFilter());
     expect(nfts).toHaveLength(1);
 
     let nft = nfts[0];
     nftCreator = nft.creator;
     nftIdentifier = nft.identifier;
 
-    let tokens = await tokenController.getTokens(0, 1, undefined);
+    let tokens = await tokenService.getTokens({from: 0, size: 1}, undefined);
     expect(tokens).toHaveLength(1);
 
     let token = tokens[0];
@@ -58,7 +59,7 @@ describe('Token Controller', () => {
   describe('Tokens list', () => {
     describe('Tokens pagination', () => {
       it(`should return a list with 25 tokens`, async () => {
-        const tokensList = await tokenController.getTokens(0, 25, undefined);
+        const tokensList = await tokenService.getTokens({from: 0, size: 25}, undefined);
 
         expect(tokensList).toBeInstanceOf(Array);
         expect(tokensList).toHaveLength(25);
@@ -69,7 +70,7 @@ describe('Token Controller', () => {
       });
 
       it(`should return a list with 10 tokens`, async () => {
-        const tokensList = await tokenController.getTokens(0, 10, undefined);
+        const tokensList = await tokenService.getTokens({from: 0, size: 10}, undefined);
         expect(tokensList).toBeInstanceOf(Array);
         expect(tokensList).toHaveLength(10);
 
@@ -81,7 +82,7 @@ describe('Token Controller', () => {
 
     describe('Tokens filters', () => {
       it(`should return a list of tokens for a collection`, async () => {
-        const tokensList = await tokenController.getTokens(0, 50, tokenName);
+        const tokensList = await tokenService.getTokens({from: 0, size: 50}, tokenName);
         expect(tokensList).toBeInstanceOf(Array);
 
         for (let token of tokensList) {
@@ -94,7 +95,7 @@ describe('Token Controller', () => {
 
   describe('Token count', () => {
     it(`should return a number`, async () => {
-      const tokensCount: Number = new Number(await tokenController.getTokenCount(undefined));
+      const tokensCount: Number = new Number(await tokenService.getTokenCount(undefined));
 
       expect(tokensCount).toBeInstanceOf(Number);
     });
@@ -102,14 +103,16 @@ describe('Token Controller', () => {
 
   describe('Specific token', () => {
     it(`should return a token for a specific identifier`, async () => {
-      const token = await tokenController.getToken(tokenIdentifier);
+      const token = await tokenService.getToken(tokenIdentifier);
 
-      expect(token.identifier).toBe(tokenIdentifier);
-      expect(token.name).toBe(tokenName)
+      if (token) {
+        expect(token.identifier).toBe(tokenIdentifier);
+        expect(token.name).toBe(tokenName);
+      }
     });
 
     it(`should throw 'Token not found' error`, async () => {
-      await expect(tokenController.getToken(tokenIdentifier + 'a')).rejects.toThrowError('Token not found');
+      await expect(tokenService.getToken(tokenIdentifier + 'a')).rejects.toThrowError('Token not found');
     });
   })
 
@@ -117,7 +120,7 @@ describe('Token Controller', () => {
   describe('Nfts list', () => {
     describe('Nfts pagination', () => {
       it(`should return a list with 25 nfts`, async () => {
-        const nftsList = await tokenController.getNfts(0, 25, undefined, undefined, undefined, undefined, undefined, undefined);
+        const nftsList = await tokenService.getNfts({from: 0, size: 25}, new NftFilter());
 
         expect(nftsList).toBeInstanceOf(Array);
         expect(nftsList).toHaveLength(25);
@@ -128,7 +131,7 @@ describe('Token Controller', () => {
       });
 
       it(`should return a list with 10 nfts`, async () => {
-        const nftsList = await tokenController.getNfts(0, 10, undefined, undefined, undefined, undefined, undefined, undefined);
+        const nftsList = await tokenService.getNfts({from: 0, size: 10}, new NftFilter());
         expect(nftsList).toBeInstanceOf(Array);
         expect(nftsList).toHaveLength(10);
 
@@ -143,7 +146,9 @@ describe('Token Controller', () => {
 
     describe('Nfts filters', () => {
       it(`should return a list with all nfts within a collection`, async () => {
-        const nftsList = await tokenController.getNfts(0, 25, undefined, undefined, tokenIdentifier, undefined, undefined, undefined);
+        const nftFilter = new NftFilter();
+        nftFilter.collection = tokenIdentifier
+        const nftsList = await tokenService.getNfts({from: 0, size: 25}, nftFilter);
         expect(nftsList).toBeInstanceOf(Array);
 
         for (let nft of nftsList) {
@@ -153,17 +158,21 @@ describe('Token Controller', () => {
       });
 
       it(`should return a list with SemiFungibleESDT tokens`, async () => {
-          const nftsList = await tokenController.getNfts(0, 25, undefined, NftType.SemiFungibleESDT, undefined, undefined, undefined, undefined);
-          expect(nftsList).toBeInstanceOf(Array);
+        const nftFilter = new NftFilter();
+        nftFilter.type = NftType.SemiFungibleESDT;
+        const nftsList = await tokenService.getNfts({from: 0, size: 25}, nftFilter);
+        expect(nftsList).toBeInstanceOf(Array);
 
-          for (let nft of nftsList) {
-              expect(nft).toHaveStructure(Object.keys(new Nft()));
-              expect(nft.type).toBe(NftType.SemiFungibleESDT);
-          }
+        for (let nft of nftsList) {
+            expect(nft).toHaveStructure(Object.keys(new Nft()));
+            expect(nft.type).toBe(NftType.SemiFungibleESDT);
+        }
       });
 
       it(`should return a list with all nfts of the creator`, async () => {
-        const nftsList = await tokenController.getNfts(0, 25, undefined, undefined, undefined, undefined, nftCreator, undefined);
+        const nftFilter = new NftFilter();
+        nftFilter.creator = nftCreator;
+        const nftsList = await tokenService.getNfts({from: 0, size: 25}, nftFilter);
         expect(nftsList).toBeInstanceOf(Array);
 
         for (let nft of nftsList) {
@@ -176,7 +185,7 @@ describe('Token Controller', () => {
 
   describe('Nft count', () => {
     it(`should return a number`, async () => {
-      const nftCount: Number = new Number(await tokenController.getNftCount(undefined, undefined, undefined, undefined, undefined, undefined));
+      const nftCount: Number = new Number(await tokenService.getNftCount(new NftFilter()));
 
       expect(nftCount).toBeInstanceOf(Number);
     });
@@ -184,13 +193,15 @@ describe('Token Controller', () => {
 
   describe('Specific nft', () => {
     it(`should return a nft for a specific identifier`, async () => {
-      const nft = await tokenController.getNft(nftIdentifier);
+      const nft = await tokenService.getNft(nftIdentifier);
 
-      expect(nft.identifier).toBe(nftIdentifier);
+      if (nft) {
+        expect(nft.token).toBe(nftIdentifier);
+      }
     });
 
     it(`should throw 'NFT not found' error`, async () => {
-      await expect(tokenController.getNft(nftIdentifier + 'a')).rejects.toThrowError('NFT not found');
+      await expect (tokenService.getNft(nftIdentifier + 'a')).toBeUndefined();
     });
   })
 
