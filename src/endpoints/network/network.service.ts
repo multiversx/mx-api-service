2123fd2c1e78bed1a5d80d00fae0c5879453e0e3
Mihai Eremia <jobs@dani.ro>
2021-09-01 20:07:30 +0300

    mend

--- src/endpoints/network/network.service.ts
+++ src/endpoints/network/network.service.ts
@@ -16,6 +16,7 @@ import { TransactionService } from '../transactions/transaction.service';
 import { VmQueryService } from '../vm.query/vm.query.service';
 import { NetworkConstants } from './entities/constants';
 import { Economics } from './entities/economics';
+import { NetworkConfig } from './entities/networkConfig';
 import { StakeService } from '../stake/stake.service';
 
 @Injectable()
@@ -30,11 +31,11 @@ export class NetworkService {
     private readonly transactionService: TransactionService,
     private readonly dataApiService: DataApiService,
     private readonly apiService: ApiService,
-    private readonly stakeService: StakeService
+    private readonly stakeService: StakeService,
   ) {}
 
   async getConstants(): Promise<NetworkConstants> {
-    let gatewayUrl = this.apiConfigService.getGatewayUrl();
+    const gatewayUrl = this.apiConfigService.getGatewayUrl();
 
     const {
       data: {
@@ -52,10 +53,16 @@ export class NetworkService {
       },
     } = await this.apiService.get(`${gatewayUrl}/network/config`);
 
-    return { chainId, gasPerDataByte, minGasLimit, minGasPrice, minTransactionVersion };
+    return {
+      chainId,
+      gasPerDataByte,
+      minGasLimit,
+      minGasPrice,
+      minTransactionVersion,
+    };
   }
 
-  async getNetworkConfig() {
+  async getNetworkConfig(): Promise<NetworkConfig> {
     const [
       {
         config: { erd_round_duration, erd_rounds_per_epoch },
@@ -65,7 +72,7 @@ export class NetworkService {
       },
     ] = await Promise.all([
       this.gatewayService.get('network/config'),
-      this.gatewayService.get('network/status/4294967295')
+      this.gatewayService.get('network/status/4294967295'),
     ]);
 
     const roundsPassed = erd_rounds_passed_in_current_epoch;
@@ -79,48 +86,61 @@ export class NetworkService {
     return this.cachingService.getOrSetCache(
       'economics',
       async () => await this.getEconomicsRaw(),
-      Constants.oneMinute() * 10
+      Constants.oneMinute() * 10,
     );
   }
 
   private async getEconomicsRaw(): Promise<Economics> {
     const locked = 2660000;
     const [
-      { account: { balance } },
-      { metrics: { erd_total_supply } },
+      {
+        account: { balance },
+      },
+      {
+        metrics: { erd_total_supply },
+      },
       [, totalWaitingStakeBase64],
       priceValue,
       marketCapValue,
     ] = await Promise.all([
-      this.gatewayService.get(`address/${this.apiConfigService.getAuctionContractAddress()}`),
+      this.gatewayService.get(
+        `address/${this.apiConfigService.getAuctionContractAddress()}`,
+      ),
       this.gatewayService.get('network/economics'),
       this.vmQueryService.vmQuery(
         this.apiConfigService.getDelegationContractAddress(),
         'getTotalStakeByType',
       ),
       this.dataApiService.getQuotesHistoricalLatest(DataQuoteType.price),
-      this.dataApiService.getQuotesHistoricalLatest(DataQuoteType.marketCap)
+      this.dataApiService.getQuotesHistoricalLatest(DataQuoteType.marketCap),
     ]);
 
-    const totalWaitingStakeHex = Buffer.from(totalWaitingStakeBase64, 'base64').toString('hex');
-    let totalWaitingStake = BigInt(
-      totalWaitingStakeHex ? '0x' + totalWaitingStakeHex : totalWaitingStakeHex
+    const totalWaitingStakeHex = Buffer.from(
+      totalWaitingStakeBase64,
+      'base64',
+    ).toString('hex');
+    const totalWaitingStake = BigInt(
+      totalWaitingStakeHex ? '0x' + totalWaitingStakeHex : totalWaitingStakeHex,
     );
 
-    const staked = parseInt((BigInt(balance) + totalWaitingStake).toString().slice(0, -18));
+    const staked = parseInt(
+      (BigInt(balance) + totalWaitingStake).toString().slice(0, -18),
+    );
     const totalSupply = parseInt(erd_total_supply.slice(0, -18));
 
     const circulatingSupply = totalSupply - locked;
 
-    let aprInfo = await this.getApr();
+    const aprInfo = await this.getApr();
 
-    return { 
-      totalSupply, 
-      circulatingSupply, 
-      staked, 
-      price: priceValue ? parseFloat(priceValue.toFixed(2)) : undefined, 
-      marketCap: marketCapValue ? parseInt(marketCapValue.toFixed(0)) : undefined, 
-      apr: aprInfo.apr ? aprInfo.apr.toRounded(6) : 0, 
+    return {
+      totalSupply,
+      circulatingSupply,
+      staked,
+      price: priceValue ? parseFloat(priceValue.toFixed(2)) : undefined,
+      marketCap: marketCapValue
+        ? parseInt(marketCapValue.toFixed(0))
+        : undefined,
+      apr: aprInfo.apr ? aprInfo.apr.toRounded(6) : 0,
       topUpApr: aprInfo.topUpApr ? aprInfo.topUpApr.toRounded(6) : 0,
       baseApr: aprInfo.baseApr ? aprInfo.baseApr.toRounded(6) : 0,
     };
@@ -128,10 +148,13 @@ export class NetworkService {
 
   async getStats(): Promise<Stats> {
     const metaChainShard = this.apiConfigService.getMetaChainShardId();
-    
+
     const [
       {
-        config: { erd_num_shards_without_meta: shards, erd_round_duration: refreshRate },
+        config: {
+          erd_num_shards_without_meta: shards,
+          erd_round_duration: refreshRate,
+        },
       },
       {
         status: {
@@ -142,7 +165,7 @@ export class NetworkService {
       },
       blocks,
       accounts,
-      transactions
+      transactions,
     ] = await Promise.all([
       this.gatewayService.get('network/config'),
       this.gatewayService.get(`network/status/${metaChainShard}`),
@@ -160,25 +183,29 @@ export class NetworkService {
       epoch,
       roundsPassed,
       roundsPerEpoch,
-    }
+    };
   }
 
-  async getValidatorStatistics() {
+  async getValidatorStatistics(): Promise<any> {
     return await this.gatewayService.get('validator/statistics');
   }
 
-  async getApr(): Promise<{ apr: number, topUpApr: number, baseApr: number }> {
-    let stats = await this.getStats();
-    let config = await this.getNetworkConfig();
-    let stake = await this.stakeService.getGlobalStake();
-    const { account: { balance: stakedBalance } } = await this.gatewayService.get(`address/${this.apiConfigService.getAuctionContractAddress()}`);
-    let [ activeStake ] = await this.vmQueryService.vmQuery(
+  async getApr(): Promise<{ apr: number; topUpApr: number; baseApr: number }> {
+    const stats = await this.getStats();
+    const config = await this.getNetworkConfig();
+    const stake = await this.stakeService.getGlobalStake();
+    const {
+      account: { balance: stakedBalance },
+    } = await this.gatewayService.get(
+      `address/${this.apiConfigService.getAuctionContractAddress()}`,
+    );
+    let [activeStake] = await this.vmQueryService.vmQuery(
       this.apiConfigService.getDelegationContractAddress(),
       'getTotalActiveStake',
     );
     activeStake = this.numberDecode(activeStake);
 
-    let elrondConfig = {
+    const elrondConfig = {
       feesInEpoch: 0,
       stakePerNode: 2500,
       protocolSustainabilityRewards: 0.1,
@@ -186,41 +213,52 @@ export class NetworkService {
 
     const feesInEpoch = elrondConfig.feesInEpoch;
     const stakePerNode = elrondConfig.stakePerNode;
-    const protocolSustainabilityRewards = elrondConfig.protocolSustainabilityRewards;
-    const epochDuration = config.roundDuration / 1000 * config.roundsPerEpoch;
+    const protocolSustainabilityRewards =
+      elrondConfig.protocolSustainabilityRewards;
+    const epochDuration = (config.roundDuration / 1000) * config.roundsPerEpoch;
     const secondsInYear = 365 * 24 * 3600;
     const epochsInYear = secondsInYear / epochDuration;
 
-    let yearIndex = Math.floor(stats.epoch / epochsInYear);
-    let inflationAmounts = this.apiConfigService.getInflationAmounts();
+    const yearIndex = Math.floor(stats.epoch / epochsInYear);
+    const inflationAmounts = this.apiConfigService.getInflationAmounts();
 
     if (yearIndex >= inflationAmounts.length) {
-      throw new Error(`There is no inflation information for year with index ${yearIndex}`);
+      throw new Error(
+        `There is no inflation information for year with index ${yearIndex}`,
+      );
     }
 
-    let inflation = inflationAmounts[yearIndex];
+    const inflation = inflationAmounts[yearIndex];
     const rewardsPerEpoch = Math.max(inflation / epochsInYear, feesInEpoch);
 
-    const rewardsPerEpochWithoutProtocolSustainability = (1 - protocolSustainabilityRewards) * rewardsPerEpoch;
-    const topUpRewardsLimit = 0.5 * rewardsPerEpochWithoutProtocolSustainability;
+    const rewardsPerEpochWithoutProtocolSustainability =
+      (1 - protocolSustainabilityRewards) * rewardsPerEpoch;
+    const topUpRewardsLimit =
+      0.5 * rewardsPerEpochWithoutProtocolSustainability;
     const networkBaseStake = stake.activeValidators * stakePerNode;
     const networkTotalStake = NumberUtils.denominateString(stakedBalance);
 
-    const networkTopUpStake = networkTotalStake - (stake.totalValidators * stakePerNode) - (stake.queueSize * stakePerNode);
+    const networkTopUpStake =
+      networkTotalStake -
+      stake.totalValidators * stakePerNode -
+      stake.queueSize * stakePerNode;
 
-    const topUpReward = ((2 * topUpRewardsLimit) / Math.PI) * Math.atan(networkTopUpStake / (2 * 2000000));
-    const baseReward = rewardsPerEpochWithoutProtocolSustainability - topUpReward;
-    
+    const topUpReward =
+      ((2 * topUpRewardsLimit) / Math.PI) *
+      Math.atan(networkTopUpStake / (2 * 2000000));
+    const baseReward =
+      rewardsPerEpochWithoutProtocolSustainability - topUpReward;
+
     const apr = (epochsInYear * (topUpReward + baseReward)) / networkTotalStake;
 
-    let topUpApr = epochsInYear * topUpReward / networkTopUpStake;
-    let baseApr = epochsInYear * baseReward / networkBaseStake;
+    const topUpApr = (epochsInYear * topUpReward) / networkTopUpStake;
+    const baseApr = (epochsInYear * baseReward) / networkBaseStake;
 
     return { apr, topUpApr, baseApr };
   }
 
-  numberDecode(encoded: string) {
+  numberDecode(encoded: string): string {
     const hex = Buffer.from(encoded, 'base64').toString('hex');
     return BigInt(hex ? '0x' + hex : hex).toString();
-  };
+  }
 }
--- src/utils/number.utils.ts
+++ src/utils/number.utils.ts
@@ -1,18 +1,11 @@
-const BigNumber = require('bignumber.js');
-interface Denomination {
-  input: string;
-  denomination?: number;
-  decimals?: number;
-  showLastNonZeroDecimal?: boolean;
-  addCommas?: boolean;
-}
-
+import BigNumber from 'bignumber.js';
+import { Balance } from '@elrondnetwork/erdjs';
 export class NumberUtils {
   static denominate(value: BigInt): number {
     return Number(value.valueOf() / BigInt(Math.pow(10, 18)));
   }
-  static denominateFloat(input: Denomination): string {
-    return format(input);
+  static denominateFloat(input: string): string {
+    return Balance.fromString(input).toDenominated();
   }
   static denominateString(value: string): number {
     return NumberUtils.denominate(BigInt(value));
@@ -20,80 +13,6 @@ export class NumberUtils {
 
   static numberDecode(encoded: string): string {
     const hex = Buffer.from(encoded, 'base64').toString('hex');
-    return BigNumber(hex, 16).toString(10);
+    return new BigNumber(hex, 16).toString(10);
   }
-}
-function format({
-  input,
-  denomination = 18,
-  decimals = 6,
-  showLastNonZeroDecimal = false,
-  addCommas = false,
-}: Denomination) {
-  if (input === '...') {
-    return input;
-  }
-  if (input === '' || input === '0' || input === undefined) {
-    input = '0';
-  }
-  showLastNonZeroDecimal =
-    typeof showLastNonZeroDecimal !== 'undefined'
-      ? showLastNonZeroDecimal
-      : false;
-  let array = input.split('');
-  if (denomination !== 0) {
-    // make sure we have enough characters
-    while (array.length < denomination + 1) {
-      array.unshift('0');
-    }
-    // add our dot
-    array.splice(array.length - denomination, 0, '.');
-    // make sure there are enough decimals after the dot
-    while (array.length - array.indexOf('.') <= decimals) {
-      array.push('0');
-    }
-
-    if (showLastNonZeroDecimal) {
-      let nonZeroDigitIndex = 0;
-      for (let i = array.length - 1; i > 0; i--) {
-        if (array[i] !== '0') {
-          nonZeroDigitIndex = i + 1;
-          break;
-        }
-      }
-      const decimalsIndex = array.indexOf('.') + decimals + 1;
-      const sliceIndex = Math.max(decimalsIndex, nonZeroDigitIndex);
-      array = array.slice(0, sliceIndex);
-    } else {
-      // trim unnecessary characters after the dot
-      array = array.slice(0, array.indexOf('.') + decimals + 1);
-    }
-  }
-  if (addCommas) {
-    // add comas every 3 characters
-    array = array.reverse();
-    const reference = denomination
-      ? array.length - array.indexOf('.') - 1
-      : array.length;
-    const count = Math.floor(reference / 3);
-    for (let i = 1; i <= count; i++) {
-      const position = array.indexOf('.') + 3 * i + i;
-      if (position !== array.length) {
-        array.splice(position, 0, ',');
-      }
-    }
-    array = array.reverse();
-  }
-
-  const allDecimalsZero = array
-    .slice(array.indexOf('.') + 1)
-    .every((digit: { toString: () => string }) => digit.toString() === '0');
-
-  const string = array.join('');
-
-  if (allDecimalsZero) {
-    return string.split('.')[0];
-  }
-
-  return decimals === 0 ? string.split('.').join('') : string;
 }
