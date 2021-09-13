dcf91b7b794562cd7f29d087f286e142817d68c3
Mihai Daniel Eremia <jobs@dani.ro>
2021-09-13 16:26:57 +0300

    Add denomination float (#101)
    * Add denominationFloat from a string input
    
    * Add denominationFloat from a string input
    
    * mend
    
    * Rename networkConfig to network.config

--- src/utils/number.utils.ts
+++ src/utils/number.utils.ts
@@ -1,16 +1,18 @@
-const BigNumber = require('bignumber.js');
-
+import BigNumber from 'bignumber.js';
+import { Balance } from '@elrondnetwork/erdjs';
 export class NumberUtils {
   static denominate(value: BigInt): number {
     return Number(value.valueOf() / BigInt(Math.pow(10, 18)));
   }
-  
+  static denominateFloat(input: string): string {
+    return Balance.fromString(input).toDenominated();
+  }
   static denominateString(value: string): number {
     return NumberUtils.denominate(BigInt(value));
   }
-  
-  static numberDecode(encoded: string) {
+
+  static numberDecode(encoded: string): string {
     const hex = Buffer.from(encoded, 'base64').toString('hex');
-    return BigNumber(hex, 16).toString(10);
-  };
+    return new BigNumber(hex, 16).toString(10);
+  }
 }
