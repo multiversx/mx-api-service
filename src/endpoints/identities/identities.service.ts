32a80a9f284066c6ba27612e91ac48700690d5f3
Traian Anghel <tanghel@live.com>
2021-09-22 16:25:44 +0300

    fixed build

--- src/endpoints/identities/identities.service.ts
+++ src/endpoints/identities/identities.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from "@nestjs/common";
-import { ApiConfigService } from "src/common/api.config.service";
 import { CachingService } from "src/common/caching.service";
 import { KeybaseIdentity } from "src/common/entities/keybase.identity";
 import { KeybaseService } from "src/common/keybase.service";
@@ -17,7 +16,6 @@ export class IdentitiesService {
      private readonly nodeService: NodeService,
      private readonly keybaseService: KeybaseService,
      private readonly cachingService: CachingService,
-     private readonly apiConfigService: ApiConfigService,
   ) {}
 
   async getIdentity(identifier: string): Promise<Identity | undefined> {
