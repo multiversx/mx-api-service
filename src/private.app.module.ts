4de9cd219b0191bfc158bc8bad99222571c86238
Traian Anghel <tanghel@live.com>
2021-09-30 16:23:55 +0300

    Multi cache delete (#161)
    * properly notify deletion. delete using wildcard technique
    
    * fixed dependency issues

--- src/private.app.module.ts
+++ src/private.app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ClientOptions, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ApiConfigService } from './common/api.config.service';
 import { CacheController } from './endpoints/cache/cache.controller';
 import { MetricsController } from './endpoints/metrics/metrics.controller';
 import { PublicAppModule } from './public.app.module';
@@ -9,10 +11,28 @@ import { PublicAppModule } from './public.app.module';
   ],
   controllers: [
     MetricsController,
-    CacheController
+    CacheController,
   ],
   providers: [
-    
+    {
+      provide: 'PUBSUB_SERVICE',
+      useFactory: (apiConfigService: ApiConfigService) => {
+        let clientOptions: ClientOptions = {
+          transport: Transport.REDIS,
+          options: {
+            url: `redis://${apiConfigService.getRedisUrl()}:6379`,
+            retryDelay: 1000,
+            retryAttempts: 10,
+            retry_strategy: function(_: any) {
+              return 1000;
+            },
+          }
+        };
+
+        return ClientProxyFactory.create(clientOptions);
+      },
+      inject: [ ApiConfigService ]
+    },
   ],
 })
 export class PrivateAppModule {}
