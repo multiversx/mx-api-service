5350cb847bf33ba7858bcaf0351afcefd09bf54d
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-01 11:12:12 +0300

    take nft name from gateway only if it is'n set

--- src/endpoints/tokens/token.service.ts
+++ src/endpoints/tokens/token.service.ts
@@ -295,10 +295,12 @@ export class TokenService {
     await this.nftThumbnailService.updateThumbnailUrlForNfts(nfts);
 
     for (let nft of nfts) {
-      let gatewayNft = await this.getNft(nft.collection);
-      if (gatewayNft) {
-        nft.name = gatewayNft.name;
-        nft.type = gatewayNft.type;
+      if (!nft.name || !nft.type) {
+        let gatewayNft = await this.getNft(nft.collection);
+        if (gatewayNft) {
+          nft.name = gatewayNft.name;
+          nft.type = gatewayNft.type;
+        }
       }
     }
 
