d6e544915da1360f086a805796e787e3ae489459
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-18 16:46:09 +0300

    register cache module

--- src/common/caching/caching.module.ts
+++ src/common/caching/caching.module.ts
@@ -1,38 +1,20 @@
-import { Module } from "@nestjs/common";
-import { ClientOptions, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { CacheModule, forwardRef, Module } from "@nestjs/common";
+import { RoundModule } from "src/endpoints/rounds/round.module";
 import { ApiConfigModule } from "../api.config.module";
-import { ApiConfigService } from "../api.config.service";
 import { CacheConfigService } from "./cache.config.service";
 import { CachingService } from "./caching.service";
 
 @Module({
   imports: [
+    CacheModule.register(),
     ApiConfigModule,
+    forwardRef(() => RoundModule),
   ],
   providers: [
     CachingService, CacheConfigService,
-    {
-      provide: 'PUBSUB_SERVICE',
-      useFactory: (apiConfigService: ApiConfigService) => {
-        let clientOptions: ClientOptions = {
-          transport: Transport.REDIS,
-          options: {
-            url: `redis://${apiConfigService.getRedisUrl()}:6379`,
-            retryDelay: 1000,
-            retryAttempts: 10,
-            retry_strategy: function(_: any) {
-              return 1000;
-            },
-          }
-        };
-
-        return ClientProxyFactory.create(clientOptions);
-      },
-      inject: [ ApiConfigService ]
-    },
   ],
   exports: [
-    CachingService, CacheConfigService
+    CachingService, CacheConfigService,
   ]
 })
 export class CachingModule { }
