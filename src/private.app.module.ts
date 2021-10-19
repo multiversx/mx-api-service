75dd025019392e324c61cf8702ef0933d6d59b6f
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-19 12:43:43 +0300

    private app module

--- src/private.app.module.ts
+++ src/private.app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ClientOptions, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ApiConfigModule } from './common/api.config.module';
 import { ApiConfigService } from './common/api.config.service';
+import { CachingModule } from './common/caching/caching.module';
 import { CacheController } from './endpoints/cache/cache.controller';
 import { MetricsController } from './endpoints/metrics/metrics.controller';
-import { PublicAppModule } from './public.app.module';
+import { MetricsModule } from './endpoints/metrics/metrics.module';
+import { PubSubModule } from './pub.sub.module';
 
 @Module({
   imports: [
-    PublicAppModule
+    forwardRef(() => ApiConfigModule),
+    forwardRef(() => CachingModule),
+    forwardRef(() => MetricsModule),
+    PubSubModule,
   ],
   controllers: [
     MetricsController,
