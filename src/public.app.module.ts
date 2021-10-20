fef11f3729a9a73fe78d92a6d29bc4ad0424b752
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-20 12:20:22 +0300

    define logging module

--- src/public.app.module.ts
+++ src/public.app.module.ts
@@ -1,33 +1,15 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { WinstonModule } from 'nest-winston';
-import * as winston from 'winston';
-const DailyRotateFile = require('winston-daily-rotate-file');
 import "./utils/extensions/array.extensions";
 import "./utils/extensions/date.extensions";
 import "./utils/extensions/number.extensions";
 import { CommonModule } from './common/common.module';
 import { EndpointsServicesModule } from  './endpoints/enpoints.services.module';
 import { EndpointsControllersModule } from './endpoints/endpoints.controllers.module';
+import { LoggingModule } from './logging.module';
 
 @Module({
   imports: [
-    WinstonModule.forRoot({
-      level: 'verbose',
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-      transports: [
-        new winston.transports.Console({ level: 'info' }),
-        new DailyRotateFile({
-          filename: 'application-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          createSymlink: true,
-          dirname: 'dist/logs',
-          symlinkName: 'application.log'
-        }),
-      ]
-    }),
+    LoggingModule,
     forwardRef(() => CommonModule),
     forwardRef(() => EndpointsServicesModule),
     EndpointsControllersModule,
