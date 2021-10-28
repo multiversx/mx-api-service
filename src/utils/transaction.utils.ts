5b52de3a2a4cfe3a5ae5eaf38f2ce5c667b7202c
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-10-28 11:46:48 +0300

    add nft create utils

--- src/utils/transaction.utils.ts
+++ src/utils/transaction.utils.ts
@@ -5,9 +5,19 @@ export class TransactionUtils {
     return BinaryUtils.base64Decode(data).startsWith('changeSFTToMetaESDT@');
   }
 
+  static isESDTNFTCreateTramsaction(data: string) {
+    return BinaryUtils.base64Decode(data).startsWith('ESDTNFTCreateTramsaction@');
+  }
+
   static extractCollectionIdentifier(data: string) {
     const collectionIdentifierHex = BinaryUtils.base64Decode(data).split('@')[1];
 
     return BinaryUtils.hexToString(collectionIdentifierHex);
+  }
+
+  static extractNFTMetadata(data: string) {
+    const nftMetadataHex = BinaryUtils.base64Decode(data).split('@')[5];
+
+    return BinaryUtils.hexToString(nftMetadataHex);
   }
 }
