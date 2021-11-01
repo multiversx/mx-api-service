c5f784c8323c1ca40f4f856e76c78283e8a9faa5
Traian Anghel <tanghel@live.com>
2021-11-01 10:55:31 +0200

    unified ticker both for collections and tokens (#241)
    * unified ticker both for collections and tokens
    
    * fixed assets property for token

--- src/endpoints/tokens/entities/token.detailed.ts
+++ src/endpoints/tokens/entities/token.detailed.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Token } from "./token";
-import { TokenAssets } from "./token.assets";
 
 export class TokenDetailed extends Token {
   @ApiProperty()
@@ -23,7 +22,4 @@ export class TokenDetailed extends Token {
   
   @ApiProperty()
   canWipe: boolean = false;
-
-  @ApiProperty()
-  assets: TokenAssets | undefined = undefined;
 }
