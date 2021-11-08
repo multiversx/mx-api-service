a6692e97f44b973dfdee5bb3ae3202b051c2a36c
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-11-08 15:31:52 +0200

    smart contract results module and services

---
+++ src/endpoints/sc-results/scresult.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
+import { ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { SmartContractResult } from "../transactions/entities/smart.contract.result";
+import { SmartContractResultService } from "./scresult.service";
+
+@Controller()
+@ApiTags('sc-results')
+export class SmartContractResultController {
+	constructor(private readonly scResultService: SmartContractResultService) {}
+
+	@ApiQuery({ name: 'from', description: 'Numer of items to skip for the result set', required: false  })
+  @ApiQuery({ name: 'size', description: 'Number of items to retrieve', required: false  })
+  @Get("/sc-results")
+  @ApiResponse({
+    status: 200,
+    description: 'All smart contract results available on the blockchain',
+    type: SmartContractResult
+  })
+  getScResults(
+    @Query('from', new DefaultValuePipe(0), ParseIntPipe) from: number, 
+    @Query('size', new DefaultValuePipe(25), ParseIntPipe) size: number,
+  ): Promise<SmartContractResult[]> {
+    return this.scResultService.getScResults({from, size});
+  }
+
+  @Get("/sc-results/count")
+  @ApiResponse({
+    status: 200,
+    description: 'The count of all smart contract results available on the blockchain',
+    type: SmartContractResult
+  })
+  getScResultsCount(): Promise<number> {
+    return this.scResultService.getScResultsCount();
+  }
+
+  @Get("/sc-results/:scHash")
+  @ApiResponse({
+    status: 200,
+    description: 'The specific smart contract result',
+    type: SmartContractResult
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Smart contract result not found'
+  })
+  getScResult(@Param('scHash') scHash: string): Promise<SmartContractResult> {
+    return this.scResultService.getScResult(scHash);
+  }
+}
---
+++ src/endpoints/sc-results/scresult.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from "@nestjs/common";
+import { ElasticService } from "src/common/elastic/elastic.service";
+import { AbstractQuery } from "src/common/elastic/entities/abstract.query";
+import { ElasticQuery } from "src/common/elastic/entities/elastic.query";
+import { ElasticSortOrder } from "src/common/elastic/entities/elastic.sort.order";
+import { QueryConditionOptions } from "src/common/elastic/entities/query.condition.options";
+import { QueryType } from "src/common/elastic/entities/query.type";
+import { QueryPagination } from "src/common/entities/query.pagination";
+import { ApiUtils } from "src/utils/api.utils";
+import { SmartContractResult } from "../transactions/entities/smart.contract.result";
+
+@Injectable()
+export class SmartContractResultService {
+  constructor(
+    private readonly elasticService: ElasticService, 
+  ) { }
+
+  private buildSmartContractResultFilterQuery(address?: string): ElasticQuery {
+    let shouldQueries: AbstractQuery[] = [];
+    let mustQueries: AbstractQuery[] = [];
+
+    if (address) {
+      shouldQueries.push(QueryType.Match('sender', address));
+      shouldQueries.push(QueryType.Match('receiver', address));
+    }
+
+    let elasticQuery = ElasticQuery.create()
+      .withCondition(QueryConditionOptions.should, shouldQueries)
+      .withCondition(QueryConditionOptions.must, mustQueries)
+
+    return elasticQuery;
+  }
+
+  async getScResults(pagination: QueryPagination): Promise<SmartContractResult[]> {
+    let elasticResult = await this.elasticService.getList('scresults', 'hash', ElasticQuery.create().withPagination(pagination));
+
+    return elasticResult.map(scResult => ApiUtils.mergeObjects(new SmartContractResult(), scResult))
+  }
+
+  async getScResult(scHash: string): Promise<SmartContractResult> {
+    const scResult =  await this.elasticService.getItem('scresults', 'hash', scHash);
+
+    return ApiUtils.mergeObjects(new SmartContractResult(), scResult);
+  }
+
+  async getScResultsCount(): Promise<number> {
+    return await this.elasticService.getCount('scresults', ElasticQuery.create());
+  }
+
+  async getAccountScResults(address: string, pagination: QueryPagination): Promise<SmartContractResult[]> {
+    const elasticQuery: ElasticQuery = this.buildSmartContractResultFilterQuery(address);
+    elasticQuery
+      .withPagination(pagination)
+      .withSort([ { name: 'timestamp', order: ElasticSortOrder.descending } ]);
+
+    let elasticResult = await this.elasticService.getList('scresults', 'hash', elasticQuery);
+
+    return elasticResult.map(scResult => ApiUtils.mergeObjects(new SmartContractResult(), scResult))
+  }
+
+  async getAccountScResultsCount(address: string): Promise<SmartContractResult[]> {
+    const elasticQuery: ElasticQuery = this.buildSmartContractResultFilterQuery(address);
+
+    return await this.elasticService.getCount('scresults', elasticQuery);
+  }
+}
