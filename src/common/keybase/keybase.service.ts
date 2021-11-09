8f56bc74c7a266451275bb850c3f9473bab54951
Traian Anghel <tanghel@live.com>
2021-11-09 14:05:52 +0200

    some more optimizations in keybases function

--- src/common/keybase/keybase.service.ts
+++ src/common/keybase/keybase.service.ts
@@ -101,50 +101,47 @@ export class KeybaseService {
       return;
     }
 
-    const keybaseProvidersArr: Keybase[] = await this.getProvidersKeybasesRaw();
-    const keybasesNodesArr: Keybase[] = await this.getNodesKeybasesRaw();
+    const providerKeybases: Keybase[] = await this.getProvidersKeybasesRaw();
+    const nodeKeybases: Keybase[] = await this.getNodesKeybasesRaw();
 
-    const keybasesArr: Keybase[] = [...keybaseProvidersArr, ...keybasesNodesArr];
+    const allKeybases: Keybase[] = [...providerKeybases, ...nodeKeybases];
 
-    const identities = new Set([...keybasesArr.map((keybase) => keybase.identity)]);
+    const distinctIdentities = allKeybases.map(x => x.identity ?? '').filter(x => x !== '').distinct();
 
-    await Promise.all(Array.from(identities).map((identity) => this.confirmKeybasesAgainstKeybasePubForIdentity(identity)));
+    await Promise.all(distinctIdentities.map(identity => this.confirmKeybasesAgainstKeybasePubForIdentity(identity)));
   }
 
-  async confirmKeybasesAgainstKeybasePubForIdentity(identity: string | undefined): Promise<void> {
-    const isKeybaseUp = await this.isKeybaseUp();
-    if (!isKeybaseUp || !identity) {
+  async confirmKeybasesAgainstKeybasePubForIdentity(identity: string): Promise<void> {
+    const { data: html } = await this.apiService.get(`https://keybase.pub/${identity}/elrond`, 100000, async (error) => error.response?.status === HttpStatus.NOT_FOUND);
+
+    if (!html) {
+      this.logger.log(`For identity '${identity}', no keybase.pub entry was found`);
       return;
     }
 
-    try {
-      const { data } = await this.apiService.get(`https://keybase.pub/${identity}/elrond`, 100000);
-
-      const nodesRegex = new RegExp("https:\/\/keybase.pub\/" + identity + "\/elrond\/[0-9a-f]{192}", 'g')
-      const blses: string[] = [];
-      for (let keybaseUrl of data.match(nodesRegex) || []) {
-        const bls = keybaseUrl.match(/[0-9a-f]{192}/)[0];
-        blses.push(bls);
-      }
-  
-      const providersRegex = new RegExp("https:\/\/keybase.pub\/" + identity + "\/elrond\/erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq[0-9a-z]{13}", 'g');
-      const addresses: string[] = [];
-      for (let keybaseUrl of data.match(providersRegex) || []) {
-        const bls = keybaseUrl.match(/erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq[0-9a-z]{13}/)[0];
-        addresses.push(bls);
-      }
-  
-      await this.cachingService.batchProcess(
-        [...blses, ...addresses],
-        key => `keybase:${key}`,
-        async () => await true,
-        Constants.oneMonth() * 6,
-        true
-      );
-    } catch(error) {
-      this.logger.log(`Keybases not found for identity ${identity}`);
+    const nodesRegex = new RegExp("https:\/\/keybase.pub\/" + identity + "\/elrond\/[0-9a-f]{192}", 'g')
+    const blses: string[] = [];
+    for (let keybaseUrl of html.match(nodesRegex) || []) {
+      const bls = keybaseUrl.match(/[0-9a-f]{192}/)[0];
+      blses.push(bls);
     }
-    
+
+    const providersRegex = new RegExp("https:\/\/keybase.pub\/" + identity + "\/elrond\/erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq[0-9a-z]{13}", 'g');
+    const addresses: string[] = [];
+    for (let keybaseUrl of html.match(providersRegex) || []) {
+      const bls = keybaseUrl.match(/erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq[0-9a-z]{13}/)[0];
+      addresses.push(bls);
+    }
+
+    this.logger.log(`For identity '${identity}', found ${blses.length} blses and addresses ${addresses}`);
+
+    await this.cachingService.batchProcess(
+      [...blses, ...addresses],
+      key => `keybase:${key}`,
+      async () => await true,
+      Constants.oneMonth() * 6,
+      true
+    );
   }
 
 
