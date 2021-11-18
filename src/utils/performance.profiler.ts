7de2dcd03537d9555c2071d53871d1ddba6ae185
Traian Anghel <tanghel@live.com>
2021-11-18 18:55:25 +0200

    sub-millisecond accuracy for performance profiler, convenience function to profiler promise directly

--- src/utils/performance.profiler.ts
+++ src/utils/performance.profiler.ts
@@ -8,18 +8,37 @@ export class PerformanceProfiler {
   duration: number = 0;
 
   constructor(description: string = '') {
-    this.started = Date.now();
+    this.started = this.now();
     this.description = description;
   }
 
   stop(description: string | null = null, log: boolean = false) {
-    this.stopped = Date.now();
+    this.stopped = this.now();
     this.duration = this.stopped - this.started;
 
     if (log) {
       let logger = new Logger(PerformanceProfiler.name);
 
-      logger.log(`${description ?? this.description}: ${this.duration}ms`);
+      logger.log(`${description ?? this.description}: ${this.duration.toFixed(3)}ms`);
+    }
+  }
+
+  private now() {
+    let hrTime = process.hrtime();
+    return hrTime[0] * 1000 + hrTime[1] / 1000000;
+  }
+
+  static async profile<T>(description: string, promise: Promise<T> | (() => Promise<T>)): Promise<T> {
+    let profiler = new PerformanceProfiler();
+   
+    try {
+      if (promise instanceof Function) {
+        return await promise();
+      } else {
+        return await promise;
+      }
+    } finally {
+      profiler.stop(description, true);
     }
   }
 }
