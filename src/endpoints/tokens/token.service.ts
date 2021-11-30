7b51d7e24f4f12d8e46aa7706e9c2ec85231e0b2
Traian Anghel <tanghel@live.com>
2021-11-30 12:43:48 +0200

    when filtering by identifier, always full match

--- src/endpoints/tokens/token.service.ts
+++ src/endpoints/tokens/token.service.ts
@@ -76,7 +76,7 @@ export class TokenService {
     if (filter.name) {
       let nameLower = filter.name.toLowerCase();
 
-      tokens = tokens.filter(token => token.name.toLowerCase().includes(nameLower));
+      tokens = tokens.filter(token => token.name.toLowerCase() === nameLower);
     }
 
     if (filter.identifier) {
