67af2a09d5d470ec00e09bbc65ce37eef64750d5
Traian Anghel <tanghel@live.com>
2021-11-30 12:43:35 +0200

    array pipe adjustments

--- src/utils/pipes/parse.array.pipe.ts
+++ src/utils/pipes/parse.array.pipe.ts
@@ -1,21 +1,22 @@
 import { ArgumentMetadata, HttpException, HttpStatus, PipeTransform } from "@nestjs/common";
 
-const MAX_ARRAY_SIZE = 1024;
 
 export class ParseArrayPipe implements PipeTransform<string | undefined, Promise<string[] | undefined>> {
-    transform(value: string | undefined, _: ArgumentMetadata): Promise<string[] | undefined> {
-        return new Promise(resolve => {
-            if (value === undefined || value === '') {
-                return resolve(undefined);
-            }
-            const valueArray = value.split(',');
+  private MAX_ARRAY_SIZE = 1024;
 
-            if (valueArray.length > MAX_ARRAY_SIZE) {
-              throw new HttpException(`Validation failed (less than ${MAX_ARRAY_SIZE} comma separated values expected)`, HttpStatus.BAD_REQUEST);
-            }
-            else {
-              resolve(valueArray);
-            }
-        });
-    }
+  transform(value: string | undefined, _: ArgumentMetadata): Promise<string[] | undefined> {
+    return new Promise(resolve => {
+      if (value === undefined || value === '') {
+        return resolve(undefined);
+      }
+
+      const valueArray = value.split(',');
+
+      if (valueArray.length > this.MAX_ARRAY_SIZE) {
+        throw new HttpException(`Validation failed (less than ${this.MAX_ARRAY_SIZE} comma separated values expected)`, HttpStatus.BAD_REQUEST);
+      }
+
+      resolve(valueArray);
+    });
+  }
 }
