4b437d56a897424b7ac56e449f0333672a197162
Traian Anghel <tanghel@live.com>
2021-12-03 10:48:06 +0200

    if cache miss, execute with pending promise

--- src/common/caching/caching.service.ts
+++ src/common/caching/caching.service.ts
@@ -44,24 +44,27 @@ export class CachingService {
     return value;
   };
 
-  pendingGetRemotes: { [key: string]: Promise<any> } = {};
+  pendingPromises: { [key: string]: Promise<any> } = {};
+
+  private async executeWithPendingPromise<T>(key: string, promise: () => Promise<T>): Promise<T> {
+    let pendingGetRemote = this.pendingPromises[key];
+    if (pendingGetRemote) {
+      return await pendingGetRemote;
+    } else {
+      try {
+        pendingGetRemote = promise();
+  
+        this.pendingPromises[key] = pendingGetRemote;
+
+        return await pendingGetRemote;
+      } finally {
+        delete this.pendingPromises[key];
+      }
+    }
+  }
 
   public async getCacheRemote<T>(key: string): Promise<T | undefined> {
-    let response;
-
-    let pendingGetRemote = this.pendingGetRemotes[key];
-    if (pendingGetRemote) {
-      response = await pendingGetRemote;
-    } else {
-      pendingGetRemote = this.asyncGet(key);
-
-      this.pendingGetRemotes[key] = pendingGetRemote;
-
-      response = await pendingGetRemote;
-
-      delete this.pendingGetRemotes[key];
-    }
-
+    let response = await this.executeWithPendingPromise<string | undefined>(key, () => this.asyncGet(key));
     if (response === undefined) {
       return undefined;
     }
@@ -285,7 +288,7 @@ export class CachingService {
       return cached;
     }
 
-    let value = await promise();
+    let value = await this.executeWithPendingPromise(key, promise);
     profiler.stop(`Cache miss for key ${key}`);
 
     if (localTtl > 0) {
