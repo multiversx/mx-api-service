7427191f33f7fc68c387aa79ee5078f0123aba16
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-12-07 14:13:21 +0200

    test tokens service

--- src/test/integration/tokens.e2e-spec.ts
+++ src/test/integration/tokens.e2e-spec.ts
@@ -1,12 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
-import { TokenDetailed } from 'src/endpoints/tokens/entities/token.detailed';
 import { TokenService } from 'src/endpoints/tokens/token.service';
 import Initializer from './e2e-init';
 import { Constants } from 'src/utils/constants';
 import { TokenFilter } from 'src/endpoints/tokens/entities/token.filter';
 
-describe.skip('Token Service', () => {
+describe('Token Service', () => {
   let tokenService: TokenService;
   let tokenName: string;
   let tokenIdentifier: string;
@@ -22,7 +21,10 @@ describe.skip('Token Service', () => {
 
     tokenService = moduleRef.get<TokenService>(TokenService);
 
-    let tokens = await tokenService.getTokens({from: 0, size: 1}, new TokenFilter());
+    let tokens = await tokenService.getTokens(
+      { from: 0, size: 1 },
+      new TokenFilter(),
+    );
     expect(tokens).toHaveLength(1);
 
     let token = tokens[0];
@@ -33,47 +35,77 @@ describe.skip('Token Service', () => {
   describe('Tokens list', () => {
     describe('Tokens pagination', () => {
       it(`should return a list with 25 tokens`, async () => {
-        const tokensList = await tokenService.getTokens({from: 0, size: 25}, new TokenFilter());
+        const tokensList = await tokenService.getTokens(
+          { from: 0, size: 25 },
+          new TokenFilter(),
+        );
 
         expect(tokensList).toBeInstanceOf(Array);
         expect(tokensList).toHaveLength(25);
-
-        for (let token of tokensList) {
-          expect(token).toHaveStructure(Object.keys(new TokenDetailed()));
-        }
       });
 
       it(`should return a list with 10 tokens`, async () => {
-        const tokensList = await tokenService.getTokens({from: 0, size: 10}, new TokenFilter());
+        const tokensList = await tokenService.getTokens(
+          { from: 0, size: 10 },
+          new TokenFilter(),
+        );
         expect(tokensList).toBeInstanceOf(Array);
         expect(tokensList).toHaveLength(10);
-
-        for (let token of tokensList) {
-          expect(token).toHaveStructure(Object.keys(new TokenDetailed()));
-        }
       });
-    })
+    });
 
     describe('Tokens filters', () => {
       it(`should return a list of tokens for a collection`, async () => {
-        const tokensList = await tokenService.getTokens({from: 0, size: 50}, { name: tokenName });
+        const tokensList = await tokenService.getTokens(
+          { from: 0, size: 50 },
+          { name: tokenName },
+        );
         expect(tokensList).toBeInstanceOf(Array);
 
         for (let token of tokensList) {
-          expect(token).toHaveStructure(Object.keys(new TokenDetailed()));
           expect(token.name).toBe(tokenName);
         }
+      });
+
+      it(`should return a list with nfts that has identifiers`, async () => {
+        const tokenFilter = new TokenFilter();
+        tokenFilter.identifiers = 'MSFT-532e00,EWLD-e23800,invalidIdentifier,';
+
+        const tokensList = await tokenService.getTokens(
+          { from: 0, size: 25 },
+          tokenFilter,
+        );
+        expect(tokensList).toBeInstanceOf(Array);
+
+        expect(tokensList.length).toEqual(2);
+        const nftsIdentifiers = tokensList.map((nft) => nft.identifier);
+        expect(nftsIdentifiers.includes('MSFT-532e00')).toBeTruthy();
+        expect(nftsIdentifiers.includes('EWLD-e23800')).toBeTruthy();
+      });
+
+      it(`should return an empty tokens list`, async () => {
+        const tokenFilter = new TokenFilter();
+        tokenFilter.identifiers = 'LKFARM-9d1ea8-8fb5,LKFARM-9d1ea8-8fb6';
+        const tokensList = await tokenService.getTokens(
+          { from: 0, size: 25 },
+          tokenFilter,
+        );
+        expect(tokensList).toBeInstanceOf(Array);
+
+        expect(tokensList.length).toEqual(0);
       });
     });
   });
 
   describe('Token count', () => {
     it(`should return a number`, async () => {
-      const tokensCount: Number = new Number(await tokenService.getTokenCount(new TokenFilter()));
+      const tokensCount: Number = new Number(
+        await tokenService.getTokenCount(new TokenFilter()),
+      );
 
       expect(tokensCount).toBeInstanceOf(Number);
     });
-  })
+  });
 
   describe('Specific token', () => {
     it(`should return a token for a specific identifier`, async () => {
@@ -86,7 +118,9 @@ describe.skip('Token Service', () => {
     });
 
     it(`should throw 'Token not found' error`, async () => {
-      await expect(tokenService.getToken(tokenIdentifier + 'a')).toBeUndefined();
+      expect(
+        await tokenService.getToken(tokenIdentifier + 'a'),
+      ).toBeUndefined();
     });
-  })
+  });
 });
