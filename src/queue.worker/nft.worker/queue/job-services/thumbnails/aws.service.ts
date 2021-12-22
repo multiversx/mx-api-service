0eb7baf460625460e868c55bb26bf1b1fea4476e
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-12-22 15:41:26 +0200

    upload to S3 bucket with client-s3 package

--- src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
+++ src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
@@ -1,36 +1,36 @@
 import { S3 } from "@aws-sdk/client-s3";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
 
 @Injectable()
 export class AWSService {
+  private readonly logger: Logger;
   constructor(
     private readonly apiConfigService: ApiConfigService,
-  ) { }
+  ) {
+    this.logger = new Logger(AWSService.name);
+  }
 
   public async uploadToS3(path: string, buffer: Buffer, type: string): Promise<string> {
-    const s3 = new S3({ 
+    const s3 = new S3({
       credentials: {
         accessKeyId: this.apiConfigService.getAwsS3KeyId(),
         secretAccessKey: this.apiConfigService.getAwsS3Secret()
       },
-      region: 'eu-central-1'
     });
 
-    await new Promise((resolve, reject) => {
-      s3.putObject({
+    try {
+      await s3.putObject({
         Bucket: this.apiConfigService.getAwsS3Bucket(),
         Key: path,
         Body: buffer,
         ContentType: type
-      }, (err, data) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(data);
       });
-    });
+    } catch (err) {
+      //handle error
+      this.logger.log("Error when uploading thumbnail to S3");
+      this.logger.error(err);
+    }
 
     return this.getItemPath(path);
   }
