ce9799481bcfb064e2631f4817900a88bbd11b04
Traian Anghel <tanghel@live.com>
2021-12-23 14:38:05 +0200

    removed error handling. in case error occurs, let it bubble up

--- src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
+++ src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
@@ -1,15 +1,12 @@
 import { S3 } from "@aws-sdk/client-s3";
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
 
 @Injectable()
 export class AWSService {
-  private readonly logger: Logger;
   constructor(
     private readonly apiConfigService: ApiConfigService,
-  ) {
-    this.logger = new Logger(AWSService.name);
-  }
+  ) {}
 
   public async uploadToS3(path: string, buffer: Buffer, type: string): Promise<string> {
     const s3 = new S3({
@@ -20,17 +17,12 @@ export class AWSService {
       region: this.apiConfigService.getAwsS3Region(),
     });
 
-    try {
-      await s3.putObject({
-        Bucket: this.apiConfigService.getAwsS3Bucket(),
-        Key: path,
-        Body: buffer,
-        ContentType: type,
-      });
-    } catch (err) {
-      this.logger.log(`Error when uploading thumbnail to S3 for path '${path}'`);
-      this.logger.error(err);
-    }
+    await s3.putObject({
+      Bucket: this.apiConfigService.getAwsS3Bucket(),
+      Key: path,
+      Body: buffer,
+      ContentType: type,
+    });
 
     return this.getItemPath(path);
   }
