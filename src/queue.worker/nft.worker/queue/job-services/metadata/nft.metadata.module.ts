94ebf5cbb601bde5dd8d4709c2c5f0a2d6345198
AlexandruPislariu <alexandru.pislariu@elrond.com>
2021-12-28 10:13:18 +0200

    install mysql npm package

--- src/queue.worker/nft.worker/queue/job-services/metadata/nft.metadata.module.ts
+++ src/queue.worker/nft.worker/queue/job-services/metadata/nft.metadata.module.ts
@@ -2,12 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from 'src/common/database/database.module';
 import { NftModule } from 'src/endpoints/nfts/nft.module';
+import { NftMetadataDb } from './entities/nft.metadata.db';
 import { NftMetadataService } from './nft.metadata.service';
 
 @Module({
   imports: [
     forwardRef(() => NftModule),
-    TypeOrmModule.forFeature([NftMetadataModule]),
+    TypeOrmModule.forFeature([NftMetadataDb]),
     DatabaseModule
   ],
   controllers: [],
