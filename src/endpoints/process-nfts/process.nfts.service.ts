1020be1255acc2135ec85c31de472ee41c5e3c99
Traian Anghel <tanghel@live.com>
2021-12-29 16:01:06 +0200

    from asyncPool to 1 concurrent again

--- src/endpoints/process-nfts/process.nfts.service.ts
+++ src/endpoints/process-nfts/process.nfts.service.ts
@@ -22,7 +22,8 @@ export class ProcessNftsService {
     let nfts = await this.nftService.getNfts({ from: 0, size: 10000 }, { collection });
 
     let results = await asyncPool(
-      this.apiConfigService.getPoolLimit(),
+      // this.apiConfigService.getPoolLimit(),
+      1,
       nfts,
       async (nft: Nft) => await this.nftWorkerService.addProcessNftQueueJob(nft, settings)
     );
