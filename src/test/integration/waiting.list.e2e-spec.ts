522cc491e53811868df0357fd31c161d553898bb
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-06 00:00:11 +0200

    refactoring waiting.list.e2e-spec.ts

--- src/test/integration/waiting.list.e2e-spec.ts
+++ src/test/integration/waiting.list.e2e-spec.ts
@@ -4,42 +4,41 @@ import {Constants} from "../../utils/constants";
 import {Test} from "@nestjs/testing";
 import {PublicAppModule} from "../../public.app.module";
 
+describe('WaitingListService', () => {
+  let waitingListService: WaitingListService;
+  let waitingListAddress: string;
 
-describe('WaitingListService', ()=>{
-    let waitingListService: WaitingListService;
-    let waitingListAddress: string;
+  beforeAll(async () => {
+    await Initializer.initialize();
+    const moduleRef = await Test.createTestingModule({
+      imports: [PublicAppModule],
+    }).compile();
 
-    beforeAll(async () => {
-        await Initializer.initialize();
-        const moduleRef = await Test.createTestingModule({
-            imports: [PublicAppModule],
-        }).compile();
+    waitingListService = moduleRef.get<WaitingListService>(WaitingListService);
+  }, Constants.oneHour() * 1000);
 
-        waitingListService = moduleRef.get<WaitingListService>(WaitingListService);
-    }, Constants.oneHour() * 1000);
-
-    describe('Waiting List', ()=>{
-        describe('getWaitingList',  ()=>{
-            it('should return a list of waiting lists', async ()=>{
-                const shardList =  await waitingListService.getWaitingList();
-                expect(shardList).toBeInstanceOf(Array);
-            })
-        })
+  describe('Waiting List', () => {
+    describe('getWaitingList', () => {
+      it('should return a list of waiting lists', async () => {
+        const getList = await waitingListService.getWaitingList();
+        expect(getList).toBeInstanceOf(Array);
+      })
+    })
+  });
+  describe('Waiting List For Address', () => {
+    describe('getWaitingListForAddress', () => {
+      it('should return a list of waitings for a specified address ', async () => {
+        const getAddress = await waitingListService.getWaitingListForAddress(waitingListAddress);
+        expect(getAddress).toBeInstanceOf(Array);
+      })
     });
-    describe('Waiting List For Address', ()=>{
-        describe('getWaitingListForAddress',  ()=>{
-            it('should return a list of waitings for a specified address ', async ()=>{
-                const shardList =  await waitingListService.getWaitingListForAddress(waitingListAddress);
-                expect(shardList).toBeInstanceOf(Array);
-            })
-        })
-    });
-    describe('Waiting List Count', ()=>{
-        describe('getWaitingListCount',  ()=>{
-            it('should return count of lists', async ()=>{
-                const shardList: Number =  new Number(await waitingListService.getWaitingListCount());
-                expect(shardList).toBeInstanceOf(Number);
-            })
-        })
-    });
-})
+  });
+  describe('Waiting List Count', () => {
+    describe('getWaitingListCount', () => {
+      it('should return count of lists', async () => {
+        const gatWaitingList: Number = new Number(await waitingListService.getWaitingListCount());
+        expect(gatWaitingList).toBeInstanceOf(Number);
+      })
+    })
+  });
+});
