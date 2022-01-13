41955c4f8a2efdcf7ab25e97741eb5af957803a4
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-13 12:02:55 +0200

    collections.e2e-spec.ts integration test

--- src/test/integration/collections.e2e-spec.ts
+++ src/test/integration/collections.e2e-spec.ts
@@ -29,7 +29,7 @@ describe('Collection Service', () => {
     const account = accounts[0];
     accountAddress = account.address;
 
-    const collections = await collectionService.getNftCollections({ from: 0, size: 1 }, new CollectionFilter());
+    const collections = await collectionService.getNftCollections({from: 0, size: 1}, new CollectionFilter());
     expect(collections).toHaveLength(1);
 
     const nftCollection = collections[0];
@@ -39,14 +39,14 @@ describe('Collection Service', () => {
   describe('Collections list', () => {
     describe('Collections pagination', () => {
       it(`should return a list with 25 nfts collections`, async () => {
-        const collectionsList = await collectionService.getNftCollections({ from: 0, size: 25 }, new CollectionFilter());
+        const collectionsList = await collectionService.getNftCollections({from: 0, size: 25}, new CollectionFilter());
 
         expect(collectionsList).toBeInstanceOf(Array);
         expect(collectionsList).toHaveLength(25);
       });
 
       it(`should return a list with 10 nfts collections`, async () => {
-        const collectionsList = await collectionService.getNftCollections({ from: 0, size: 10 }, new CollectionFilter());
+        const collectionsList = await collectionService.getNftCollections({from: 0, size: 10}, new CollectionFilter());
         expect(collectionsList).toBeInstanceOf(Array);
         expect(collectionsList).toHaveLength(10);
 
@@ -61,7 +61,7 @@ describe('Collection Service', () => {
       it(`should return a list with all nfts within a collection`, async () => {
         const collectionFilter = new CollectionFilter();
         collectionFilter.collection = collectionIdentifier;
-        const nftsCollections = await collectionService.getNftCollections({ from: 0, size: 25 }, collectionFilter);
+        const nftsCollections = await collectionService.getNftCollections({from: 0, size: 25}, collectionFilter);
         expect(nftsCollections).toBeInstanceOf(Array);
 
         for (const nftCollection of nftsCollections) {
@@ -72,7 +72,7 @@ describe('Collection Service', () => {
       it(`should return a list with SemiFungibleESDT collections`, async () => {
         const collectionFilter = new CollectionFilter();
         collectionFilter.type = NftType.SemiFungibleESDT;
-        const collectionsList = await collectionService.getNftCollections({ from: 0, size: 25 }, collectionFilter);
+        const collectionsList = await collectionService.getNftCollections({from: 0, size: 25}, collectionFilter);
         expect(collectionsList).toBeInstanceOf(Array);
 
         for (const nftCollection of collectionsList) {
@@ -81,6 +81,7 @@ describe('Collection Service', () => {
       });
     });
   });
+
 
   describe('Collections count', () => {
     it(`should return a number`, async () => {
@@ -95,14 +96,17 @@ describe('Collection Service', () => {
       const collectionFilter = new CollectionAccountFilter();
       collectionFilter.collection = '3LR0NDPUNK-f87097-invalid';
 
-      const collectionAddress = await collectionService.getCollectionForAddress(accountAddress,collectionFilter.collection )
+      const collectionAddress = await collectionService.getCollectionForAddress(accountAddress, collectionFilter.collection)
       expect(collectionAddress).toBeUndefined();
     });
   });
 
   describe('Get Collections NonFungibleESDT for a specific address', () => {
     it(`should return collections of NonFungibleESDT for a address`, async () => {
-      const collectionsList = await collectionService.getCollectionsForAddress(accountAddress, new CollectionAccountFilter(), {from: 0, size:3});
+      const collectionsList = await collectionService.getCollectionsForAddress(accountAddress, new CollectionAccountFilter(), {
+        from: 0,
+        size: 3
+      });
       expect(collectionsList).toBeInstanceOf(Array);
     });
   });
