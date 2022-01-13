e499f95a54d74751d0905d604f17eb655147b560
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-13 09:54:18 +0200

    delegation.e2e-spec.ts 96,29% coverage

---
+++ src/test/integration/delegation.e2e-spec.ts
@@ -0,0 +1,33 @@
+import {DelegationService} from "../../endpoints/delegation/delegation.service";
+import Initializer from "./e2e-init";
+import {Test} from "@nestjs/testing";
+import {PublicAppModule} from "../../public.app.module";
+import {Constants} from "../../utils/constants";
+
+describe('Delegation Service', () => {
+  let delegationService: DelegationService;
+
+  beforeAll(async () => {
+    await Initializer.initialize();
+    const moduleRef = await Test.createTestingModule({
+      imports: [PublicAppModule],
+    }).compile();
+
+    delegationService = moduleRef.get<DelegationService>(DelegationService);
+
+  }, Constants.oneHour() * 1000);
+
+  describe('Get Delegation', () => {
+    it('should return delegation value', async () => {
+      const delegationValue = await delegationService.getDelegation();
+      expect(delegationValue).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('Get Delegation Raw', () => {
+    it('should return delegation raw', async () => {
+      const delegationValue = await delegationService.getDelegationRaw();
+      expect(delegationValue).toBeInstanceOf(Object);
+    })
+  });
+})
