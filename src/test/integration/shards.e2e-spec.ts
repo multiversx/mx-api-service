b67c6d4be5dbbe587acd29ec6cf5c5616955147b
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-17 13:10:18 +0200

    shards.e2e-spec.ts update integration test

--- src/test/integration/shards.e2e-spec.ts
+++ src/test/integration/shards.e2e-spec.ts
@@ -16,6 +16,7 @@ describe('Shard Service', () => {
     }).compile();
 
     shardService = publicAppModule.get<ShardService>(ShardService);
+
     shards = await shardService.getAllShards();
   }, Constants.oneHour() * 1000);
 
@@ -39,7 +40,25 @@ describe('Shard Service', () => {
           const shardList = await shardService.getAllShards();
           expect(shardList).toBeInstanceOf(Array);
         });
+      });
+    });
 
+    describe('Get Shards', () => {
+      it('should return a list of shards based on queryPagination size 1', async () => {
+        const shardsValue = await shardService.getShards({from: 0, size: 1});
+        expect(shardsValue).toBeInstanceOf(Array);
+      });
+      it('should return a list of shards based on queryPagination size 10', async () => {
+        const shardsValue = await shardService.getShards({from: 0, size: 10});
+        expect(shardsValue).toBeInstanceOf(Array);
+      });
+      it('all shards should have shard, validators and activeValidators', async () => {
+        const shardsValue = await shardService.getShards({from: 0, size: 1});
+        for (const shard of shardsValue) {
+          expect(shard).toHaveProperty('shard');
+          expect(shard).toHaveProperty('validators');
+          expect(shard).toHaveProperty('activeValidators');
+        }
       });
     });
   });
