c022917d3ebd34bb335c64fbd6c6e99503bbd188
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-25 12:54:38 +0200

    api.config test improvements

--- src/test/integration/api.config.e2e-spec.ts
+++ src/test/integration/api.config.e2e-spec.ts
@@ -7,6 +7,22 @@ import { PublicAppModule } from "../../public.app.module";
 describe('API Config', () => {
   let apiConfigService: ApiConfigService;
 
+  const esdtContractAddress: string = 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u';
+  const auctionContractAddress: string = 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l';
+  const stakingContractAddress: string = 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqllls0lczs7';
+  const delegationContractAddress: string = 'erd1qqqqqqqqqqqqqpgqxwakt2g7u9atsnr03gqcgmhcv38pt7mkd94q6shuwt';
+  const delegationManagerContractAddress: string = 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqylllslmq6y6';
+
+  const gatewayUrl: string = 'https://gateway.elrond.com';
+  const elasticUrl: string = 'https://index.elrond.com';
+  const mexUrl: string = 'https://mex-indexer.elrond.com';
+  const ifpsUrl: string = 'https://ipfs.io/ipfs';
+  const vmQueryUrl: string = 'https://gateway.elrond.com';
+  const providersUrl: string = 'https://internal-delegation-api.elrond.com/providers';
+  const mediaUrl: string = 'https://media.elrond.com';
+  const externalMediaUrl: string = 'https://media.elrond.com';
+  const thumbnailsNftUrl: string = 'https://media.elrond.com/nfts/thumbnail';
+
   beforeAll(async () => {
     await Initializer.initialize();
 
@@ -19,8 +35,8 @@ describe('API Config', () => {
   }, Constants.oneHour() * 1000);
 
   describe('Get Config values', () => {
-    describe('getApiUrls', () => {
 
+    describe('getApiUrls', () => {
       it('should return a list of API urls', async () => {
         const value = apiConfigService.getApiUrls();
         expect(value).toBeInstanceOf(Array);
@@ -28,60 +44,60 @@ describe('API Config', () => {
 
       it('should return gateway url', async () => {
         const value = apiConfigService.getGatewayUrl();
-        expect(value).toBe('https://gateway.elrond.com');
+        expect(value).toBe(gatewayUrl);
       });
 
       it('should return elastic url', async () => {
         const value = apiConfigService.getElasticUrl();
-        expect(value).toBe('https://index.elrond.com');
+        expect(value).toBe(elasticUrl);
       });
 
       it('should return mex url', async () => {
         const value = apiConfigService.getMexUrl();
-        expect(value).toBe('https://mex-indexer.elrond.com');
+        expect(value).toBe(mexUrl);
       });
 
       it('should return IPFS URL', async () => {
         const value = apiConfigService.getIpfsUrl();
-        expect(value).toBe('https://ipfs.io/ipfs');
+        expect(value).toBe(ifpsUrl);
       });
 
-      it('should return Esdt Contract Address', async () => {
+      it('should return ESDT contract address', async () => {
         const value = apiConfigService.getEsdtContractAddress();
-        expect(value).toBe('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u');
+        expect(value).toBe(esdtContractAddress);
       });
 
       it('should return auction contract address', async () => {
         const value = apiConfigService.getAuctionContractAddress();
-        expect(value).toBe('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l');
+        expect(value).toBe(auctionContractAddress);
       });
 
       it('should return staking contract address', async () => {
         const value = apiConfigService.getStakingContractAddress();
-        expect(value).toBe('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqllls0lczs7');
+        expect(value).toBe(stakingContractAddress);
       });
 
       it('should return delegation contract address', () => {
         const value = apiConfigService.getDelegationContractAddress();
-        expect(value).toBe('erd1qqqqqqqqqqqqqpgqxwakt2g7u9atsnr03gqcgmhcv38pt7mkd94q6shuwt');
+        expect(value).toBe(delegationContractAddress);
       });
 
-      it('should return delegation contract ShardId', () => {
+      it('should return delegation contract shard id', () => {
         const value = apiConfigService.getDelegationContractShardId();
         expect(value).toBe(2);
       });
 
       it('should return delegation manager contract address', () => {
         const value = apiConfigService.getDelegationManagerContractAddress();
-        expect(value).toBe('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqylllslmq6y6');
+        expect(value).toBe(delegationManagerContractAddress);
       });
 
       it('should return Vm Query Url', () => {
         const value = apiConfigService.getVmQueryUrl();
-        expect(value).toBe('https://gateway.elrond.com');
+        expect(value).toBe(vmQueryUrl);
       });
 
-      it('should return Redis Url', () => {
+      it('should return redis url', () => {
         const value = apiConfigService.getRedisUrl();
         expect(value).toBe('127.0.0.1');
       });
@@ -101,22 +117,22 @@ describe('API Config', () => {
         expect(value).toBe(10);
       });
 
-      it('should return process Ttl', async () => {
+      it('should return process ttl', async () => {
         const value = apiConfigService.getProcessTtl();
         expect(value).toBe(600);
       });
 
-      it('should return Axios Timeout', async () => {
+      it('should return axios timeout', async () => {
         const value = apiConfigService.getAxiosTimeout();
         expect(value).toBe(61000);
       });
 
-      it('should return Server Timeout', async () => {
+      it('should return server timeout', async () => {
         const value = apiConfigService.getServerTimeout();
         expect(value).toBe(60000);
       });
 
-      it('should return headers Timeout', async () => {
+      it('should return headers timeout', async () => {
         const value = apiConfigService.getHeadersTimeout();
         expect(value).toBe(61000);
       });
@@ -148,7 +164,7 @@ describe('API Config', () => {
 
       it('should return providers url', async () => {
         const value = apiConfigService.getProvidersUrl();
-        expect(value).toBe('https://internal-delegation-api.elrond.com/providers');
+        expect(value).toBe(providersUrl);
       });
 
       it('should return data url', async () => {
@@ -156,32 +172,32 @@ describe('API Config', () => {
         expect(value).toBeUndefined();
       });
 
-      it('should return transaction processor cron active', async () => {
+      it('should return true if transaction processor cron is active', async () => {
         const value = apiConfigService.getIsTransactionProcessorCronActive();
         expect(value).toBeTruthy();
       });
 
-      it('should return transaction processor max look behind', async () => {
+      it('should return transaction processor max number', async () => {
         const value = apiConfigService.getTransactionProcessorMaxLookBehind();
         expect(value).toBe(1000);
       });
 
-      it('should return cache warmer cron active', async () => {
+      it('should return true if cache warmer cron is active', async () => {
         const value = apiConfigService.getIsCacheWarmerCronActive();
         expect(value).toBeTruthy();
       });
 
-      it('should return fast warmer cron active', async () => {
+      it('should return false if fast warmer cron is not active', async () => {
         const value = apiConfigService.getIsFastWarmerCronActive();
         expect(value).toBeFalsy();
       });
 
-      it('should return boolean if Public API is active', async () => {
+      it('should return true if public api is active', async () => {
         const value = apiConfigService.getIsPublicApiActive();
         expect(value).toBeTruthy();
       });
 
-      it('should return boolean if Private API is active', async () => {
+      it('should return true if private api is active', async () => {
         const value = apiConfigService.getIsPrivateApiActive();
         expect(value).toBeTruthy();
       });
@@ -216,7 +232,7 @@ describe('API Config', () => {
         expect(value).toBe('api');
       });
 
-      it('should return metachain shard id', async () => {
+      it('should return meta chain shard id', async () => {
         const value = apiConfigService.getMetaChainShardId();
         expect(value).toBe(4294967295);
       });
@@ -238,7 +254,7 @@ describe('API Config', () => {
 
       it('should return media url', async () => {
         const value = apiConfigService.getMediaUrl();
-        expect(value).toBe('https://media.elrond.com');
+        expect(value).toBe(mediaUrl);
       });
 
       it('should return media internal url', async () => {
@@ -248,12 +264,12 @@ describe('API Config', () => {
 
       it('should return external media url', async () => {
         const value = apiConfigService.getExternalMediaUrl();
-        expect(value).toBe('https://media.elrond.com');
+        expect(value).toBe(externalMediaUrl);
       });
 
       it('should return nft thumbnails url', async () => {
         const value = apiConfigService.getNftThumbnailsUrl();
-        expect(value).toBe('https://media.elrond.com/nfts/thumbnail');
+        expect(value).toBe(thumbnailsNftUrl);
       });
 
       it('should return access address', async () => {
@@ -266,12 +282,12 @@ describe('API Config', () => {
         expect(value).toBe('./src/test/mocks/');
       });
 
-      it('should return no security admin', async () => {
+      it('should return null', async () => {
         const value = apiConfigService.getSecurityAdmins();
         expect(value).toBeNull();
       });
 
-      it('should return if is process nfts flac active ( default false )', async () => {
+      it('should return false with nfts flag not active', async () => {
         const value = apiConfigService.getIsProcessNftsFlagActive();
         expect(value).toBeFalsy();
       });
