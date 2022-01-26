6d3422c89efeb99822663ab3b1bfd5f8c80bef02
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-01-26 13:53:10 +0200

    fix token.asset.e2e-spec.ts test

--- src/test/integration/token.asset.e2e-spec.ts
+++ src/test/integration/token.asset.e2e-spec.ts
@@ -7,8 +7,6 @@ import {TokenAssetService} from "../../endpoints/tokens/token.asset.service";
 describe('Token Service', () => {
   let tokenAssetService: TokenAssetService;
 
-  const identifier: string = 'EGLDRIDE-7bd51a';
-
   beforeAll(async () => {
     await Initializer.initialize();
     const moduleRef = await Test.createTestingModule({
@@ -22,30 +20,6 @@ describe('Token Service', () => {
   describe('Get All Assets', () => {
     it(`should return all assets`, async () => {
       const assets = await tokenAssetService.getAllAssets();
-      expect(assets).toBeInstanceOf(Object);
-    });
-  });
-
-  describe('Get Assets', () => {
-    it(`should return tokenIdentifier assets`, async () => {
-      const assets = await tokenAssetService.getAssets(identifier);
-      expect(assets).toBeInstanceOf(Object);
-    });
-    it(`asset contain property of TokenAssets`, async () => {
-      const assets = await tokenAssetService.getAssets(identifier);
-      expect(assets).toHaveProperty('website');
-      expect(assets).toHaveProperty('description');
-      expect(assets).toHaveProperty('status');
-    });
-    it(`asset property "website" to be equal with "maiar.exchange"`, async () => {
-      const assets = await tokenAssetService.getAssets(identifier);
-      expect(assets?.website).toEqual('https://maiar.exchange');
-    });
-  });
-
-  describe('Get All Assets Raw', () => {
-    it('should return all assets raw', async () => {
-      const assets = await tokenAssetService.getAllAssetsRaw();
       expect(assets).toBeInstanceOf(Object);
     });
   });
