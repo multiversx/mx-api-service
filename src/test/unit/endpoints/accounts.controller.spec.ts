2f7e0054f2a5fd7adb6c8b9eadbfd2fb31bc5443
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-02-02 11:48:35 +0200

    new unit test for account controller

--- src/test/unit/endpoints/accounts.controller.spec.ts
+++ src/test/unit/endpoints/accounts.controller.spec.ts
@@ -34,9 +34,9 @@ describe('Accounts Endpoint',  () => {
 			.expect('header', 'content-type', /application\/json/)
 			.expect('status', 200)
 			.then(function (res: any) {
-				let accountAddress = res.json[0].address;
+				const accountAddress = res.json[0].address;
 				return frisby.get('https://api.elrond.com/accounts/' + accountAddress)
-					.expect('status', 200)
+					.expect('status', 200);
 			});
 	});
 
@@ -46,7 +46,7 @@ describe('Accounts Endpoint',  () => {
 		return frisby
 			.get(url + "/" + invalidAddress)
 			.expect('header', 'content-type', /application\/json/)
-			.expect('status', 400)
+			.expect('status', 400);
 	});
 	it('/accounts/count - should return total numbers of accounts', async () => {
 		const url = 'https://api.elrond.com/accounts/';
@@ -55,8 +55,8 @@ describe('Accounts Endpoint',  () => {
 			.get(url + 'count')
 			.then(function (res: any) {
 				const data = res['_body'];
-				expect(typeof data).toBe('string')
-				expect(data).toBeDefined()
+				expect(typeof data).toBe('string');
+				expect(data).toBeDefined();
 			});
 	});
 
@@ -71,9 +71,9 @@ describe('Accounts Endpoint',  () => {
 			.expect('header', 'content-type', /application\/json/)
 			.expect('status', 200)
 			.then(function (res: any) {
-				let accountAddress = res.json[0].address;
+				const accountAddress = res.json[0].address;
 				return frisby.get('https://api.elrond.com/accounts/' + accountAddress + "/deferred")
-					.expect('status', 200)
+					.expect('status', 200);
 			});
 	});
 
@@ -83,7 +83,7 @@ describe('Accounts Endpoint',  () => {
 		return frisby
 			.get(url + "/" + address + '/tokens')
 			.expect('header', 'content-type', /application\/json/)
-			.expect('status', 200)
+			.expect('status', 200);
 	});
 
 	it('/accounts/{address}/tokens - should return 1 token for address based on identifier', async () => {
@@ -97,9 +97,9 @@ describe('Accounts Endpoint',  () => {
 			.expect('header', 'content-type', /application\/json/)
 			.expect('status', 200)
 			.then(function(res:any){
-				let identifier = res.json[0].identifier;
-				expect(identifier).toEqual(tokenDetails.identifier)
-			})
+				const identifier = res.json[0].identifier;
+				expect(identifier).toEqual(tokenDetails.identifier);
+			});
 	});
 	it('/accounts/{address}}/tokens/count - should return tokens count for a specific address', async () => {
 		const address = tokenDetails.owner;
@@ -110,8 +110,8 @@ describe('Accounts Endpoint',  () => {
 			.expect('status', 200)
 			.then(function (res: any) {
 				const data = res['_body'];
-				expect(typeof data).toBe('string')
-				expect(data).toBeDefined()
+				expect(typeof data).toBe('string');
+				expect(data).toBeDefined();
 			});
 	});
 	it('/accounts/{address}/collections - should return all collections', async () => {
@@ -127,16 +127,16 @@ describe('Accounts Endpoint',  () => {
 			.expect('status', 200)
 			.then(function (res: any) {
 				const data = JSON.parse(res['_body']);
-				expect(data[0].collection).toEqual('IOTS1075-d46483')
-				expect(data[0].type).toEqual('NonFungibleESDT')
-				expect(data[0].name).toEqual('IoTS')
-				expect(data[0].ticker).toEqual('IOTS1075-d46483')
-				expect(data[0].canFreeze).toEqual(true)
-				expect(data[0].canWipe).toEqual(false)
-				expect(data[0].canPause).toEqual(true)
-				expect(data[0].canTransferRole).toEqual(false)
-				expect(data[0].canCreate).toEqual(true)
-				expect(data[0].canBurn).toEqual(false)
+				expect(data[0].collection).toEqual('IOTS1075-d46483');
+				expect(data[0].type).toEqual('NonFungibleESDT');
+				expect(data[0].name).toEqual('IoTS');
+				expect(data[0].ticker).toEqual('IOTS1075-d46483');
+				expect(data[0].canFreeze).toEqual(true);
+				expect(data[0].canWipe).toEqual(false);
+				expect(data[0].canPause).toEqual(true);
+				expect(data[0].canTransferRole).toEqual(false);
+				expect(data[0].canCreate).toEqual(true);
+				expect(data[0].canBurn).toEqual(false);
 			});
 	});
 
@@ -149,9 +149,9 @@ describe('Accounts Endpoint',  () => {
 			.expect('status', 200)
 			.then(function (res: any) {
 				const data = res['_body'];
-				expect(data).toBeDefined()
+				expect(data).toBeDefined();
 				expect(data.length).toBe(1);
-				expect(typeof data).toBe('string')
+				expect(typeof data).toBe('string');
 			});
 	});
 
@@ -165,16 +165,16 @@ describe('Accounts Endpoint',  () => {
 			.expect('status', 200)
 			.then(function (res: any) {
 				const data = JSON.parse(res['_body']);
-				expect(data.collection).toEqual('IOTS1075-d46483')
-				expect(data.type).toEqual('NonFungibleESDT')
-				expect(data.name).toEqual('IoTS')
-				expect(data.ticker).toEqual('IOTS1075-d46483')
-				expect(data.canFreeze).toEqual(true)
-				expect(data.canWipe).toEqual(false)
-				expect(data.canPause).toEqual(true)
-				expect(data.canTransferRole).toEqual(false)
-				expect(data.canCreate).toEqual(true)
-				expect(data.canBurn).toEqual(false)
+				expect(data.collection).toEqual('IOTS1075-d46483');
+				expect(data.type).toEqual('NonFungibleESDT');
+				expect(data.name).toEqual('IoTS');
+				expect(data.ticker).toEqual('IOTS1075-d46483');
+				expect(data.canFreeze).toEqual(true);
+				expect(data.canWipe).toEqual(false);
+				expect(data.canPause).toEqual(true);
+				expect(data.canTransferRole).toEqual(false);
+				expect(data.canCreate).toEqual(true);
+				expect(data.canBurn).toEqual(false);
 			});
 	});
 
@@ -187,11 +187,11 @@ describe('Accounts Endpoint',  () => {
 			.expect('status', 200)
 			.then(function (res: any) {
 				const data = JSON.parse(res['_body']);
-				expect(data.identifier).toEqual(tokenDetails.identifier)
-				expect(data.name).toEqual(tokenDetails.name)
-				expect(data.owner).toEqual(tokenDetails.owner)
-				expect(data.decimals).toEqual(tokenDetails.decimals)
-				expect(data.isPaused).toEqual(tokenDetails.isPaused)
+				expect(data.identifier).toEqual(tokenDetails.identifier);
+				expect(data.name).toEqual(tokenDetails.name);
+				expect(data.owner).toEqual(tokenDetails.owner);
+				expect(data.decimals).toEqual(tokenDetails.decimals);
+				expect(data.isPaused).toEqual(tokenDetails.isPaused);
 			});
 	});
 });
