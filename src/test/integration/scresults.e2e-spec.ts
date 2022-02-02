41d079bf4d3e7e572f0cfd46e90f629376d3847e
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-02-02 14:21:59 +0200

    fix scresults.e2e-spec.ts integration test

--- src/test/integration/scresults.e2e-spec.ts
+++ src/test/integration/scresults.e2e-spec.ts
@@ -1,33 +1,42 @@
-import { Test } from '@nestjs/testing';
-import { PublicAppModule } from 'src/public.app.module';
-import Initializer from './e2e-init';
-import { Constants } from 'src/utils/constants';
-import { SmartContractResultService } from 'src/endpoints/sc-results/scresult.service';
-import { SmartContractResult } from 'src/endpoints/sc-results/entities/smart.contract.result';
+import {SmartContractResultService} from "../../endpoints/sc-results/scresult.service";
+import Initializer from "./e2e-init";
+import {Test} from "@nestjs/testing";
+import {PublicAppModule} from "../../public.app.module";
+import {SmartContractResultFilter} from "../../endpoints/sc-results/entities/smart.contract.result.filter";
+import {Constants} from "../../utils/constants";
 import smartContractResults from "../mocks/smartcontract/scresults";
+import {SmartContractResult} from "../../endpoints/sc-results/entities/smart.contract.result";
 
 describe('Scresults Service', () => {
   let scresultsService: SmartContractResultService;
   let accountAddress: string;
   let scHash: string;
+
   beforeAll(async () => {
     await Initializer.initialize();
     const moduleRef = await Test.createTestingModule({
       imports: [PublicAppModule],
     }).compile();
+
     scresultsService = moduleRef.get<SmartContractResultService>(SmartContractResultService);
-    const scresults = await scresultsService.getScResults({ from: 0, size: 1 });
+
+    const scresults = await scresultsService.getScResults(
+      {from: 0, size: 1},
+      new SmartContractResultFilter());
+
     expect(scresults).toHaveLength(1);
+
     const scResult = scresults[0];
     accountAddress = scResult.sender;
     scHash = scresults[0].hash;
   }, Constants.oneHour() * 1000);
+
   describe('Scresults list', () => {
     it('scresults should have hash, nonce and timestamp', async () => {
-      const results = await scresultsService.getScResults({
-        from: 0,
-        size: 1,
-      });
+      const results = await scresultsService.getScResults(
+        {from: 0, size: 1},
+        new SmartContractResultFilter());
+
       expect(results).toEqual(
         expect.arrayContaining([
           expect.objectContaining({hash: smartContractResults[0].hash}),
@@ -38,9 +47,11 @@ describe('Scresults Service', () => {
       const results = await scresultsService.getScResults({
         from: 0,
         size: 25,
-      });
+      }, new SmartContractResultFilter());
+
       expect(results).toBeInstanceOf(Array);
       expect(results).toHaveLength(25);
+
       for (const result of results) {
         expect(result).toHaveStructure(
           Object.keys(new SmartContractResult()),
@@ -51,9 +62,11 @@ describe('Scresults Service', () => {
       const results = await scresultsService.getScResults({
         from: 0,
         size: 50,
-      });
+      }, new SmartContractResultFilter());
+
       expect(results).toBeInstanceOf(Array);
       expect(results).toHaveLength(50);
+
       for (const result of results) {
         expect(result).toHaveStructure(
           Object.keys(new SmartContractResult()),
@@ -65,9 +78,10 @@ describe('Scresults Service', () => {
     it(`should return a list of scresults for an account`, async () => {
       const results = await scresultsService.getAccountScResults(
         accountAddress,
-        { from: 0, size: 25 },
+        {from: 0, size: 25},
       );
       expect(results).toBeInstanceOf(Array);
+
       for (const result of results) {
         expect(result).toHaveStructure(
           Object.keys(new SmartContractResult()),
@@ -76,28 +90,29 @@ describe('Scresults Service', () => {
       }
     });
   });
+
   describe('Scresults count', () => {
     it(`should return smart contract count`, async () => {
-      const count =await scresultsService.getScResultsCount();
+      const count = await scresultsService.getScResultsCount();
       expect(typeof count).toBe('number');
     });
   });
-  describe('Specific scresult', () => {
-    describe('Scresult Details', () => {
-      it(`should return a detailed scresult with hash`, async () => {
-        const result = await scresultsService.getScResult(scHash);
-        expect(result).toBeDefined();
-        expect(result).toBeInstanceOf(Object);
-        expect(result?.hash).toStrictEqual(scHash);
-      });
 
-      it(`should throw 'Smart contract result not found' error`, async () => {
-        expect(
-          await scresultsService.getScResult(scHash + 'a'),
-        ).toBeUndefined();
-      });
+  describe('Scresult Details', () => {
+    it(`should return a detailed scresult with hash`, async () => {
+      const result = await scresultsService.getScResult(scHash);
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(Object);
+      expect(result?.hash).toStrictEqual(scHash);
+    });
+
+    it(`should throw 'Smart contract result not found' error`, async () => {
+      expect(
+        await scresultsService.getScResult(scHash + 'a'),
+      ).toBeUndefined();
     });
   });
+
   describe('Get Results of a smart contract count', () => {
     it('should return sc results count', async () => {
       const count = await scresultsService.getAccountScResultsCount(accountAddress);
