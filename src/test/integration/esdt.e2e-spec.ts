c099cdcb00b30f2f7ece3ec72a724d887b1c3008
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-02-28 11:40:55 +0200

    update esdt.e2e-spec.ts integration test

--- src/test/integration/esdt.e2e-spec.ts
+++ src/test/integration/esdt.e2e-spec.ts
@@ -1,23 +1,18 @@
-import { EsdtSupply } from '../../endpoints/esdt/entities/esdt.supply';
+import { EsdtAddressService } from 'src/endpoints/esdt/esdt.address.service';
 import { Test } from "@nestjs/testing";
 import { EsdtService } from "../../endpoints/esdt/esdt.service";
-import tokenExample from "../data/esdt/token/token.example";
-import { TokenAddressRoles } from "src/endpoints/tokens/entities/token.address.roles";
-import { NftFilter } from "src/endpoints/nfts/entities/nft.filter";
-import { EsdtAddressService } from "src/endpoints/esdt/esdt.address.service";
-import { EsdtDataSource } from "src/endpoints/esdt/entities/esdt.data.source";
-import { NftCollection } from "src/endpoints/collections/entities/nft.collection";
-import { NftCollectionAccount } from "src/endpoints/collections/entities/nft.collection.account";
-import { CollectionFilter } from "src/endpoints/collections/entities/collection.filter";
-import '../../utils/extensions/jest.extensions';
 import { PublicAppModule } from "src/public.app.module";
-import { NftType } from 'src/endpoints/nfts/entities/nft.type';
+import { CachingService } from "src/common/caching/caching.service";
+import { TokenAddressRoles } from "src/endpoints/tokens/entities/token.address.roles";
+import { EsdtSupply } from "src/endpoints/esdt/entities/esdt.supply";
+import { NftFilter } from "src/endpoints/nfts/entities/nft.filter";
+import { NftType } from "src/endpoints/nfts/entities/nft.type";
+import { EsdtDataSource } from 'src/endpoints/esdt/entities/esdt.data.source';
+import '../../utils/extensions/jest.extensions';
 
 describe('ESDT Service', () => {
   let esdtService: EsdtService;
   let esdtAddressService: EsdtAddressService;
-
-  const egldMexTokenIdentifier: string = 'EGLDMEX-0be9e5';
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -26,20 +21,10 @@ describe('ESDT Service', () => {
 
     esdtService = moduleRef.get<EsdtService>(EsdtService);
     esdtAddressService = moduleRef.get<EsdtAddressService>(EsdtAddressService);
+
   });
 
-  beforeEach(() => { jest.restoreAllMocks(); });
-
-  describe('Get Esdts For Address', () => {
-    it('gateway & elastic esdts of address should be the same', async () => {
-      const esdtAddress: string = 'erd1qqqqqqqqqqqqqpgqhe8t5jewej70zupmh44jurgn29psua5l2jps3ntjj3';
-
-      const gatewayNfts = await esdtAddressService.getEsdtsForAddress(esdtAddress, new NftFilter(), { from: 0, size: 25 }, EsdtDataSource.gateway);
-      const elasticNfts = await esdtAddressService.getEsdtsForAddress(esdtAddress, new NftFilter(), { from: 0, size: 25 }, EsdtDataSource.elastic);
-
-      expect(gatewayNfts).toStrictEqual(elasticNfts);
-    });
-
+  describe("Get ESDT For Address", () => {
     it("should return esdts of type MetaESDT", async () => {
       const address: string = "erd1k3wtee6vzk47halxwm7qud3mrdjrlw4fyjvuhamtzy4hjhe6htcsv9jcgs";
       const filter = new NftFilter();
@@ -94,145 +79,118 @@ describe('ESDT Service', () => {
         expect(result.type).toStrictEqual("SemiFungibleESDT");
       }
     });
+
+    it('gateway & elastic esdts of address should be the same', async () => {
+      const esdtAddress: string = 'erd1qqqqqqqqqqqqqpgqhe8t5jewej70zupmh44jurgn29psua5l2jps3ntjj3';
+
+      const gatewayNfts = await esdtAddressService.getEsdtsForAddress(esdtAddress, new NftFilter(), { from: 0, size: 25 }, EsdtDataSource.gateway);
+      const elasticNfts = await esdtAddressService.getEsdtsForAddress(esdtAddress, new NftFilter(), { from: 0, size: 25 }, EsdtDataSource.elastic);
+
+      expect(gatewayNfts).toStrictEqual(elasticNfts);
+    });
   });
 
-  describe('Get Esdt Collections For Address', () => {
-    it('gateway esdt collections should have property canCreate & canBurn', async () => {
-      const esdtAddress: string = 'erd1zqhn3w4w7uamw6eelrqcjjm8ac732s2z69hgkduldm6fapa90drswejs34';
+  describe("Get ESDT Tokens Properties", () => {
+    it("should return all ESDT tokens", async () => {
+      const results = await esdtService.getAllEsdtTokens();
 
-      const gatewayNfts: NftCollectionAccount[] | NftCollection[] = await esdtAddressService.getEsdtCollectionsForAddress(esdtAddress, new CollectionFilter(), { from: 0, size: 25 }, EsdtDataSource.gateway);
-
-      for (const gatewayNft of gatewayNfts) {
-        expect(gatewayNft).toHaveProperty('canCreate');
-        expect(gatewayNft).toHaveProperty('canBurn');
+      for (const result of results) {
+        expect(result.hasOwnProperty("identifier")).toBeTruthy();
+        expect(result.hasOwnProperty("name")).toBeTruthy();
+        expect(result.hasOwnProperty("ticker")).toBeTruthy();
+        expect(result.hasOwnProperty("owner")).toBeTruthy();
+        expect(result.hasOwnProperty("minted")).toBeTruthy();
+        expect(result.hasOwnProperty("burnt")).toBeTruthy();
+        expect(result.hasOwnProperty("isPaused")).toBeTruthy();
+        expect(result.hasOwnProperty("canUpgrade")).toBeTruthy();
+        expect(result.hasOwnProperty("canMint")).toBeTruthy();
+        expect(result.hasOwnProperty("canBurn")).toBeTruthy();
+        expect(result.hasOwnProperty("canChangeOwner")).toBeTruthy();
+        expect(result.hasOwnProperty("canPause")).toBeTruthy();
+        expect(result.hasOwnProperty("canFreeze")).toBeTruthy();
+        expect(result.hasOwnProperty("canWipe")).toBeTruthy();
       }
     });
   });
 
-  describe('Get Esdt Collections For Address', () => {
-    it('elastic esdt collections should have property of NftCollection', async () => {
-      const esdtAddress: string = 'erd1zqhn3w4w7uamw6eelrqcjjm8ac732s2z69hgkduldm6fapa90drswejs34';
+  describe("Get ESDT Token Properties", () => {
+    it("should return the properties of the token ( tiker property should not be present )", async () => {
+      jest
+        .spyOn(CachingService.prototype, 'getOrSetCache')
+        // eslint-disable-next-line require-await
+        .mockImplementation(jest.fn(async (_key: string, promise: any) => promise()));
 
-      const gatewayNfts: NftCollectionAccount[] | NftCollection[] = await esdtAddressService.getEsdtCollectionsForAddress(esdtAddress, new CollectionFilter(), { from: 0, size: 25 }, EsdtDataSource.gateway);
+      const tokenIdentifier: string = "EGLDMEX-0be9e5";
+      const results = await esdtService.getEsdtTokenProperties(tokenIdentifier);
 
-      for (const gatewayNft of gatewayNfts) {
-        expect(gatewayNft.hasOwnProperty('collection')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('type')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('name')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('ticker')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('canFreeze')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('canWipe')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('canPause')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('canTransferRole')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('assets')).toBe(true);
-        expect(gatewayNft.hasOwnProperty('roles')).toBe(true);
-      }
-    });
-  });
-
-  describe('Get All Esdts Tokens', () => {
-    it('should return all esdts tokens', async () => {
-      const tokens = await esdtService.getAllEsdtTokens();
-
-      if (!tokens) {
-        throw new Error('Token properties are not defined');
+      if (!results) {
+        throw new Error("Properties are not defined");
       }
 
-      for (const token of tokens) {
-        expect(token).toBeInstanceOf(Object);
-      }
-    });
-  });
-
-  describe('Get Esdt Token Properties', () => {
-    it('should be return token properties', async () => {
-      const properties = await esdtService.getEsdtTokenProperties(tokenExample.identifier);
-      if (!properties) {
-        throw new Error('Properties not defined');
-      }
-
-      expect(properties.name).toEqual(tokenExample.name);
-      expect(properties.decimals).toEqual(tokenExample.decimals);
-      expect(properties.canUpgrade).toEqual(tokenExample.canUpgrade);
-      expect(properties.canMint).toEqual(tokenExample.canMint);
-      expect(properties.canBurn).toEqual(tokenExample.canBurn);
-    });
-  });
-
-  describe('Get Token Supply', () => {
-    it('should return esdt token supply', async () => {
-      const supply = await esdtService.getTokenSupply(tokenExample.identifier);
-      if (!supply) {
-        throw new Error('Token supply must be defined');
-      }
-
-      expect(supply).toHaveStructure(Object.keys(new EsdtSupply()));
-    });
-  });
-
-  describe('Get Esdt Token Properties Raw', () => {
-    it('should return token properties', async () => {
-      const properties = await esdtService.getEsdtTokenPropertiesRaw(tokenExample.identifier);
-      expect(properties).toBeDefined();
+      expect(results.hasOwnProperty("identifier")).toBeTruthy();
+      expect(results.hasOwnProperty("name")).toBeTruthy();
+      expect(results.hasOwnProperty("owner")).toBeTruthy();
+      expect(results.hasOwnProperty("minted")).toBeTruthy();
+      expect(results.hasOwnProperty("burnt")).toBeTruthy();
+      expect(results.hasOwnProperty("isPaused")).toBeTruthy();
+      expect(results.hasOwnProperty("canUpgrade")).toBeTruthy();
+      expect(results.hasOwnProperty("canMint")).toBeTruthy();
+      expect(results.hasOwnProperty("canBurn")).toBeTruthy();
+      expect(results.hasOwnProperty("canChangeOwner")).toBeTruthy();
+      expect(results.hasOwnProperty("canPause")).toBeTruthy();
+      expect(results.hasOwnProperty("canFreeze")).toBeTruthy();
+      expect(results.hasOwnProperty("canWipe")).toBeTruthy();
     });
   });
 
   describe('Get Esdt Addresses Roles', () => {
     it('return addresses role', async () => {
-      const roles = await esdtService.getEsdtAddressesRoles(egldMexTokenIdentifier);
-      if (!roles) {
-        throw new Error('Roles must be defined');
+      const tokenIdentifier: string = "EGLDMEX-0be9e5";
+      const results = await esdtService.getEsdtAddressesRoles(tokenIdentifier);
+
+      if (!results) {
+        throw new Error('Roles properties must be defined');
       }
 
-      expect(roles).toBeInstanceOf(Array);
-
-      for (const role of roles) {
-        expect(role).toHaveStructure(Object.keys(new TokenAddressRoles()));
+      for (const result of results) {
+        expect(result).toHaveStructure(Object.keys(new TokenAddressRoles()));
       }
-    });
-  });
-
-  describe('Get Token Account Count', () => {
-    it('return token account count', async () => {
-      const count = await esdtService.getTokenAccountsCount(tokenExample.identifier);
-      return expect(typeof count).toBe('number');
     });
   });
 
   describe('Get Esdt Addresses Roles Raw', () => {
     it('EGLDMEX token should have valid roles', async () => {
-      const roles = await esdtService.getEsdtAddressesRolesRaw(egldMexTokenIdentifier);
-      if (!roles) {
-        throw new Error('Roles must be defined');
+      const tokenIdentifier: string = "EGLDMEX-0be9e5";
+      const results = await esdtService.getEsdtAddressesRolesRaw(tokenIdentifier);
+
+      if (!results) {
+        throw new Error('Properties are not defined');
       }
 
-      expect(roles).toBeInstanceOf(Array);
-
-      for (const role of roles) {
-        expect(role).toHaveStructure(Object.keys(new TokenAddressRoles()));
-      }
-    });
-
-    it('Token example should have valid roles', async () => {
-      const roles = await esdtService.getEsdtAddressesRolesRaw(tokenExample.identifier);
-      if (!roles) {
-        throw new Error('Roles must be defined');
-      }
-
-      expect(roles).toBeInstanceOf(Array);
-
-      for (const role of roles) {
-        expect(role).toHaveStructure(Object.keys(new TokenAddressRoles()));
+      for (const result of results) {
+        expect(result).toHaveStructure(Object.keys(new TokenAddressRoles()));
       }
     });
   });
 
-  describe("getLockedSupplyRaw", () => {
-    it("should return locked supply based on identifier", async () => {
-      const esdtIdentifier: string = "RIDE-7d18e9";
-      const supply = await esdtService.getLockedSupplyRaw(esdtIdentifier);
+  describe('Get Token Supply', () => {
+    it('should return esdt token supply', async () => {
+      const tokenIdentifier: string = "EGLDMEX-0be9e5";
+      const results = await esdtService.getTokenSupply(tokenIdentifier);
 
-      expect(typeof supply).toBe('string');
+      if (!results) {
+        throw new Error('Properties are not defined');
+      }
+      expect(results).toHaveStructure(Object.keys(new EsdtSupply()));
+    });
+  });
+
+  describe('Get Token Account Count', () => {
+    it('return token account count', async () => {
+      const tokenIdentifier: string = "EGLDMEX-0be9e5";
+      const result = await esdtService.getTokenAccountsCount(tokenIdentifier);
+
+      expect(typeof result).toStrictEqual('number');
     });
   });
 });
