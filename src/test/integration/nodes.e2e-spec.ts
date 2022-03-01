ff4a7803f41584beb2564f655403bb3f8086e966
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-03-01 11:12:05 +0200

    revert node test

--- src/test/integration/nodes.e2e-spec.ts
+++ src/test/integration/nodes.e2e-spec.ts
@@ -211,7 +211,7 @@ describe('Node Service', () => {
       const filteredNode = await nodeService.getNodes({ from: 0, size: 1 }, nodeFilter);
 
       for (const node of filteredNode) {
-        expect(node.issues).toEqual([]);
+        expect(node.issues).toBeUndefined();
       }
     });
 
