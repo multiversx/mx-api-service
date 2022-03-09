70ad4b32cc67eccfa7b92ac99766d39f1c581aeb
Traian Anghel <tanghel@live.com>
2022-03-09 11:41:00 +0200

    fixed dependencies for cache warmer

--- src/crons/cache.warmer/cache.warmer.module.ts
+++ src/crons/cache.warmer/cache.warmer.module.ts
@@ -4,6 +4,8 @@ import { CacheWarmerService } from './cache.warmer.service';
 import { EndpointsServicesModule } from '../../endpoints/endpoints.services.module';
 import { PluginModule } from 'src/plugins/plugin.module';
 import { KeybaseModule } from 'src/common/keybase/keybase.module';
+import { ApiConfigService } from 'src/common/api-config/api.config.service';
+import { ClientOptions, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -13,6 +15,25 @@ import { KeybaseModule } from 'src/common/keybase/keybase.module';
     KeybaseModule,
   ],
   providers: [
+    {
+      provide: 'PUBSUB_SERVICE',
+      useFactory: (apiConfigService: ApiConfigService) => {
+        const clientOptions: ClientOptions = {
+          transport: Transport.REDIS,
+          options: {
+            url: `redis://${apiConfigService.getRedisUrl()}:6379`,
+            retryDelay: 1000,
+            retryAttempts: 10,
+            retry_strategy: function (_: any) {
+              return 1000;
+            },
+          },
+        };
+
+        return ClientProxyFactory.create(clientOptions);
+      },
+      inject: [ApiConfigService],
+    },
     CacheWarmerService,
   ],
 })
