44616e5c261960c52049dda1a55a13492e63f7d3
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-03-15 16:10:42 +0200

    integration test: new identities.controller.e2e-spec.ts test

---
+++ src/test/integration/controllers/identities.controller.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { PublicAppModule } from 'src/public.app.module';
+import request = require('supertest');
+
+describe("Identities Controller", () => {
+  let app: INestApplication;
+  const route: string = "/identities";
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [PublicAppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it("/identities - should return 200 status code and a list of identities", async () => {
+    await request(app.getHttpServer())
+      .get(route)
+      .expect(200)
+      .then(res => {
+        console.log(res.body);
+      });
+  });
+
+  it("/identities/{identifier} - should return 200 status code and identity details", async () => {
+    const identifier: string = "justminingfr";
+
+    await request(app.getHttpServer())
+      .get(route + "/" + identifier)
+      .expect(200)
+      .then(res => {
+        console.log(res.body);
+      });
+  });
+
+  it("/identities/{identifier} - should return 404 status code Error Code: Identity not found", async () => {
+    const identifier: string = "justminingfrT";
+
+    await request(app.getHttpServer())
+      .get(route + "/" + identifier)
+      .expect(404)
+      .then(res => {
+        expect(res.body.message).toEqual("Identity not found");
+      });
+  });
+});
