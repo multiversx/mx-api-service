df72cf67e71894d12fad9171d79712cff4c14238
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-04-10 19:38:16 +0300

    e2e: update waitingList-e2e-test

--- src/test/integration/waiting.list.e2e-spec.ts
+++ src/test/integration/waiting.list.e2e-spec.ts
@@ -1,19 +1,17 @@
 import { WaitingListService } from "../../endpoints/waiting-list/waiting.list.service";
-import { Test } from "@nestjs/testing";
 import { WaitingList } from "../../endpoints/waiting-list/entities/waiting.list";
 import { WaitingListModule } from "src/endpoints/waiting-list/waiting.list.module";
 import { CachingService } from "src/common/caching/caching.service";
+import { Test } from "@nestjs/testing";
 import '../../utils/extensions/jest.extensions';
 
 describe('WaitingListService', () => {
   let waitingListService: WaitingListService;
-
   const waitingList = [{
-
     address: 'erd1wcat0qk32u5xquuds9etauayqs29tqrzs5x3plc6djy54rmatdpsejg6qx',
     value: '270000000000000000001',
     nonce: 8859612,
-    rank: 54,
+    rank: 60,
   },
   ];
 
@@ -45,18 +43,11 @@ describe('WaitingListService', () => {
     });
   });
 
-
   describe('getWaitingListForAddress', () => {
     it('should return a list of waitings for a specified address ', async () => {
-      jest
-        .spyOn(CachingService.prototype, 'getOrSetCache')
+      jest.spyOn(CachingService.prototype, 'getOrSetCache')
         // eslint-disable-next-line require-await
-        .mockImplementation(jest.fn(async (_key: string, promise: any) => promise()));
-
-      jest
-        .spyOn(WaitingListService.prototype, 'getWaitingList')
-        // eslint-disable-next-line require-await
-        .mockImplementation(jest.fn(async () => waitingList));
+        .mockImplementation(async () => waitingList);
 
       const results = await waitingListService.getWaitingListForAddress("erd1wcat0qk32u5xquuds9etauayqs29tqrzs5x3plc6djy54rmatdpsejg6qx");
 
@@ -64,9 +55,9 @@ describe('WaitingListService', () => {
       expect(results).toEqual(expect.arrayContaining([
         expect.objectContaining({
           address: "erd1wcat0qk32u5xquuds9etauayqs29tqrzs5x3plc6djy54rmatdpsejg6qx",
-          value: "27000000000000000000",
+          value: '270000000000000000001',
           nonce: 8859612,
-          rank: 54,
+          rank: 60,
         }),
       ]));
     });
@@ -80,7 +71,6 @@ describe('WaitingListService', () => {
         .mockImplementation(jest.fn(async (_key: string, _promise: any) => waitingList));
 
       const result = await waitingListService.getWaitingListCount();
-
       expect(result).toStrictEqual(1);
     });
   });
