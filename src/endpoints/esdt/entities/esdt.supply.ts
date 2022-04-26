6340567a1dec0b95df46ef94665ce12fb8080e7e
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-04-26 23:20:42 +0300

    swagger-docs: add esdtSupply ApiProperty()

--- src/endpoints/esdt/entities/esdt.supply.ts
+++ src/endpoints/esdt/entities/esdt.supply.ts
@@ -1,9 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class EsdtSupply {
+  @ApiProperty()
   totalSupply: string = '0';
+
+  @ApiProperty()
   circulatingSupply: string = '0';
+
+  @ApiProperty()
   minted: string = '0';
+
+  @ApiProperty()
   burned: string = '0';
+
+  @ApiProperty()
   initialMinted: string = '0';
 }
