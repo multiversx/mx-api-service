0d2845883238ea8a11389b4decc4ddc200c81c75
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-04-27 01:28:37 +0300

    swagger-docs: /unbond-period

--- src/endpoints/keys/entities/key.unbond.period.ts
+++ src/endpoints/keys/entities/key.unbond.period.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class KeyUnbondPeriod {
+  @ApiProperty()
   remainingUnBondPeriod: number = 0;
 }
--- src/endpoints/keys/keys.controller.ts
+++ src/endpoints/keys/keys.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, HttpException, HttpStatus, Param } from "@nestjs/common";
-import { ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { ParseBlsHashPipe } from "src/utils/pipes/parse.bls.hash.pipe";
 import { KeyUnbondPeriod } from "./entities/key.unbond.period";
 import { KeysService } from "./keys.service";
@@ -7,23 +7,24 @@ import { KeysService } from "./keys.service";
 @Controller()
 @ApiTags('keys')
 export class KeysController {
-	constructor(private readonly keysService: KeysService) { }
+  constructor(private readonly keysService: KeysService) { }
 
-	@Get("/keys/:key/unbond-period")
-	@ApiResponse({
-		status: 200,
-		description: 'The unbonding period remaining for the given BLS key',
-		type: KeyUnbondPeriod,
-	})
-	@ApiQuery({ name: 'key', description: 'The BLS key of the node', required: true })
-	async getKeyUnbondPeriod(
-		@Param('key', ParseBlsHashPipe) key: string
-	): Promise<KeyUnbondPeriod> {
-		const result = await this.keysService.getKeyUnbondPeriod(key);
-		if (!result) {
-			throw new HttpException('Key not found', HttpStatus.NOT_FOUND);
-		}
+  @Get("/keys/:key/unbond-period")
+  @ApiOperation({ summary: 'Unbonding details', description: 'Returns unbonding period remaining for a given bls key' })
+  @ApiResponse({
+    status: 200,
+    description: 'The unbonding period remaining for the given BLS key',
+    type: KeyUnbondPeriod,
+  })
+  @ApiQuery({ name: 'key', description: 'The BLS key of the node', required: true })
+  async getKeyUnbondPeriod(
+    @Param('key', ParseBlsHashPipe) key: string
+  ): Promise<KeyUnbondPeriod> {
+    const result = await this.keysService.getKeyUnbondPeriod(key);
+    if (!result) {
+      throw new HttpException('Key not found', HttpStatus.NOT_FOUND);
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
