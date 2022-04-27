259b0196171b236229ea1e9bc5aed53e16c0fef2
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-04-27 17:06:39 +0300

    use local variables

--- src/common/graphql/graphql.service.ts
+++ src/common/graphql/graphql.service.ts
@@ -5,21 +5,20 @@ import { GraphQLClient } from 'graphql-request';
 @Injectable()
 export class GraphQlService {
   private readonly logger: Logger;
-  private readonly MAIAR_EXCHANGE_URL: string;
-  private readonly graphqlClient: GraphQLClient;
 
   constructor(
     private readonly apiConfigService: ApiConfigService
   ) {
     this.logger = new Logger(GraphQlService.name);
-    this.MAIAR_EXCHANGE_URL = this.apiConfigService.getMaiarExchangeUrlMandatory();
-
-    this.graphqlClient = new GraphQLClient(this.MAIAR_EXCHANGE_URL);
   }
 
   async getData(query: string, variables: any): Promise<any> {
+    const MAIAR_EXCHANGE_URL = this.apiConfigService.getMaiarExchangeUrlMandatory();
+
+    const graphqlClient = new GraphQLClient(MAIAR_EXCHANGE_URL);
+
     try {
-      const data = await this.graphqlClient.request(query, variables);
+      const data = await graphqlClient.request(query, variables);
 
       if (!data) {
         return null;
