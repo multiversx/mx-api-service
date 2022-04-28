8154ae2dd2476dcb9561573b8cfcfaf9442aaa03
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2022-04-28 15:37:40 +0300

    swagger-docs: update /stake

--- src/endpoints/stake/entities/global.stake.ts
+++ src/endpoints/stake/entities/global.stake.ts
@@ -1,12 +1,16 @@
+import { SwaggerUtils } from 'src/utils/swagger.utils';
 import { ApiProperty } from "@nestjs/swagger";
 
 export class GlobalStake {
-  @ApiProperty({type: Number, default: 0})
+  @ApiProperty({type: Number, default: 3200})
   totalValidators: number = 0;
 
-  @ApiProperty({type: Number, default: 0})
+  @ApiProperty({type: Number, default: 3199})
   activeValidators: number = 0;
 
-  @ApiProperty({type: Number, default: 0})
+  @ApiProperty({type: Number, default: 2})
   queueSize: number = 0;
+
+  @ApiProperty(SwaggerUtils.amountPropertyOptions())
+  totalStaked: number = 0;
 }
--- src/endpoints/stake/stake.controller.ts
+++ src/endpoints/stake/stake.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { GlobalStake } from "./entities/global.stake";
 import { StakeService } from "./stake.service";
 
 @Controller()
@@ -10,10 +11,13 @@ export class StakeController {
   ) { }
 
   @Get('/stake')
-  @ApiOperation({ summary: 'Stake details', description: 'Returns stake informations as well as total/active validators and nodes queue size ' })
+  @ApiOperation({
+    summary: 'Stake',
+    description: 'Returns stake details as well as total active validators and nodes queue size ',
+  })
   @ApiResponse({
     status: 200,
-    description: 'Stake details',
+    type: GlobalStake,
   })
   async getGlobalStake() {
     return await this.stakeService.getGlobalStake();
