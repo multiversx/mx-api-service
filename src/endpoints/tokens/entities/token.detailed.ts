53e8f7b654e7061d3b684f778b66a1b17c9a9502
Traian Anghel <tanghel@live.com>
2022-04-29 15:50:51 +0300

    Remove fields (#603)
    * token list and details fixes
    
    - do not overwrite cacheable token with balance (since it's cached by reference)
    - set minted / burnt / initialMinted only if returned from gateway
    
    * consistent token supply information in token details
    
    * attempt to fix tests
    
    * removed unused import
    
    * removed minted & burnt from check
    
    * removed minted & burnt from mocked response
    
    * removed fields from token detailed

--- src/endpoints/tokens/entities/token.detailed.ts
+++ src/endpoints/tokens/entities/token.detailed.ts
@@ -4,27 +4,6 @@ import { TokenRoles } from "./token.roles";
 
 export class TokenDetailed extends Token {
   @ApiProperty()
-  canUpgrade: boolean = false;
-
-  @ApiProperty()
-  canMint: boolean = false;
-
-  @ApiProperty()
-  canBurn: boolean = false;
-
-  @ApiProperty()
-  canChangeOwner: boolean = false;
-
-  @ApiProperty()
-  canPause: boolean = false;
-
-  @ApiProperty()
-  canFreeze: boolean = false;
-
-  @ApiProperty()
-  canWipe: boolean = false;
-
-  @ApiProperty()
   supply: string | undefined = undefined;
 
   @ApiProperty()
