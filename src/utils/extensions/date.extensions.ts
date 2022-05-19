192a529026481639697cd8d396ba43bcc28b487f
Traian Anghel <tanghel@live.com>
2022-05-19 13:04:51 +0300

    do not fetch price for transaction older than 10 sept 2020

--- src/utils/extensions/date.extensions.ts
+++ src/utils/extensions/date.extensions.ts
@@ -6,8 +6,17 @@ Date.prototype.toISODateString = function (): string {
   return this.toISOString().slice(0, 10);
 };
 
+Date.prototype.isGreaterThan = function (other: Date): boolean {
+  return this.getTime() > other.getTime();
+};
+
+Date.prototype.isLessThan = function (other: Date): boolean {
+  return this.getTime() < other.getTime();
+};
 
 declare interface Date {
   toISODateString(): string;
   isToday(): boolean;
+  isGreaterThan(other: Date): boolean;
+  isLessThan(other: Date): boolean;
 }
