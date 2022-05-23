0d7b505ecd0eec7bc2d791e0bb5f87291fed28ea
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-05-23 13:39:50 +0300

    swagger-docs: update provider endpoint responses

--- src/endpoints/providers/provider.controller.ts
+++ src/endpoints/providers/provider.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, HttpException, HttpStatus, Param, Query } from "@nestjs/common";
-import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { ProviderService } from "./provider.service";
 import { Provider } from "./entities/provider";
 import { ParseAddressPipe } from "src/utils/pipes/parse.address.pipe";
@@ -11,11 +11,8 @@ export class ProviderController {
 
   @Get("/providers")
   @ApiOperation({ summary: 'Providers', description: 'Returns a list of all providers' })
-  @ApiResponse({
-    status: 200,
-    isArray: true,
-    type: Provider,
-  })
+  @ApiOkResponse({ type: [Provider] })
+
   @ApiQuery({ name: 'identity', description: 'Search by identity', required: false })
   async getProviders(
     @Query('identity') identity: string | undefined,
@@ -25,14 +22,9 @@ export class ProviderController {
 
   @Get('/providers/:address')
   @ApiOperation({ summary: 'Provider', description: 'Returns provider details for a given address' })
-  @ApiResponse({
-    status: 200,
-    type: Provider,
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Provider not found',
-  })
+  @ApiOkResponse({ type: Provider })
+  @ApiNotFoundResponse({ description: 'Provider not found' })
+
   async getProvider(@Param('address', ParseAddressPipe) address: string): Promise<Provider> {
     const provider = await this.providerService.getProvider(address);
     if (provider === undefined) {
