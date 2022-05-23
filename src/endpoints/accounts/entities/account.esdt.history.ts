285151e0e2e6bc60b5c78b1ce81871471db329d1
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-05-23 12:46:25 +0300

    swagger-docs: update block endpoint responses

--- src/endpoints/accounts/entities/account.esdt.history.ts
+++ src/endpoints/accounts/entities/account.esdt.history.ts
@@ -2,6 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 import { AccountHistory } from "./account.history";
 
 export class AccountEsdtHistory extends AccountHistory {
-    @ApiProperty({ type: String })
+    @ApiProperty({ type: String, example: 'WEGLD-bd4d79' })
     token: string = '';
 }
