58eb2d09992f739984d39418f43fe1c49de59df4
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-05-31 15:24:22 +0300

    update nft.tag.e2e-spec.ts test

--- src/test/integration/nft.tag.e2e-spec.ts
+++ src/test/integration/nft.tag.e2e-spec.ts
@@ -16,27 +16,29 @@ describe('NFT Tag Service', () => {
 
   });
 
-  it(`should return list of tags for 1 nft`, async () => {
-    const tags = await tagService.getNftTags({ from: 0, size: 1 });
+  it(`should return list of tags for 2 nft`, async () => {
+    const tags = await tagService.getNftTags({ from: 0, size: 2 });
 
     for (const tag of tags) {
       expect(tag).toHaveStructure(Object.keys(new Tag()));
     }
+    expect(tags).toHaveLength(2);
   });
 
-  it(`should return a list of tags raw for 1 nft`, async () => {
-    const tagsRaw = await tagService.getNftTagsRaw({ from: 0, size: 1 });
-    expect(tagsRaw.length).toBe(1);
+  it(`should return a list of tags raw for 2 nft`, async () => {
+    const tagsRaw = await tagService.getNftTagsRaw({ from: 0, size: 2 });
 
     for (const tag of tagsRaw) {
       expect(tag).toHaveStructure(Object.keys(new Tag()));
     }
+    expect(tagsRaw).toHaveLength(2);
   });
 
   describe('Get Nft Tag', () => {
-    it('should return tag', async () => {
-      const tag = await tagService.getNftTag('ZWxyb25k');
-      expect(tag).toHaveStructure(Object.keys(new Tag()));
+    it('should return a specific tag', async () => {
+      const tag = await tagService.getNftTag('elrond');
+
+      expect(tag.tag).toStrictEqual('elrond');
     });
   });
 });
