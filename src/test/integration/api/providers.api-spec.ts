beb7d09bf48e15ec4b05dbc29318c9bc92250bf2
Traian Anghel <tanghel@live.com>
2022-05-31 16:52:53 +0300

    api tests

---
+++ src/test/integration/api/providers.api-spec.ts
@@ -0,0 +1,30 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { CleanupInterceptor } from 'src/interceptors/cleanup.interceptor';
+import { FieldsInterceptor } from 'src/interceptors/fields.interceptor';
+import { PublicAppModule } from 'src/public.app.module';
+import { ApiChecker } from 'src/utils/api.checker.utils';
+
+describe("API Testing", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [PublicAppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalInterceptors(
+      new FieldsInterceptor(),
+      new CleanupInterceptor(),
+    );
+    await app.init();
+  });
+
+  it("/providers", async () => {
+    const checker = new ApiChecker('providers', app.getHttpServer());
+    await checker.checkStatus();
+    // await checker.checkDetails();
+    await checker.checkFilter(['identity']);
+  });
+});
