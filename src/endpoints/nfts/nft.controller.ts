b9ebf90e2cd16df2501f7b3eb46be8385fdfe164
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-06-01 22:12:22 +0300

    revert optionalEnumPipe from getNfts()

--- src/endpoints/nfts/nft.controller.ts
+++ src/endpoints/nfts/nft.controller.ts
@@ -5,7 +5,6 @@ import { NftMediaService } from "src/queue.worker/nft.worker/queue/job-services/
 import { ParseAddressPipe } from "src/utils/pipes/parse.address.pipe";
 import { ParseArrayPipe } from "src/utils/pipes/parse.array.pipe";
 import { ParseOptionalBoolPipe } from "src/utils/pipes/parse.optional.bool.pipe";
-import { ParseOptionalEnumPipe } from 'src/utils/pipes/parse.optional.enum.pipe';
 import { Nft } from "./entities/nft";
 import { NftFilter } from "./entities/nft.filter";
 import { NftOwner } from "./entities/nft.owner";
@@ -41,7 +40,7 @@ export class NftController {
     @Query('size', new DefaultValuePipe(25), ParseIntPipe) size: number,
     @Query('search') search: string | undefined,
     @Query('identifiers', ParseArrayPipe) identifiers: string[] | undefined,
-    @Query('type', new ParseOptionalEnumPipe(NftType)) type: NftType | undefined,
+    @Query('type') type: NftType | undefined,
     @Query('collection') collection: string | undefined,
     @Query('name') name: string | undefined,
     @Query('tags', ParseArrayPipe) tags: string[] | undefined,
