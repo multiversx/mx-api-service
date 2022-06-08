e2f913c63396fbd45986798e28122e2c40338c46
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-06-08 15:32:42 +0300

    Update integration tests (#670)
    * [e2e]: update service integration tests
    
    * [e2e]: remove console.log

--- src/test/integration/nft.worker.e2e-spec.ts
+++ src/test/integration/nft.worker.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { Test } from "@nestjs/testing";
 import { NftWorkerService } from "../../queue.worker/nft.worker/nft.worker.service";
 import { Nft } from "../../endpoints/nfts/entities/nft";
-import nftExample from "../data/esdt/nft/nft.example";
 import { ProcessNftSettings } from "../../endpoints/process-nfts/entities/process.nft.settings";
 import { NftWorkerModule } from "src/queue.worker/nft.worker/nft.worker.module";
 import { NftType } from "../../endpoints/nfts/entities/nft.type";
+import { NftThumbnailService } from "src/queue.worker/nft.worker/queue/job-services/thumbnails/nft.thumbnail.service";
 
 describe('Nft Worker Service', () => {
   let nftWorkerService: NftWorkerService;
@@ -17,34 +17,79 @@ describe('Nft Worker Service', () => {
     nftWorkerService = moduleRef.get<NftWorkerService>(NftWorkerService);
   });
 
-  describe('Add Process Nft Queue Job', () => {
-    it('should force refresh media for nft', async () => {
-      const nft = new Nft();
-      nft.identifier = nftExample.identifier;
+  beforeEach(() => { jest.restoreAllMocks(); });
 
-      const nftOptions = new ProcessNftSettings();
-      nftOptions.forceRefreshMedia = true;
 
-      const result = await nftWorkerService.addProcessNftQueueJob(nft, nftOptions);
+  describe('addProcessNftQueueJob', () => {
+    it('should return true if nft is processed', async () => {
+      const nft: Nft = new Nft();
+      nft.identifier = 'MAW-894a92-0270';
+
+      const settings = new ProcessNftSettings();
+      settings.forceRefreshMedia = true;
+
+      const result = await nftWorkerService.addProcessNftQueueJob(nft, settings);
+
       expect(result).toStrictEqual(true);
     });
 
-    it("should return null if nft type is MetaESDT", async () => {
-      const nft = new Nft();
-      nft.type = NftType.MetaESDT;
+    it('should return false if nft does not need to be processed', async () => {
+      jest.spyOn(NftWorkerService.prototype, 'needsProcessing')
+        // eslint-disable-next-line require-await
+        .mockImplementation(jest.fn(async (_nft: Nft, _settings: ProcessNftSettings) => false));
 
-      const nftOptions = new ProcessNftSettings();
-      nftOptions.forceRefreshMedia = true;
+      const nft: Nft = new Nft();
+      nft.identifier = 'MAW-894a92-0270';
 
-      const result = await nftWorkerService.addProcessNftQueueJob(nft, nftOptions);
-      expect(result).toBeFalsy();
+      const settings: ProcessNftSettings = new ProcessNftSettings();
+      settings.forceRefreshMedia = true;
+
+      const result = await nftWorkerService.addProcessNftQueueJob(nft, settings);
+
+      expect(result).toStrictEqual(false);
     });
   });
 
-  describe('Needs Processing', () => {
-    it('should return true on nft processing', async () => {
+  describe('needsProcessing', () => {
+    it('should return false if esdt is the type MetaESDT', async () => {
+      const nft: Nft = new Nft();
+      nft.type = NftType.MetaESDT;
+
+      const settings: ProcessNftSettings = new ProcessNftSettings();
+      settings.forceRefreshMedia = true;
+
+      const result = await nftWorkerService.needsProcessing(nft, settings);
+
+      expect(result).toStrictEqual(false);
+    });
+
+    it('should return true if one of the "forceRefresh" settings is active', async () => {
+      const nft: Nft = new Nft();
+      nft.identifier = 'MAW-894a92-0270';
+
+      const settings: ProcessNftSettings = new ProcessNftSettings();
+      settings.forceRefreshMedia = true;
+
+      const result = await nftWorkerService.needsProcessing(nft, settings);
+
+      expect(result).toStrictEqual(true);
+    });
+
+    it('should return true if skipRefreshThumbnail settings is active', async () => {
+      const nft: Nft = new Nft();
+      nft.identifier = 'MAW-894a92-0270';
+
+      const settings: ProcessNftSettings = new ProcessNftSettings();
+      settings.skipRefreshThumbnail = true;
+
+      const result = await nftWorkerService.needsProcessing(nft, settings);
+
+      expect(result).toStrictEqual(true);
+    });
+
+    it('should return true if media is undefined', async () => {
       const nft = new Nft();
-      nft.identifier = nftExample.identifier;
+      nft.media = undefined;
 
       const nftOptions = new ProcessNftSettings();
       nftOptions.forceRefreshMedia = true;
@@ -53,23 +98,24 @@ describe('Nft Worker Service', () => {
       expect(result).toStrictEqual(true);
     });
 
-    it('should return true if ProcessNftSettings are set to true', async () => {
+    it('should return true if metadata is undefined', async () => {
       const nft = new Nft();
-      nft.identifier = nftExample.identifier;
+      nft.metadata = undefined;
 
-      const nftOptions = new ProcessNftSettings();
-      nftOptions.forceRefreshMedia = true;
-
-      const result = await nftWorkerService.needsProcessing(nft, nftOptions);
+      const result = await nftWorkerService.needsProcessing(nft, new ProcessNftSettings());
       expect(result).toStrictEqual(true);
     });
 
-    it('should return true if nft.media.length == 0', async () => {
+    it('should return true if media is defined and has thumbnail is generated', async () => {
+      jest.spyOn(NftThumbnailService.prototype, 'hasThumbnailGenerated')
+        // eslint-disable-next-line require-await
+        .mockImplementation(jest.fn(async (_identifier: string, _fileUrl: string) => false));
+
       const nft = new Nft();
-      nft.identifier = 'LKMEX-aab910-23049b';
+      nft.identifier = 'MAW-894a92-0270';
 
       const nftOptions = new ProcessNftSettings();
-      nftOptions.forceRefreshMedia = true;
+      nftOptions.skipRefreshThumbnail = false;
 
       const result = await nftWorkerService.needsProcessing(nft, nftOptions);
       expect(result).toStrictEqual(true);
