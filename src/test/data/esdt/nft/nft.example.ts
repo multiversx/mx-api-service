02e5ca244ae14ef781708aa15f30cb4aa1dbe805
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-06-15 16:00:51 +0300

    update dependencies version (#680)
    * [update]: update dependencies version
    
    * [e2e-fix]: Process Nfts Service integration test refactor

--- src/test/data/esdt/nft/nft.example.ts
+++ src/test/data/esdt/nft/nft.example.ts
@@ -1,83 +1,41 @@
-const nftExample = {
-  identifier: "MAW-894a92-0270",
-  collection: "MAW-894a92",
-  timestamp: 1643636676,
-  attributes: "bWV0YWRhdGE6UW1UQjk3dkhMYkdBZnkxVDJQZFUyWE5QdXlXQjd1Znd3aFNoNU1wTENWbjEybS8zOTUuanNvbg==",
-  nonce: 624,
-  type: "NonFungibleESDT",
-  name: "MAW #624",
-  creator: "erd1qqqqqqqqqqqqqpgqu3hd3lvuvevpmgqa2zxurer3s65693keys5sztn823",
+import { NftType } from "src/endpoints/nfts/entities/nft.type";
+
+const nftCollection = [{
+  identifier: 'EROBOT-527a29-c4',
+  collection: 'EROBOT-527a29',
+  timestamp: 1645784238,
+  attributes: 'dGFnczpFbHJvbmQsIFJvYm90cywgUm9ib3QsIGVSb2JvdHM7bWV0YWRhdGE6UW1lRHhBTlQzOUs4VWdRbzRGWFF4SkdKWURYTVVYZ2dNVlRNY1VBWnpuTnM2cS80MzEuanNvbg==',
+  nonce: 196,
+  type: NftType.NonFungibleESDT,
+  name: 'Elrond Robots #196',
+  creator: 'erd1qqqqqqqqqqqqqpgqlxyw866pd8pvfqvphgsz9dgx5mr44uv5ys5sew4epr',
   royalties: 10,
   uris: [
-    "aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1UQjk3dkhMYkdBZnkxVDJQZFUyWE5QdXlXQjd1Znd3aFNoNU1wTENWbjEybS8zOTUucG5n",
-    "aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1UQjk3dkhMYkdBZnkxVDJQZFUyWE5QdXlXQjd1Znd3aFNoNU1wTENWbjEybS8zOTUuanNvbg==",
-    "aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1UQjk3dkhMYkdBZnkxVDJQZFUyWE5QdXlXQjd1Znd3aFNoNU1wTENWbjEybS9jb2xsZWN0aW9uLmpzb24=",
+    'aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1lRHhBTlQzOUs4VWdRbzRGWFF4SkdKWURYTVVYZ2dNVlRNY1VBWnpuTnM2cS80MzEucG5n',
+    'aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1lRHhBTlQzOUs4VWdRbzRGWFF4SkdKWURYTVVYZ2dNVlRNY1VBWnpuTnM2cS80MzEuanNvbg==',
+    'aHR0cHM6Ly9pcGZzLmlvL2lwZnMvUW1lRHhBTlQzOUs4VWdRbzRGWFF4SkdKWURYTVVYZ2dNVlRNY1VBWnpuTnM2cS9jb2xsZWN0aW9uLmpzb24=',
   ],
-  url: "https://media.elrond.com/nfts/asset/QmTB97vHLbGAfy1T2PdU2XNPuyWB7ufwwhSh5MpLCVn12m/395.png",
+  url: 'https://media.elrond.com/nfts/asset/QmeDxANT39K8UgQo4FXQxJGJYDXMUXggMVTMcUAZznNs6q/431.png',
   media: [
     {
-      url: "https://media.elrond.com/nfts/thumbnail/default.png",
-      originalUrl: "https://media.elrond.com/nfts/thumbnail/default.png",
-      thumbnailUrl: "https://media.elrond.com/nfts/thumbnail/default.png",
-      fileType: "image/png",
+      url: 'https://media.elrond.com/nfts/thumbnail/default.png',
+      originalUrl: 'https://media.elrond.com/nfts/thumbnail/default.png',
+      thumbnailUrl: 'https://media.elrond.com/nfts/thumbnail/default.png',
+      fileType: 'image/png',
       fileSize: 29512,
     },
   ],
   isWhitelistedStorage: true,
-  tags: [
-    "Elrond",
-    "MAW",
-    "superMAW",
-    "Romania",
-    "TrustStaking",
-  ],
-  metadata: {
-    dna: "7eb2a6d3e1e0c93c5920a941744bd38a547858de",
-    edition: 395,
-    compiler: "Trust Staking",
-    createdAt: 1639180014636,
-    attributes: [
-      {
-        "value": "10",
-        "trait_type": "Background",
-      },
-      {
-        "value": "Suit_1",
-        "trait_type": "Body",
-      },
-      {
-        "value": "Mouth_Jail",
-        "trait_type": "Mouth",
-      },
-      {
-        "value": "Head_Trib",
-        "trait_type": "Head",
-      },
-      {
-        "value": "EyeL_Metalic_Black",
-        "trait_type": "Left Eye",
-      },
-      {
-        "value": "EyeR_Yellow",
-        "trait_type": "Right Eye",
-      },
-      {
-        "value": "EarL_Metalic_Gold",
-        "trait_type": "Left Ear",
-      },
-      {
-        "value": "EarR_Metalic_Black",
-        "trait_type": "Right Ear",
-      },
-      {
-        "value": "Drop3",
-        "trait_type": "Drops",
-      },
-    ],
-    description: "The unique MAW NFT Collection! ",
-  },
-  owner: "erd1dgctxljv7f6x8ngsqden99snygjw37dle3t8ratn59r33slsy4rqc3dpsh",
-  supply: "1",
-  ticker: "MAW-894a92",
-};
-export default nftExample;
+  thumbnailUrl: '',
+  tags: ['elrond', ' robots', ' robot', ' erobots'],
+  metadata: undefined,
+  owner: 'erd13w5hlehc42zvhd9u78ylrac9axntn9p9jqn9kvy3c052l8rmt2yqa59l76',
+  balance: undefined,
+  supply: '1',
+  decimals: undefined,
+  ticker: 'EROBOT-527a29',
+  scamInfo: undefined,
+  assets: undefined,
+},
+];
+export default nftCollection;
