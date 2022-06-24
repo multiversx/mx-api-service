aa50d80a2886ac3ecb00f7e7a34c055c206bb634
Traian Anghel <tanghel@live.com>
2022-06-24 15:55:02 +0300

    fixed imports

--- src/endpoints/vm.query/vm.query.module.ts
+++ src/endpoints/vm.query/vm.query.module.ts
@@ -1,10 +1,18 @@
 import { CachingModule } from "@elrondnetwork/nestjs-microservice-template";
 import { Module } from "@nestjs/common";
+import { ApiConfigModule } from "src/common/api-config/api.config.module";
+import { GatewayModule } from "src/common/gateway/gateway.module";
+import { MetricsModule } from "src/common/metrics/metrics.module";
+import { ProtocolModule } from "src/common/protocol/protocol.module";
 import { VmQueryService } from "./vm.query.service";
 
 @Module({
   imports: [
     CachingModule,
+    GatewayModule,
+    ProtocolModule,
+    ApiConfigModule,
+    MetricsModule,
   ],
   providers: [
     VmQueryService,
