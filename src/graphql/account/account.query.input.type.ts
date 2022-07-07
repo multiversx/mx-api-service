4316c517ecd5b47d055bdbbead9ce14782d9fd3b
filip <filipbaciiu@gmail.com>
2022-07-07 17:08:41 +0300

    Updated skip parameter to from

--- src/graphql/account/account.query.input.type.ts
+++ src/graphql/account/account.query.input.type.ts
@@ -4,13 +4,13 @@ import { QueryPagination } from "src/common/entities/query.pagination";
 @InputType({ description: "Get accounts input." })
 export class GetAccountsInput {
   @Field(() => Float, { name: "from", description: "Number of accounts to skip for the given result set.", nullable: true, defaultValue: 0 })
-  skip: number = 0;
+  from: number = 0;
 
   @Field(() => Float, { name: "size", description: "Number of accounts to retrieve for the given result set.", nullable: true, defaultValue: 25 })
   size: number = 25;
 
   public static resolve(input: GetAccountsInput): QueryPagination {
-    return { from: input.skip, size: input.size };
+    return { from: input.from, size: input.size };
   }
 }
 
