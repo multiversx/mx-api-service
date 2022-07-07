c887bbb27477e74e8dab5ee2f8c94af87519a866
filip <filipbaciiu@gmail.com>
2022-07-07 10:41:00 +0300

    Removed forwardRef in GraphQL account module

--- src/graphql/account/account.module.ts
+++ src/graphql/account/account.module.ts
@@ -1,12 +1,10 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { AccountModule as InternalAccountModule } from "src/endpoints/accounts/account.module";
 import { AccountQueryResolver } from "src/graphql/account/account.resolver.query";
 
 @Module({
-  imports: [
-    forwardRef(() => InternalAccountModule),
-  ],
+  imports: [InternalAccountModule],
   providers: [AccountQueryResolver],
 })
 export class AccountModule {}
