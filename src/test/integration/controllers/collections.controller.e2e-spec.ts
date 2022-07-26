f8fb1ac8bdb72f2858e0bd24571757b1c513b2ab
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-07-26 13:19:11 +0300

    skip temporary tests execution for e2e-controllers

--- src/test/integration/controllers/collections.controller.e2e-spec.ts
+++ src/test/integration/controllers/collections.controller.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe("collections Controller", () => {
+describe.skip("collections Controller", () => {
   let app: INestApplication;
   const route: string = "/collections";
 
--- src/test/integration/controllers/tokens.controller.e2e-spec.ts
+++ src/test/integration/controllers/tokens.controller.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe("Tokens Controller", () => {
+describe.skip("Tokens Controller", () => {
   let app: INestApplication;
   const route: string = "/tokens";
 
