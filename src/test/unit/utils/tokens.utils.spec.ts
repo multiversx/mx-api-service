e27d23620baa675876dfed585ab1eee22a845096
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-08-08 14:32:25 +0300

    remove api utils unit tests  (#768)
    * Remove utils unit tests -> moved to ernest
    
    * remove isToken. isCollection, isNft unit tests

--- src/test/unit/utils/tokens.utils.spec.ts
+++ src/test/unit/utils/tokens.utils.spec.ts
@@ -1,5 +1,4 @@
 import { TokenHelpers } from "src/utils/token.helpers";
-import { TokenUtils } from "@elrondnetwork/erdnest";
 
 describe('Token Utils', () => {
   describe('canBool', () => {
@@ -19,27 +18,6 @@ describe('Token Utils', () => {
     it('Check function getThumbnailUrlIdentifier', () => {
       expect(TokenHelpers.getThumbnailUrlIdentifier('MOS-b9b4b2-2710', 'https://media.elrond.com/nfts/asset/QmUUhAmBQKGkSqN775NZAAYUaqd8ssMadFg2UYSECSERz6/914.png'))
         .toStrictEqual('MOS-b9b4b2-947a3912');
-    });
-  });
-
-  describe('isToken', () => {
-    it('Check isToken function', () => {
-      expect(TokenUtils.isToken('MEX-455c57')).toBeTruthy();
-      expect(TokenUtils.isToken('EWLD-e23800-455c74')).toBeFalsy();
-    });
-  });
-
-  describe('isCollection', () => {
-    it('Check isCollection function', () => {
-      expect(TokenUtils.isCollection('MOS-b9b4b2')).toBeTruthy();
-      expect(TokenUtils.isCollection('MOS-b9b4b2-455c74')).toBeFalsy();
-    });
-  });
-
-  describe('isNft', () => {
-    it('Check isNft function', () => {
-      expect(TokenUtils.isNft('MOS-b9b4b2-947a3912')).toBeTruthy();
-      expect(TokenUtils.isNft('MOS-b9b4b2')).toBeFalsy();
     });
   });
 });
