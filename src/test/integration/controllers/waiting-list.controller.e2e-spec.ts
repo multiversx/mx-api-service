500907f399e6c02d3b65cc3d25ed84934160b0a0
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-09 16:50:10 +0300

    update waiting-list.controller.e2e-spec.ts test

--- src/test/integration/controllers/waiting-list.controller.e2e-spec.ts
+++ src/test/integration/controllers/waiting-list.controller.e2e-spec.ts
@@ -3,11 +3,11 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe.skip("Waiting-List Controller", () => {
+describe("Waiting-List Controller", () => {
   let app: INestApplication;
-  const route: string = "/waiting-list";
+  const path: string = "/waiting-list";
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [PublicAppModule],
     }).compile();
@@ -19,19 +19,23 @@ describe.skip("Waiting-List Controller", () => {
 
   it("/waiting-list - should return 200 status code and one list of accounts in waiting-list", async () => {
     await request(app.getHttpServer())
-      .get(route)
+      .get(`${path}`)
       .expect(200);
   });
 
   it("/waiting-list/count - should return 200 status code and waiting-lists total count", async () => {
     await request(app.getHttpServer())
-      .get(route + "/count")
+      .get(`${path}/count`)
       .expect(200);
   });
 
   it("/waiting-list/c - should return 200 status code and waiting-lists total count (alternative)", async () => {
     await request(app.getHttpServer())
-      .get(route + "/c")
+      .get(`${path}/count`)
       .expect(200);
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
