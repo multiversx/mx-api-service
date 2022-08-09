e8c3b0c88c245607536de2ef76ba1341e226c359
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-09 17:29:39 +0300

    update shards.controller.e2e-spec.ts

--- src/test/integration/controllers/shards.controller.e2e-spec.ts
+++ src/test/integration/controllers/shards.controller.e2e-spec.ts
@@ -3,33 +3,45 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe.skip("Shards Controller", () => {
+describe("Shards Controller", () => {
   let app: INestApplication;
-  const route: string = "/shards";
+  const path: string = "/shards";
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [PublicAppModule],
     }).compile();
 
     app = moduleRef.createNestApplication();
-
     await app.init();
   });
 
-  it("/shards - should return 200 status code and list of shards details", async () => {
+  it("should return all available shards", async () => {
     await request(app.getHttpServer())
-      .get(route)
-      .expect(200);
+      .get(`${path}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toHaveLength(4);
+        expect(res.body[0].shard).toBeDefined();
+        expect(res.body[0].shard).toStrictEqual(2);
+      });
   });
 
-  it("/shards?from&size - should return 200 status code and list of 10 shards details", async () => {
+  it("should return 1 shard details", async () => {
     const params = new URLSearchParams({
-      'from': '0',
-      'size': '10',
+      'size': '1',
     });
     await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body[0].shard).toStrictEqual(2);
+        expect(res.body[0].validators).toBeDefined();
+        expect(res.body[0].activeValidators).toBeDefined();
+      });
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
