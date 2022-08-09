3b89d91cf30eefd8045f24be5b69b0cb33448b85
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-09 13:36:18 +0300

    collections e2e controller test

--- src/test/integration/controllers/collections.controller.e2e-spec.ts
+++ src/test/integration/controllers/collections.controller.e2e-spec.ts
@@ -3,9 +3,9 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe.skip("collections Controller", () => {
+describe("collections Controller", () => {
   let app: INestApplication;
-  const route: string = "/collections";
+  const path: string = "/collections";
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,207 +16,150 @@ describe.skip("collections Controller", () => {
     await app.init();
   });
 
-  it("/collections - should return 200 status code and a list of collections details", async () => {
+  it('/collections -> should return 25 collections', async () => {
     await request(app.getHttpServer())
-      .get(route)
-      .expect(200);
-  });
-
-  it("/collections?from&size - should return 200 status code and a list of 100 collections details", async () => {
-    const params = new URLSearchParams({
-      'from': '0',
-      'size': '100',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?fcreator - should return 200 status code and a list of collection of creator details", async () => {
-    const params = new URLSearchParams({
-      'creator': 'erd1qqqqqqqqqqqqqpgq0p9k56lutyjsz288gsrtu64nfj43ll8vys5sjy7luv',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of NonFungibleESDT collection details", async () => {
-    const params = new URLSearchParams({
-      'type': 'NonFungibleESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of 10 NonFungibleESDT collections details", async () => {
-    const params = new URLSearchParams({
-      'from': '0',
-      'size': '10',
-      'type': 'NonFungibleESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of SemiFungibleESDT collection details", async () => {
-    const params = new URLSearchParams({
-      'type': 'SemiFungibleESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of 10 SemiFungibleESDT collections details", async () => {
-    const params = new URLSearchParams({
-      'from': '0',
-      'size': '10',
-      'type': 'SemiFungibleESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of MetaESDT collection details", async () => {
-    const params = new URLSearchParams({
-      'type': 'MetaESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?type - should return 200 status code and a list of 10 MetaESDT collections details", async () => {
-    const params = new URLSearchParams({
-      'from': '0',
-      'size': '10',
-      'type': 'MetaESDT',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?identifiers - should return 200 status code and one collection details based on identifier filter", async () => {
-    const params = new URLSearchParams({
-      'identifiers': 'PEACE-086f39',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections?identifiers - should return 200 status code and two collections details based on identifier filter", async () => {
-    const params = new URLSearchParams({
-      'identifiers': 'PEACE-086f39,MOS-b9b4b2',
-    });
-
-    await request(app.getHttpServer())
-      .get(route + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count - should return 200 status code and collections count", async () => {
-    await request(app.getHttpServer())
-      .get(route + "/count")
-      .expect(200);
-  });
-
-  it("/collections/count?creator - should return 200 status code and creator collections count", async () => {
-    const params = new URLSearchParams({
-      'creator': 'erd1qqqqqqqqqqqqqpgq0p9k56lutyjsz288gsrtu64nfj43ll8vys5sjy7luv',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?type - should return 200 status code and NonFungibleESDT collections count", async () => {
-    const params = new URLSearchParams({
-      'type': 'NonFungibleESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?type - should return 200 status code and SemiFungibleESDT collections count", async () => {
-    const params = new URLSearchParams({
-      'type': 'SemiFungibleESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?type - should return 200 status code and MetaESDT collections count", async () => {
-    const params = new URLSearchParams({
-      'type': 'MetaESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?creator&type - should return 200 status code and collection count of type NonFungibleESDT for a specific creator", async () => {
-    const params = new URLSearchParams({
-      'creator': 'erd1qqqqqqqqqqqqqpgq0p9k56lutyjsz288gsrtu64nfj43ll8vys5sjy7luv',
-      'type': 'NonFungibleESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?creator&type - should return 200 status code and collection count of type SemiFungibleESDT for a specific creator", async () => {
-    const params = new URLSearchParams({
-      'creator': 'erd1qqqqqqqqqqqqqpgq0p9k56lutyjsz288gsrtu64nfj43ll8vys5sjy7luv',
-      'type': 'SemiFungibleESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/count?creator&type - should return 200 status code and collection count of type MetaESDT for a specific creator", async () => {
-    const params = new URLSearchParams({
-      'creator': 'erd1qqqqqqqqqqqqqpgq0p9k56lutyjsz288gsrtu64nfj43ll8vys5sjy7luv',
-      'type': 'MetaESDT',
-    });
-    await request(app.getHttpServer())
-      .get(route + "/count" + "?" + params)
-      .expect(200);
-  });
-
-  it("/collections/:collection - should return 200 status code and collection details for a specifc collection", async () => {
-    const collection: string = "MOS-b9b4b2";
-
-    await request(app.getHttpServer())
-      .get(route + "/" + collection)
-      .expect(200);
-  });
-
-  it("/collections/:collection - should return 404 status code Error Code: NFT Collection not found", async () => {
-    const collection: string = "MOS-b9b4b2T";
-
-    await request(app.getHttpServer())
-      .get(route + "/" + collection)
-      .expect(404)
+      .get(path)
+      .expect(200)
       .then(res => {
-        expect(res.body.message).toEqual("NFT collection not found");
+        expect(res.body).toHaveLength(25);
       });
   });
+
+  it('/collections -> should return 5 collections', async () => {
+    const params = new URLSearchParams({
+      'size': '5',
+    });
+
+    await request(app.getHttpServer())
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toHaveLength(5);
+      });
+  });
+
+  it('/collections -> should return collection detailes for a given identifier', async () => {
+    const params = new URLSearchParams({
+      'search': 'MEDAL-ae074f',
+    });
+
+    const expected =
+      [
+        {
+          collection: "MEDAL-ae074f",
+          type: "NonFungibleESDT",
+          name: "GLUMedals",
+          ticker: "MEDAL-ae074f",
+          owner: "erd126y66ear20cdskrdky0kpzr9agjul7pcut7ktlr6p0eu8syxhvrq0gsqdj",
+          timestamp: 1654019676,
+          canFreeze: false,
+          canWipe: false,
+          canPause: false,
+          canTransferNftCreateRole: false,
+          roles: [],
+        }];
+
+    await request(app.getHttpServer())
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toStrictEqual(expected);
+      });
+  });
+
+  it('/collections -> should return two collections details', async () => {
+    const params = new URLSearchParams({
+      'identifiers': 'MEDAL-ae074f,EROBOT-527a29',
+    });
+
+    const expected =
+      [
+        {
+          collection: "MEDAL-ae074f",
+          type: "NonFungibleESDT",
+          name: "GLUMedals",
+          ticker: "MEDAL-ae074f",
+          owner: "erd126y66ear20cdskrdky0kpzr9agjul7pcut7ktlr6p0eu8syxhvrq0gsqdj",
+          timestamp: 1654019676,
+          canFreeze: false,
+          canWipe: false,
+          canPause: false,
+          canTransferNftCreateRole: false,
+          roles: [],
+        },
+        {
+          collection: "EROBOT-527a29",
+          type: "NonFungibleESDT",
+          name: "eRobots",
+          ticker: "EROBOT-527a29",
+          owner: "erd1nz42knvgmxpevepsyvq9dx3wzdgtd6lmu96y28tuupayazgx4fvs3w9d09",
+          timestamp: 1638990678,
+          canFreeze: false,
+          canWipe: false,
+          canPause: false,
+          canTransferNftCreateRole: false,
+          roles: [],
+        },
+      ];
+
+    await request(app.getHttpServer())
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toStrictEqual(expected);
+      });
+  });
+
+  it('/collections -> should return one collection details based on canCreate role address', async () => {
+    const params = new URLSearchParams({
+      'canCreate': 'erd1qqqqqqqqqqqqqpgq09vq93grfqy7x5fhgmh44ncqfp3xaw57ys5s7j9fed',
+    });
+
+    await request(app.getHttpServer())
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body.length).toBeGreaterThanOrEqual(1);
+      });
+  });
+
+  it('/collections -> should return one collection details based on canBurn role address', async () => {
+    const params = new URLSearchParams({
+      'canCreate': 'erd1kj2tpra7e5vayparwv0fzhgsmnrw442r5u53sphjfs2h8lrl54sstuf6y4',
+    });
+
+    await request(app.getHttpServer())
+      .get(`${path}?${params}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body.length).toBeGreaterThanOrEqual(1);
+      });
+  });
+
+  test.each`
+  types
+  ${'NonFungibleESDT'}
+  ${'SemiFungibleESDT'}
+  ${'MetaESDT'}
+  `
+    (
+      `for the given collection type $types, should return 25 collections`,
+      async ({ types }) => {
+        const params = new URLSearchParams({
+          'type': types,
+        });
+
+        await request(app.getHttpServer())
+          .get(`${path}?${params}`)
+          .expect(200)
+          .then(res => {
+            expect(res.body).toHaveLength(25);
+            expect(res.body).toEqual(
+              expect.arrayContaining([
+                expect.objectContaining({ type: `${types}` }),
+              ])
+            );
+          });
+      }
+    );
 });
