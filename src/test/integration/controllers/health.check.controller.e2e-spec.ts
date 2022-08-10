0de8975fddab1e9477ed4d008f51cda8e2500bcb
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-10 10:25:55 +0300

    update health.check.controller.e2e-spec.ts

--- src/test/integration/controllers/health.check.controller.e2e-spec.ts
+++ src/test/integration/controllers/health.check.controller.e2e-spec.ts
@@ -3,9 +3,9 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 
-describe.skip("Usernames Controller", () => {
+describe("Hello Controller", () => {
   let app: INestApplication;
-  const route: string = "/hello";
+  const path: string = "/hello";
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -13,17 +13,17 @@ describe.skip("Usernames Controller", () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
-
     await app.init();
   });
 
-  it("/hello - should return status code 200 and response message 'hello' ", async () => {
-
-    await request(app.getHttpServer())
-      .get(route)
-      .expect(200)
-      .then(res => {
-        expect(res.text).toStrictEqual('hello');
-      });
+  describe('/hello', () => {
+    it("should returns 'hello', used for performing health checks ", async () => {
+      await request(app.getHttpServer())
+        .get(`${path}`)
+        .expect(200)
+        .then(res => {
+          expect(res.text).toStrictEqual('hello');
+        });
+    });
   });
 });
