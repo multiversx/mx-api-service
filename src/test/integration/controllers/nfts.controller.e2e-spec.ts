7d403d67b9b1dd30d042ae28677b0faa305751fd
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-10 18:00:38 +0300

    fix validation message

--- src/test/integration/controllers/nfts.controller.e2e-spec.ts
+++ src/test/integration/controllers/nfts.controller.e2e-spec.ts
@@ -26,19 +26,7 @@ describe("NFT Controller", () => {
         });
     });
 
-    it(`/nfts - should return 400 status code because "withScamInfo" flag is active and size is greater than 100 (limit)`, async () => {
-      const params = new URLSearchParams({
-        'from': '0',
-        'size': '101',
-        'withScamInfo': 'true',
-      });
-      await request(app.getHttpServer())
-        .get(path + "?" + params)
-        .expect(400)
-        .then(res => {
-          expect(res.body.message).toEqual("Maximum size of 100 is allowed when activating flags 'withOwner' or 'withSupply'");
-        });
-    });
+
 
     it("/nfts?withSupply - should return 200 status code and one list of nfts with filter withSupply", async () => {
       const params = new URLSearchParams({
@@ -363,6 +351,20 @@ describe("NFT Controller", () => {
         .expect(404)
         .then(res => {
           expect(res.body.message).toStrictEqual("NFT not found");
+        });
+    });
+
+    it(`/nfts - should return 400 status code because "withScamInfo" flag is active and size is greater than 100 (limit)`, async () => {
+      const params = new URLSearchParams({
+        'from': '0',
+        'size': '101',
+        'withScamInfo': 'true',
+      });
+      await request(app.getHttpServer())
+        .get(path + "?" + params)
+        .expect(400)
+        .then(res => {
+          expect(res.body.message).toEqual("Maximum size of 100 is allowed when activating flags 'withOwner' or 'withSupply' or 'withScamInfo'");
         });
     });
   });
