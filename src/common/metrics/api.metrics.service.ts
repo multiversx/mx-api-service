a81639cb832395292aeac46af799a0413e23b7a0
Traian Anghel <tanghel@live.com>
2022-08-17 17:31:50 +0300

    API call cpu usage metrics (#783)
    * temp stuff
    
    * request cpu time interceptor integration
    
    * add request cpu time in response headers
    
    * fix validation message
    
    * removed response header setting
    
    Co-authored-by: catalinfaurpaul <catalinfaurpaul@gmail.com>

--- src/common/metrics/api.metrics.service.ts
+++ src/common/metrics/api.metrics.service.ts
@@ -8,6 +8,7 @@ import { ProtocolService } from "../protocol/protocol.service";
 
 @Injectable()
 export class ApiMetricsService {
+  private static apiCpuTimeHistogram: Histogram<string>;
   private static vmQueriesHistogram: Histogram<string>;
   private static gatewayDurationHistogram: Histogram<string>;
   private static persistenceDurationHistogram: Histogram<string>;
@@ -23,6 +24,15 @@ export class ApiMetricsService {
     private readonly protocolService: ProtocolService,
     private readonly metricsService: MetricsService,
   ) {
+    if (!ApiMetricsService.apiCpuTimeHistogram) {
+      ApiMetricsService.apiCpuTimeHistogram = new Histogram({
+        name: 'api_cpu_time',
+        help: 'API CPU time',
+        labelNames: ['endpoint'],
+        buckets: [],
+      });
+    }
+
     if (!ApiMetricsService.vmQueriesHistogram) {
       ApiMetricsService.vmQueriesHistogram = new Histogram({
         name: 'vm_query',
@@ -74,6 +84,10 @@ export class ApiMetricsService {
         labelNames: ['shardId'],
       });
     }
+  }
+
+  setApiCpuTime(endpoint: string, duration: number) {
+    ApiMetricsService.apiCpuTimeHistogram.labels(endpoint).observe(duration);
   }
 
   setVmQuery(address: string, func: string, duration: number) {
