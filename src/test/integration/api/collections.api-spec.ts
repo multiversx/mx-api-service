64723701e5668c755841d0820d99e94d6858e2f1
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-08-23 18:10:01 +0300

    disable collection.api-spec.ts (temp)

--- src/test/integration/api/collections.api-spec.ts
+++ src/test/integration/api/collections.api-spec.ts
@@ -4,7 +4,7 @@ import { Test } from '@nestjs/testing';
 import { PublicAppModule } from 'src/public.app.module';
 import { ApiChecker } from 'src/utils/api.checker';
 
-describe("API Testing", () => {
+describe.skip("API Testing", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
