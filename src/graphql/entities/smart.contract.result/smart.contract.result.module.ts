f0607de4027e71922aaf33b74e49b30db88a63f4
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-09-15 20:47:30 +0300

    Add results Query Endpoint

--- src/graphql/entities/smart.contract.result/smart.contract.result.module.ts
+++ src/graphql/entities/smart.contract.result/smart.contract.result.module.ts
@@ -3,9 +3,11 @@ import { Module } from "@nestjs/common";
 import { SmartContractResultLoader } from "src/graphql/entities/smart.contract.result/smart.contract.result.loader";
 import { SmartContractResultResolver } from "src/graphql/entities/smart.contract.result/smart.contract.result.resolver";
 import { TransactionModule } from "src/endpoints/transactions/transaction.module";
+import { SmartContractResultQueryResolver } from "./smart.contract.result.query.resolver";
+import { SmartContractResultModule as InternalSmartContractResultModule } from "src/endpoints/sc-results/scresult.module";
 
 @Module({
-  imports: [TransactionModule],
-  providers: [SmartContractResultLoader, SmartContractResultResolver],
+  imports: [TransactionModule, InternalSmartContractResultModule],
+  providers: [SmartContractResultLoader, SmartContractResultResolver, SmartContractResultQueryResolver],
 })
-export class SmartContractResultModule {}
+export class SmartContractResultModule { }
---
+++ src/graphql/entities/smart.contract.result/smart.contract.result.query.ts
@@ -0,0 +1,40 @@
+import { Args, Float, Query, Resolver } from "@nestjs/graphql";
+import { SmartContractResultService } from "src/endpoints/sc-results/scresult.service";
+import { SmartContractResult } from "src/endpoints/sc-results/entities/smart.contract.result";
+import { GetSmartContractHashInput, GetSmartContractResultInput } from "./smart.contract.result.input";
+import { SmartContractResultFilter } from "src/endpoints/sc-results/entities/smart.contract.result.filter";
+import { QueryPagination } from "src/common/entities/query.pagination";
+import { NotFoundException } from "@nestjs/common";
+
+@Resolver()
+export class SmartContractResultQuery {
+  constructor(protected readonly smartContractResultService: SmartContractResultService) { }
+
+  @Query(() => [SmartContractResult], { name: "scResults", description: "Retrieve all smart contract results for the given input." })
+  public async getScResults(@Args("input", { description: "Input to retrieve the given smart contract results for." }) input: GetSmartContractResultInput): Promise<SmartContractResult[]> {
+    return await this.smartContractResultService.getScResults(
+      new QueryPagination({
+        from: input.from,
+        size: input.size,
+      }),
+      new SmartContractResultFilter({
+        miniBlockHash: input.miniBlockHash,
+        originalTxHashes: input.originalTxHashes,
+      })
+    );
+  }
+
+  @Query(() => Float, { name: "scResultsCount", description: "Returns total number of smart contracts." })
+  public async getScResultsCount(): Promise<number> {
+    return await this.smartContractResultService.getScResultsCount();
+  }
+
+  @Query(() => SmartContractResult, { name: "scResult", description: "Retrieve the smart contract details for the given input.", nullable: true })
+  public async getScResult(@Args("input", { description: "Input to retrieve the given smart contract for." }) input: GetSmartContractHashInput): Promise<SmartContractResult | undefined> {
+    try {
+      return await this.smartContractResultService.getScResult(input.scHash);
+    } catch {
+      throw new NotFoundException('Smart contract not found');
+    }
+  }
+}
