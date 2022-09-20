13990567423581e4d1df25d4ec74e8fda23ca20a
catalinfaurpaul <catalinfaurpaul@gmail.com>
2022-09-20 12:06:14 +0300

    Add graph spec: shard stake waitingList webSocket

---
+++ src/test/integration/graphql/waiting.list.graph-spec.ts
@@ -0,0 +1,44 @@
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { PublicAppModule } from "src/public.app.module";
+import request = require('supertest');
+
+describe('WaitingList', () => {
+  let app: INestApplication;
+  const gql = '/graphql';
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [PublicAppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  describe('Query - Get Waiting List', () => {
+    it('should returns node waiting list', async () => {
+      await request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `{
+            waitingList{
+              address
+              value
+              nonce
+              rank
+            }
+          }`,
+        })
+        .expect(200)
+        .then(res => {
+          for (const item of res.body.data.waitingList) {
+            expect(item.address).toBeDefined();
+            expect(item.value).toBeDefined();
+            expect(item.nonce).toBeDefined();
+            expect(item.rank).toBeDefined();
+          }
+        });
+    });
+  });
+});
