2325de5566eac71e42fb7bfcd6996cf72c24de2c
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-09-23 16:39:56 +0300

    Add pagination to waiting-list endpoint (#815)
    * SERVICES-641 - Add pagination to
    waiting-list endpoint
    
    * imported ParseIntPipe from erdnest
    
    Co-authored-by: Traian Anghel <tanghel@live.com>

--- src/graphql/entities/waiting.list/waiting.list.query.ts
+++ src/graphql/entities/waiting.list/waiting.list.query.ts
@@ -1,6 +1,7 @@
-import { Float, Resolver, Query } from "@nestjs/graphql";
+import { Float, Resolver, Query, Args } from "@nestjs/graphql";
 import { WaitingList } from "src/endpoints/waiting-list/entities/waiting.list";
 import { WaitingListService } from "src/endpoints/waiting-list/waiting.list.service";
+import { GetWaitingListInput } from "./waiting.list.input";
 
 
 @Resolver()
@@ -8,8 +9,8 @@ export class WaitingListQuery {
   constructor(protected readonly waitingListService: WaitingListService) { }
 
   @Query(() => [WaitingList], { name: "waitingList", description: "Retrieve all address that are in waiting." })
-  public async getWaitingList(): Promise<WaitingList[]> {
-    return await this.waitingListService.getWaitingList();
+  public async getWaitingList(@Args("input", { description: "Input to retrieve the given waiting accounts for." }) input: GetWaitingListInput): Promise<WaitingList[]> {
+    return await this.waitingListService.getWaitingList(GetWaitingListInput.resolve(input));
   }
 
   @Query(() => Float, { name: "waitingListCount", description: "Retrieve all addresses count that are in waiting." })
