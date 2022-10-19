0b062f6f428ab61f330b55731d964d51e40473ce
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-10-19 12:47:09 +0300

    add transfers query (#851)
    * SERVICES-783 add transfers query
    
    * SERVICES-783 add schema.gql
    
    * SERVICES-783 fix after review
    
    Co-authored-by: Traian Anghel <tanghel@live.com>

--- src/endpoints/transactions/entities/transaction.type.ts
+++ src/endpoints/transactions/entities/transaction.type.ts
@@ -1,4 +1,20 @@
+import { registerEnumType } from "@nestjs/graphql";
+
 export enum TransactionType {
   Transaction = 'Transaction',
   SmartContractResult = 'SmartContractResult'
 }
+
+registerEnumType(TransactionType, {
+  name: 'TransactionType',
+  description: 'Transaction type object type.',
+  valuesMap: {
+    Transaction: {
+      description: 'Transaction type.',
+    },
+    SmartContractResult: {
+      description: 'SmartContractResult type.',
+    },
+
+  },
+});
