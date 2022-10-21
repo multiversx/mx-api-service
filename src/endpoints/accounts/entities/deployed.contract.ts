84b4df0d5d782cd416d6f709b5ab7f4c4ae87906
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2022-10-21 12:38:23 +0300

    label account contracts endpoint (#862)
    * SERVICES-807 Add contracts address assets
    
    * SERVICES-807 fix assets contract description
    
    Co-authored-by: Traian Anghel <tanghel@live.com>

--- src/endpoints/accounts/entities/deployed.contract.ts
+++ src/endpoints/accounts/entities/deployed.contract.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { AccountAssets } from "src/common/assets/entities/account.assets";
 
 export class DeployedContract {
   constructor(init?: Partial<DeployedContract>) {
@@ -13,4 +14,7 @@ export class DeployedContract {
 
   @ApiProperty({ type: Number })
   timestamp: number = 0;
+
+  @ApiProperty({ type: AccountAssets, nullable: true, description: 'Contract assets' })
+  assets: AccountAssets | undefined = undefined;
 }
