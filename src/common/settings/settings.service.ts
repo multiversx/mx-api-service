abb52b834597678431240a8e105e592548d5fe7e
Traian Anghel <tanghel@live.com>
2022-11-02 15:03:48 +0200

    removed settings that should stay in apiConfigService

--- src/common/settings/settings.service.ts
+++ src/common/settings/settings.service.ts
@@ -21,28 +21,8 @@ export class SettingsService {
     return await this.getSetting<boolean>('USE_REQUEST_LOGGING', this.apiConfigService.getUseRequestLoggingFlag());
   }
 
-  async getUseKeepAliveAgentFlag(): Promise<boolean> {
-    return await this.getSetting<boolean>('USE_KEEP_ALIVE_AGENT', this.apiConfigService.getUseKeepAliveAgentFlag());
-  }
-
-  async getUseTracingFlag(): Promise<boolean> {
-    return await this.getSetting<boolean>('USE_TRACING', this.apiConfigService.getUseTracingFlag());
-  }
-
   async getUseVmQueryTracingFlag(): Promise<boolean> {
     return await this.getSetting<boolean>('USE_VM_QUERY_TRACING', this.apiConfigService.getUseVmQueryTracingFlag());
-  }
-
-  async getIsProcessNftsFlagActive(): Promise<boolean> {
-    return await this.getSetting<boolean>('PROCESS_NFTS', this.apiConfigService.getIsProcessNftsFlagActive());
-  }
-
-  async getIsIndexerV3FlagActive(): Promise<boolean> {
-    return await this.getSetting<boolean>('INDEXER_V3', this.apiConfigService.getIsIndexerV3FlagActive());
-  }
-
-  async isStakingV4Enabled(): Promise<boolean> {
-    return await this.getSetting<boolean>('STAKING_V4', this.apiConfigService.isStakingV4Enabled());
   }
 
   private async getSetting<T>(name: string, fallbackValue: T): Promise<T> {
