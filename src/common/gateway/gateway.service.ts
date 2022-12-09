8157876935b2f2cbd41fa883a3b8031bce7a2355
Traian Anghel <tanghel@live.com>
2022-12-09 19:57:42 +0200

    also use url processing for post requests towards the gateway

--- src/common/gateway/gateway.service.ts
+++ src/common/gateway/gateway.service.ts
@@ -80,7 +80,7 @@ export class GatewayService {
     const profiler = new PerformanceProfiler();
 
     try {
-      return await this.apiService.post(`${this.apiConfigService.getGatewayUrl()}/${url}`, data, new ApiSettings(), errorHandler);
+      return await this.apiService.post(`${this.getUrl(component)}/${url}`, data, new ApiSettings(), errorHandler);
     } finally {
       profiler.stop();
 
