bd4caf0920378cfc17610552a3072f75c4e76c4d
Traian Anghel <tanghel@live.com>
2022-12-10 22:20:36 +0200

    cache dapp config

--- src/endpoints/dapp-config/dapp.config.service.ts
+++ src/endpoints/dapp-config/dapp.config.service.ts
@@ -1,4 +1,4 @@
-import { FileUtils } from "@elrondnetwork/erdnest";
+import { CachingService, Constants, FileUtils } from "@elrondnetwork/erdnest";
 import { Injectable } from "@nestjs/common";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
 import { DappConfig } from "./entities/dapp-config";
@@ -6,10 +6,20 @@ import { DappConfig } from "./entities/dapp-config";
 @Injectable()
 export class DappConfigService {
   constructor(
-    private readonly apiConfigService: ApiConfigService
+    private readonly apiConfigService: ApiConfigService,
+    private readonly cachingService: CachingService,
   ) { }
 
-  getDappConfiguration(): DappConfig | undefined {
+  async getDappConfiguration(): Promise<DappConfig | undefined> {
+    return await this.cachingService.getOrSetCache(
+      'dappConfig',
+      async () => await this.getDappConfigurationRaw(),
+      Constants.oneHour(),
+      Constants.oneMinute()
+    );
+  }
+
+  getDappConfigurationRaw(): DappConfig | undefined {
     const network = this.apiConfigService.getNetwork();
     const configuration = FileUtils.parseJSONFile(`./config/dapp.config.${network}.json`);
 
