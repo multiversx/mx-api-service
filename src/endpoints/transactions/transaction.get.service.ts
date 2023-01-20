f4977c79748402a0c0ccb344f2bf3b678af999ba
Catalin Faur <52102171+CatalinFaurPaul@users.noreply.github.com>
2023-01-20 13:02:54 +0200

    transaction operations field should select logs results (#905)
    * SERVICES-980 add transaction operation field
    
    * SERVICES-980 add getTransaction spec tests
    
    * proper fix
    
    * fix after self-review
    
    * moved operations fetch above logs processing
    
    Co-authored-by: Traian Anghel <tanghel@live.com>

--- src/endpoints/transactions/transaction.get.service.ts
+++ src/endpoints/transactions/transaction.get.service.ts
@@ -93,7 +93,7 @@ export class TransactionGetService {
         }
       }
 
-      if (!fields || fields.length === 0 || fields.includes(TransactionOptionalFieldOption.logs)) {
+      if (!fields || fields.length === 0 || fields.includes(TransactionOptionalFieldOption.logs) || fields.includes(TransactionOptionalFieldOption.operations)) {
         const logs = await this.getTransactionLogsFromElastic(hashes);
 
         if (!fields || fields.length === 0 || fields.includes(TransactionOptionalFieldOption.operations)) {
