a858095bfd0df4e7dd1220cd18ef08bba37ab813
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2023-02-06 11:26:16 +0200

    DB decorators (#886)
    * remove mysql persistence driver
    
    * add error and performance decorators
    
    * implement errorLog, passthrough and performance monitor decorator
    
    * decouple ApiMetricsService from application
    
    * update package.lock
    
    * move metrics logs strings to enums
    
    * fixed imports
    
    * removed console log
    
    * updated package lock file
    
    * implicit method name if not specified in log performance decorator, usage in indexer service
    
    * use log performance decorator directly
    
    * event emitter refactoring
    
    * add log performance decorators locally
    
    * add error and performance decorators
    
    * implement errorLog, passthrough and performance monitor decorator
    
    * removed console log
    
    * decouple ApiMetricsService from application
    
    * update package.lock
    
    * move metrics logs strings to enums
    
    * updated package lock file
    
    * implicit method name if not specified in log performance decorator, usage in indexer service
    
    * use log performance decorator directly
    
    * event emitter refactoring
    
    * add log performance decorators locally
    
    * rebase /w development
    
    * updated package lock
    
    * brought everything up-to-date
    
    * fix swappable settings missing dependency
    
    * update packages
    
    * skip database connection if database setting id disabled
    
    * SERVICES-863 fix expected token
    
    * SERVICES-863 fix metrics test
    
    * SERVICES-863 add EventEmitter2 provider
    
    * fix metrics
    
    * Fix integration tests
    
    * fix event listeners
    
    ---------
    
    Co-authored-by: Traian Anghel <tanghel@live.com>
    Co-authored-by: catalinfaurpaul <catalinfaurpaul@gmail.com>

--- src/graphql/interceptors/graphql.metrics.interceptor.ts
+++ src/graphql/interceptors/graphql.metrics.interceptor.ts
@@ -5,17 +5,17 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { ApiMetricsService } from 'src/common/metrics/api.metrics.service';
+import { MetricsEvents } from 'src/utils/metrics-events.constants';
+import { LogMetricsEvent } from 'src/common/entities/log.metrics.event';
 
 @Injectable()
 export class GraphQLMetricsInterceptor implements NestInterceptor {
-  private readonly apiMetricsService: ApiMetricsService;
-  constructor(apiMetricsService: ApiMetricsService) {
-    this.apiMetricsService = apiMetricsService;
-  }
+
+  constructor(private readonly eventEmitter: EventEmitter2) { }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     if (context.getType<GqlContextType>() === 'graphql') {
@@ -28,10 +28,14 @@ export class GraphQLMetricsInterceptor implements NestInterceptor {
       return next.handle().pipe(
         tap(() => {
           profiler.stop();
+
           if (parentType === 'Query') {
-            this.apiMetricsService.setGraphqlDuration(
-              fieldName,
-              profiler.duration,
+            const metricsEvent = new LogMetricsEvent();
+            metricsEvent.args = [fieldName, profiler.duration];
+
+            this.eventEmitter.emit(
+              MetricsEvents.SetGraphqlDuration,
+              metricsEvent
             );
           }
         }),
