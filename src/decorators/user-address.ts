07ebb0babfb6f8f8b51cd2d713b42d8b4e425540
AlexandruPislariu <alexandru.pislariu@elrond.com>
2023-02-06 14:22:20 +0200

    remove decorator

--- src/decorators/user-address.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator } from "@nestjs/common";
-
-export const UserAddress = createParamDecorator((_, req) => {
-  const jwt = req.args[0].jwt;
-  const nativeAuth = req.args[0].nativeAuth;
-
-  const address = jwt?.address ?? nativeAuth?.address;
-  return address;
-});
