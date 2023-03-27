8bdb3b853c81f382706c760d57cfa04bf58662e5
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2023-03-27 10:55:57 +0300

    add better logs for cache warmer

--- src/test/integration/services/e2e-init.ts
+++ src/test/integration/services/e2e-init.ts
@@ -111,7 +111,7 @@ export default class Initializer {
 
     await this.execute(description, async () => {
       const value = await promise();
-      console.log('Cache warmer set key ', key, value);
+      console.log('Cache warmer set key ', key, typeof value, (value as any).length || 0);
       await Initializer.cachingService.set(key, value, Constants.oneHour());
     });
   }
