cdcc9e01e2a31bca38a1db42bf724df18764b178
tanghel <tanghel@live.com>
2023-04-11 13:36:21 +0300

    faster github validation

--- src/crons/cache.warmer/cache.warmer.service.ts
+++ src/crons/cache.warmer/cache.warmer.service.ts
@@ -63,7 +63,7 @@ export class CacheWarmerService {
     this.configCronJob(
       'handleKeybaseAgainstKeybasePubInvalidations',
       CronExpression.EVERY_MINUTE,
-      CronExpression.EVERY_30_MINUTES,
+      CronExpression.EVERY_10_MINUTES,
       async () => await this.handleKeybaseAgainstKeybasePubInvalidations()
     );
 
