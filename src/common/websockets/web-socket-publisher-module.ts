03e510f3455b534b2b2098dfc154a74376e9ce63
tanghel <tanghel@live.com>
2023-04-25 13:22:37 +0300

    split pubsub listener in two parts
    - local cache refresher
    - websocket publisher

--- src/common/websockets/web-socket-publisher-module.ts
+++ src/common/websockets/web-socket-publisher-module.ts
@@ -1,16 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TransactionActionModule } from "src/endpoints/transactions/transaction-action/transaction.action.module";
 import { WebSocketPublisherService } from "./web-socket-publisher-service";
+import { WebSocketPublisherController } from "./web-socket-publisher-controller";
+import { DynamicModuleUtils } from "src/utils/dynamic.module.utils";
 
 @Module({
   imports: [
     TransactionActionModule,
   ],
+  controllers: [
+    WebSocketPublisherController,
+  ],
   providers: [
     WebSocketPublisherService,
+    DynamicModuleUtils.getPubSubService(),
   ],
   exports: [
     WebSocketPublisherService,
+    'PUBSUB_SERVICE',
   ],
 })
 export class WebSocketPublisherModule { }
