b5dcbd0dad1f560725488b4d4b1e458278ec6e0c
tanghel <tanghel@live.com>
2023-05-21 00:45:51 +0300

    add ticker as well

--- src/endpoints/tokens/token.service.ts
+++ src/endpoints/tokens/token.service.ts
@@ -244,6 +244,7 @@ export class TokenService {
         const tokenWithBalance: TokenWithBalance = {
           ...token,
           identifier: elasticToken.identifier,
+          ticker: elasticToken.identifier,
           balance: elasticToken.balance,
           attributes: elasticToken.data?.attributes,
           valueUsd: undefined,
