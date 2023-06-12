33cf8e4aa06315f06e3cf8b3fa04bc2a38d5f0c9
Catalin Faur <52102171+cfaur09@users.noreply.github.com>
2023-06-12 12:26:22 +0300

    add remainingUnbondPeriod (#1078)
    * add remainingUnbondPeriod
    
    * fix after review
    
    * fix node test

--- src/endpoints/nodes/node.service.ts
+++ src/endpoints/nodes/node.service.ts
@@ -19,6 +19,7 @@ import { AddressUtils } from "@multiversx/sdk-nestjs-common";
 import { CacheService } from "@multiversx/sdk-nestjs-cache";
 import { NodeSort } from "./entities/node.sort";
 import { ProtocolService } from "src/common/protocol/protocol.service";
+import { KeysService } from "../keys/keys.service";
 
 @Injectable()
 export class NodeService {
@@ -32,6 +33,7 @@ export class NodeService {
     @Inject(forwardRef(() => BlockService))
     private readonly blockService: BlockService,
     private readonly protocolService: ProtocolService,
+    private readonly keysService: KeysService
   ) { }
 
   private getIssues(node: Node, version: string | undefined): string[] {
@@ -269,6 +271,21 @@ export class NodeService {
     }
   }
 
+  private async applyNodeUnbondingPeriods(nodes: Node[]): Promise<void> {
+    const leavingNodes = nodes.filter(node => node.status === NodeStatus.leaving);
+
+    await Promise.all(leavingNodes.map(async node => {
+      const keyUnbondPeriod = await this.keysService.getKeyUnbondPeriod(node.bls);
+      node.remainingUnBondPeriod = keyUnbondPeriod?.remainingUnBondPeriod;
+    }));
+
+    for (const node of nodes) {
+      if (node.status !== NodeStatus.leaving) {
+        node.remainingUnBondPeriod = undefined;
+      }
+    }
+  }
+
   private async applyNodeStakeInfo(nodes: Node[]) {
     let addresses = nodes
       .filter(({ type }) => type === NodeType.validator)
@@ -319,6 +336,8 @@ export class NodeService {
       const auctions = await this.gatewayService.getValidatorAuctions();
       this.processAuctions(nodes, auctions);
     }
+
+    await this.applyNodeUnbondingPeriods(nodes);
 
     return nodes;
   }
