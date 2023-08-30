5f6bcfa97f9ee84267df734c309eddfb0643e5b4
catalinfaurpaul <catalinfaurpaul@gmail.com>
2023-08-30 12:39:36 +0300

    small fix

--- src/endpoints/providers/entities/provider.query.options.ts
+++ src/endpoints/providers/entities/provider.query.options.ts
@@ -9,7 +9,7 @@ export class ProviderQueryOptions {
   withIdentityInfo?: boolean = false;
 
   static applyDefaultOptions(owner: string | undefined, options: ProviderQueryOptions): ProviderQueryOptions {
-    if (options.withLatestInfo === true && owner === undefined && !owner) {
+    if (options.withLatestInfo === true && !owner) {
       throw new BadRequestException(`'withLatestInfo' can only be activated when an 'owner' filter is also applied.`);
     }
 
