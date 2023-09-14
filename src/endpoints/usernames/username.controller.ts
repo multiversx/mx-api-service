d7360d77d825117e16a2880c81d735713bd5411a
catalinfaurpaul <catalinfaurpaul@gmail.com>
2023-09-14 18:51:26 +0300

    call getAccount instead of redirect

--- src/endpoints/usernames/username.controller.ts
+++ src/endpoints/usernames/username.controller.ts
@@ -1,13 +1,18 @@
 import { AccountUsername } from './entities/account.username';
-import { Controller, Get, HttpException, HttpStatus, Param, Res } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Param, Query } from "@nestjs/common";
 import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { UsernameService } from "./username.service";
 import { NoCache } from '@multiversx/sdk-nestjs-cache';
+import { AccountService } from '../accounts/account.service';
+import { ParseBoolPipe } from '@multiversx/sdk-nestjs-common';
 
 @Controller()
 @ApiTags('usernames')
 export class UsernameController {
-  constructor(private readonly usernameService: UsernameService) { }
+  constructor(
+    private readonly usernameService: UsernameService,
+    private readonly accountService: AccountService,
+  ) { }
 
   @Get("/usernames/:username")
   @ApiOperation({ summary: 'Account details by username', description: 'Returns account details for a given username. Performs a redirect on the proper account address' })
@@ -15,12 +20,16 @@ export class UsernameController {
   @ApiNotFoundResponse({ description: 'Username not found' })
 
   @NoCache()
-  async getUsernameDetails(@Param('username') username: string, @Res() res: any): Promise<any> {
+  async getUsernameDetails(
+    @Param('username') username: string,
+    @Query('withGuardianInfo', new ParseBoolPipe) withGuardianInfo: boolean): Promise<any> {
     const address = await this.usernameService.getAddressForUsername(username);
     if (!address) {
       throw new HttpException('Account not found', HttpStatus.NOT_FOUND);
     }
 
-    return res.redirect(`/accounts/${address}`);
+    const account = await this.accountService.getAccount(address, undefined, withGuardianInfo);
+
+    return account;
   }
 }
