865e3d020c8fda36a4b08c04987ff0883e85055b
catalinfaurpaul <catalinfaurpaul@gmail.com>
2023-10-23 23:34:44 +0300

    revert caching.e2e-spec.ts

---
+++ src/test/integration/services/caching.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { Constants } from '@multiversx/sdk-nestjs-common';
+import { CacheService } from "@multiversx/sdk-nestjs-cache";
+import { Test } from '@nestjs/testing';
+import { ApiConfigModule } from 'src/common/api-config/api.config.module';
+import { DynamicModuleUtils } from 'src/utils/dynamic.module.utils';
+
+describe('Caching Service', () => {
+  let cachingService: CacheService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        ApiConfigModule,
+        DynamicModuleUtils.getCacheModule(),
+      ],
+    }).compile();
+
+    cachingService = moduleRef.get<CacheService>(CacheService);
+  });
+
+  describe('Cache Local', () => {
+    //CRUD
+    it(`should return undefined, 'test' key isn't set`, async () => {
+      const cacheValue = await cachingService.getLocal('test');
+      expect(cacheValue).toBeUndefined();
+    });
+
+    it(`should return 'test' value after key is set`, async () => {
+      await cachingService.setLocal('test', 'test', Constants.oneSecond());
+
+      const cacheValue = await cachingService.getLocal('test');
+      expect(cacheValue).toBe('test');
+    });
+
+    it(`should return 'test-update' value after key is set`, async () => {
+      await cachingService.setLocal('test', 'test-update', Constants.oneSecond());
+
+      const cacheValue = await cachingService.getLocal('test');
+      expect(cacheValue).toBe('test-update');
+    });
+
+    it(`should return undefined because key is invalidated`, async () => {
+      await cachingService.deleteInCache('test');
+
+      const cacheValue = await cachingService.getLocal('test');
+      expect(cacheValue).toBeUndefined();
+    });
+
+  });
+
+  describe('Get Or Set Cache', () => {
+    it(`should return 'test' value after key is set`, async () => {
+      // eslint-disable-next-line require-await
+      const cacheValue = await cachingService.getOrSet('test', async () => 'test', Constants.oneSecond());
+      expect(cacheValue).toBe('test');
+    });
+  });
+
+  describe('Batch process in chunks', () => {
+    const input: Array<Number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const output: Array<String> = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
+    const emptyOutput: Array<any> = Array(15).fill(null);
+    const cacheKeyFunction = (number: Number) => number.toString();
+    const handlerFunction = async (number: Number) => await number.toString();
+
+    it(`should return emptyOutput because keys aren't set`, async () => {
+      const cacheValueChunks = await cachingService.batchGetManyRemote(input.map((x) => cacheKeyFunction(x)));
+
+      expect(cacheValueChunks).toStrictEqual(emptyOutput);
+    });
+
+    it(`should return ouput keys as string`, async () => {
+      await cachingService.batchProcess(input, cacheKeyFunction, handlerFunction, Constants.oneSecond());
+
+      const cacheValueChunks = await cachingService.batchGetManyRemote(input.map((x) => cacheKeyFunction(x)));
+      expect(cacheValueChunks).toStrictEqual(output);
+    });
+  });
+});
