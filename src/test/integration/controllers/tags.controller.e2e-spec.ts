eb9f036ffeed2fe3491803e725ea153c58a391aa
Ramona Lobont <ramona.lobont@multiversx.com>
2023-12-22 10:36:44 +0200

    added new e2e tests for tags controller

--- src/test/integration/controllers/tags.controller.e2e-spec.ts
+++ src/test/integration/controllers/tags.controller.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { Tag } from 'src/endpoints/nfttags/entities/tag';
 import { PublicAppModule } from 'src/public.app.module';
 import request = require('supertest');
 import { URLSearchParams } from 'url';
@@ -24,6 +25,7 @@ describe("Tags Controller", () => {
         .get(`${path}`)
         .expect(200)
         .then(res => {
+          expect(res.body).toBeInstanceOf(Array<Tag>);
           expect(res.body).toHaveLength(25);
         });
     });
@@ -42,19 +44,61 @@ describe("Tags Controller", () => {
           expect(res.body[0].tag).toMatch('elrond');
         });
     });
-  });
 
-  describe('tags/{tag}', () => {
-    it('should return NFT tag details for a given tag', async () => {
-      const tag: string = 'elrond';
+    test.each`
+        size
+        ${25}
+        ${55}
+        ${4}
+        ${10000}`
+      (
+        `should return a list of $size items`,
+        async ({ size }) => {
+          const params = new URLSearchParams({
+            'size': size,
+          });
+          await request(app.getHttpServer())
+            .get(`${path}?${params}`)
+            .expect(200)
+            .then(res => {
+              expect(res.body).toHaveLength(size);
+            });
+        }
+      );
 
-      await request(app.getHttpServer())
-        .get(`${path}/${tag}`)
-        .expect(200)
-        .then(res => {
-          expect(res.body.tag).toMatch('elrond');
-          expect(res.body.count).toBeGreaterThanOrEqual(3500);
+
+    [
+      {
+        filter: 'search',
+        value: 'elrond',
+      },
+      {
+        filter: 'search',
+        value: 'spacefox',
+      },
+      {
+        filter: 'search',
+        value: 'space',
+      },
+      {
+        filter: 'search',
+        value: 'game',
+      },
+    ].forEach(({ filter, value }) => {
+      describe(`when filter ${filter} is applied`, () => {
+        it(`should return a list of all NFT tags based on tag name ${value}`, async () => {
+          await request(app.getHttpServer())
+            .get(`${path}?${filter}=${value}`)
+            .expect(200)
+            .then(res => {
+              expect(res.body).toBeDefined();
+              expect(res.body).toBeInstanceOf(Array<Tag>);
+              for (let i = 0; i < res.body.length; i++) {
+                expect(res.body[i].tag).toMatch(value);
+              }
+            });
         });
+      });
     });
   });
 
@@ -72,13 +116,35 @@ describe("Tags Controller", () => {
       const params = new URLSearchParams({
         'search': 'elrond',
       });
-
       await request(app.getHttpServer())
         .get(`${path}/count?${params}`)
         .expect(200)
         .then(res => {
           expect(+res.text).toBeGreaterThanOrEqual(30);
         });
+    });
+
+    describe('tags/{tag}', () => {
+      it('should return NFT tag details for a given tag', async () => {
+        const tag: string = 'elrond';
+        await request(app.getHttpServer())
+          .get(`${path}/${tag}`)
+          .expect(200)
+          .then(res => {
+            expect(res.body.tag).toMatch('elrond');
+            expect(res.body.count).toBeGreaterThanOrEqual(3500);
+          });
+      });
+
+      it('should return 404 "Nft tag not found"', async () => {
+        const tag: string = 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv';
+        await request(app.getHttpServer())
+          .get(`${path}/${tag}`)
+          .expect(404)
+          .then(res => {
+            expect(res.body.message).toStrictEqual("Nft tag not found");
+          });
+      });
     });
   });
 
