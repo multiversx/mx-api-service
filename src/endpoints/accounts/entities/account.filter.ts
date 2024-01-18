ccadcb59e46537baac76f771f03046886333198a
tanghel <tanghel@live.com>
2024-01-18 18:29:34 +0200

    improvements

--- src/endpoints/accounts/entities/account.filter.ts
+++ src/endpoints/accounts/entities/account.filter.ts
@@ -5,10 +5,21 @@ export class AccountFilter {
   constructor(init?: Partial<AccountFilter>) {
     Object.assign(this, init);
   }
+
   ownerAddress?: string;
 
   sort?: AccountSort;
   order?: SortOrder;
   isSmartContract?: boolean;
   withOwnerAssets?: boolean;
+  withDetails?: boolean;
+
+  isSet(): boolean {
+    return this.ownerAddress !== undefined ||
+      this.sort !== undefined ||
+      this.order !== undefined ||
+      this.isSmartContract !== undefined ||
+      this.withOwnerAssets !== undefined ||
+      this.withDetails !== undefined;
+  }
 }
