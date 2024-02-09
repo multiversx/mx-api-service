f0d5069abcd522520de30fb4ca1661e445f2f0c1
tanghel <tanghel@live.com>
2024-02-09 18:35:30 +0400

    check for topic length

--- src/endpoints/transactions/transaction.get.service.ts
+++ src/endpoints/transactions/transaction.get.service.ts
@@ -142,6 +142,7 @@ export class TransactionGetService {
     const asyncCallbackEvents = transferValueOnlyEvents.filter(x => x.data == asyncCallbackEncoded);
 
     if (backTransferEvents.length === 1 && asyncCallbackEvents.length === 1 &&
+      backTransferEvents[0].topics.length > 1 &&
       JSON.stringify(backTransferEvents[0].topics) === JSON.stringify(asyncCallbackEvents[0].topics)
     ) {
       backTransferEvents[0].topics[0] = '0';
