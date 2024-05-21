4cea36e681d177497089fc5c9d52c5b23457c13a
tanghel <tanghel@live.com>
2024-05-21 19:33:59 +0300

    save node auctions in node refresh function

--- src/crons/cache.warmer/cache.warmer.service.ts
+++ src/crons/cache.warmer/cache.warmer.service.ts
@@ -123,6 +123,13 @@ export class CacheWarmerService {
   @Lock({ name: 'Node invalidations', verbose: true })
   async handleNodeInvalidations() {
     const nodes = await this.nodeService.getAllNodesRaw();
+
+    if (this.apiConfigService.isStakingV4Enabled()) {
+      const auctions = await this.gatewayService.getValidatorAuctions();
+
+      await this.nodeService.processAuctions(nodes, auctions);
+    }
+
     await this.invalidateKey(CacheInfo.Nodes.key, nodes, CacheInfo.Nodes.ttl);
   }
 
@@ -135,12 +142,6 @@ export class CacheWarmerService {
 
   @Lock({ name: 'Node auction invalidations', verbose: true })
   async handleNodeAuctionInvalidations() {
-    const nodes = await this.nodeService.getAllNodes();
-    const auctions = await this.gatewayService.getValidatorAuctions();
-
-    await this.nodeService.processAuctions(nodes, auctions);
-    await this.invalidateKey(CacheInfo.Nodes.key, nodes, CacheInfo.Nodes.ttl);
-
     const nodesAuctions = await this.nodeService.getAllNodesAuctionsRaw();
     await this.invalidateKey(CacheInfo.NodesAuctions.key, nodesAuctions, CacheInfo.NodesAuctions.ttl);
   }
