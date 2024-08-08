28e26583e879e50e5fece858053d4af96c669cb0
tanghel <tanghel@live.com>
2024-08-08 14:55:44 +0300

    support patch versions for nodes as well

--- src/endpoints/nodes/node.service.ts
+++ src/endpoints/nodes/node.service.ts
@@ -666,7 +666,7 @@ export class NodeService {
       const node: Node = new Node({
         bls,
         name,
-        version: version ? (version.includes('-rc') ? version.split('-').slice(0, 2).join('-').split('/')[0] : version.split('-')[0].split('/')[0]) : '',
+        version: version ? (version.includes('-rc') || version.includes('-patch') ? version.split('-').slice(0, 2).join('-').split('/')[0] : version.split('-')[0].split('/')[0]) : '',
         identity: identity && identity !== '' ? identity.toLowerCase() : identity,
         rating: parseFloat(parseFloat(rating).toFixed(2)),
         tempRating: parseFloat(parseFloat(tempRating).toFixed(2)),
