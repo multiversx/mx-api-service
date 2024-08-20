33ba74704727eb1a7e93ec3830f1c4d2c64d1396
Nicolae Mogage <mogagenicolae@yahoo.com>
2024-08-20 10:22:07 +0300

    Add preload for all tokens/nodes

--- k6/preload.js
+++ k6/preload.js
@@ -3,6 +3,8 @@ import http from 'k6/http';
 const BASE_URL = 'http://localhost:3001';
 
 export default function () {
-    http.get(`${BASE_URL}/tokens`);
-    http.get(`${BASE_URL}/nodes`);
+    const numberofTokens = http.get(`${BASE_URL}/tokens/count`);
+    http.get(`${BASE_URL}/tokens?size=${numberofTokens}`);
+    const numberofNodes = http.get(`${BASE_URL}/nodes/count`);
+    http.get(`${BASE_URL}/nodes?size=${numberofNodes}`);
 }
