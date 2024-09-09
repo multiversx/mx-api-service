9b6a1db67ab94c0d8840821ce73e935682987f4b
cfaur09 <catalinfaurpaul@gmail.com>
2024-09-09 17:52:58 +0300

    Update applications.spec.ts

--- src/test/unit/services/applications.spec.ts
+++ src/test/unit/services/applications.spec.ts
@@ -5,108 +5,108 @@ import { ApplicationService } from "src/endpoints/applications/application.servi
 import { ApplicationFilter } from "src/endpoints/applications/entities/application.filter";
 
 describe('ApplicationService', () => {
-	let service: ApplicationService;
-	let indexerService: ElasticIndexerService;
+    let service: ApplicationService;
+    let indexerService: ElasticIndexerService;
 
-	beforeEach(async () => {
-		const module: TestingModule = await Test.createTestingModule({
-			providers: [
-				ApplicationService,
-				{
-					provide: ElasticIndexerService,
-					useValue: {
-						getApplications: jest.fn(),
-						getApplicationCount: jest.fn(),
-					},
-				},
-			],
-		}).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ApplicationService,
+                {
+                    provide: ElasticIndexerService,
+                    useValue: {
+                        getApplications: jest.fn(),
+                        getApplicationCount: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-		service = module.get<ApplicationService>(ApplicationService);
-		indexerService = module.get<ElasticIndexerService>(ElasticIndexerService);
-	});
+        service = module.get<ApplicationService>(ApplicationService);
+        indexerService = module.get<ElasticIndexerService>(ElasticIndexerService);
+    });
 
-	it('should be defined', () => {
-		expect(service).toBeDefined();
-	});
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 
-	describe('getApplications', () => {
-		it('should return an array of applications', async () => {
-			const indexResult = [
-				{
-					address: 'erd1qqqqqqqqqqqqqpgq8372f63glekg7zl22tmx7wzp4drql25r6avs70dmp0',
-					deployer: 'erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams',
-					currentOwner: 'erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams',
-					initialCodeHash: 'kDh8hR9vyceELMUuy6JdAg0X90+ZaLeyVQS6tPbY82s=',
-					timestamp: 1724955216,
-				},
-				{
-					address: 'erd1qqqqqqqqqqqqqpgquc4v0pujmewzr26tm2gtawmsq4vsrm4mwmfs459g65',
-					deployer: 'erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v',
-					currentOwner: 'erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v',
-					initialCodeHash: 'kDiPwFRJhcB7TmeBbQvw1uWQ8vuhRSU6XF71Z4OybeQ=',
-					timestamp: 1725017514,
-				},
-			];
+    describe('getApplications', () => {
+        it('should return an array of applications', async () => {
+            const indexResult = [
+                {
+                    address: 'erd1qqqqqqqqqqqqqpgq8372f63glekg7zl22tmx7wzp4drql25r6avs70dmp0',
+                    deployer: 'erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams',
+                    currentOwner: 'erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams',
+                    initialCodeHash: 'kDh8hR9vyceELMUuy6JdAg0X90+ZaLeyVQS6tPbY82s=',
+                    timestamp: 1724955216,
+                },
+                {
+                    address: 'erd1qqqqqqqqqqqqqpgquc4v0pujmewzr26tm2gtawmsq4vsrm4mwmfs459g65',
+                    deployer: 'erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v',
+                    currentOwner: 'erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v',
+                    initialCodeHash: 'kDiPwFRJhcB7TmeBbQvw1uWQ8vuhRSU6XF71Z4OybeQ=',
+                    timestamp: 1725017514,
+                },
+            ];
 
-			jest.spyOn(indexerService, 'getApplications').mockResolvedValue(indexResult);
+            jest.spyOn(indexerService, 'getApplications').mockResolvedValue(indexResult);
 
-			const queryPagination = new QueryPagination;
-			const filter = new ApplicationFilter;
-			const result = await service.getApplications(queryPagination, filter);
+            const queryPagination = new QueryPagination;
+            const filter = new ApplicationFilter;
+            const result = await service.getApplications(queryPagination, filter);
 
-			expect(indexerService.getApplications)
-				.toHaveBeenCalledWith(filter, queryPagination);
-			expect(indexerService.getApplications)
-				.toHaveBeenCalledTimes(1);
+            expect(indexerService.getApplications)
+                .toHaveBeenCalledWith(filter, queryPagination);
+            expect(indexerService.getApplications)
+                .toHaveBeenCalledTimes(1);
 
-			expect(result).toEqual([
-				{
-					contract: "erd1qqqqqqqqqqqqqpgq8372f63glekg7zl22tmx7wzp4drql25r6avs70dmp0",
-					deployer: "erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams",
-					owner: "erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams",
-					codeHash: "kDh8hR9vyceELMUuy6JdAg0X90+ZaLeyVQS6tPbY82s=",
-					timestamp: 1724955216,
-				},
-				{
-					contract: "erd1qqqqqqqqqqqqqpgquc4v0pujmewzr26tm2gtawmsq4vsrm4mwmfs459g65",
-					deployer: "erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v",
-					owner: "erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v",
-					codeHash: "kDiPwFRJhcB7TmeBbQvw1uWQ8vuhRSU6XF71Z4OybeQ=",
-					timestamp: 1725017514,
-				},
-			]);
-		});
+            expect(result).toEqual([
+                {
+                    contract: "erd1qqqqqqqqqqqqqpgq8372f63glekg7zl22tmx7wzp4drql25r6avs70dmp0",
+                    deployer: "erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams",
+                    owner: "erd1j770k2n46wzfn5g63gjthhqemu9r23n9tp7seu95vpz5gk5s6avsk5aams",
+                    codeHash: "kDh8hR9vyceELMUuy6JdAg0X90+ZaLeyVQS6tPbY82s=",
+                    timestamp: 1724955216,
+                },
+                {
+                    contract: "erd1qqqqqqqqqqqqqpgquc4v0pujmewzr26tm2gtawmsq4vsrm4mwmfs459g65",
+                    deployer: "erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v",
+                    owner: "erd1szcgm7vq3tmyxfgd4wd2k2emh59az8jq5jjpj9799a0k59u0wmfss4vw3v",
+                    codeHash: "kDiPwFRJhcB7TmeBbQvw1uWQ8vuhRSU6XF71Z4OybeQ=",
+                    timestamp: 1725017514,
+                },
+            ]);
+        });
 
-		it('should return an empty array if no applications are found', async () => {
-			jest.spyOn(indexerService, 'getApplications').mockResolvedValue([]);
+        it('should return an empty array if no applications are found', async () => {
+            jest.spyOn(indexerService, 'getApplications').mockResolvedValue([]);
 
-			const queryPagination = new QueryPagination;
-			const filter = new ApplicationFilter;
-			const result = await service.getApplications(queryPagination, filter);
+            const queryPagination = new QueryPagination;
+            const filter = new ApplicationFilter;
+            const result = await service.getApplications(queryPagination, filter);
 
-			expect(indexerService.getApplications)
-				.toHaveBeenCalledWith(filter, queryPagination);
-			expect(indexerService.getApplications)
-				.toHaveBeenCalledTimes(1);
+            expect(indexerService.getApplications)
+                .toHaveBeenCalledWith(filter, queryPagination);
+            expect(indexerService.getApplications)
+                .toHaveBeenCalledTimes(1);
 
-			expect(result).toEqual([]);
-		});
-	});
+            expect(result).toEqual([]);
+        });
+    });
 
-	describe('getApplicationsCount', () => {
-		it('should return total applications count', async () => {
-			jest.spyOn(indexerService, 'getApplicationCount').mockResolvedValue(2);
+    describe('getApplicationsCount', () => {
+        it('should return total applications count', async () => {
+            jest.spyOn(indexerService, 'getApplicationCount').mockResolvedValue(2);
 
-			const filter = new ApplicationFilter;
-			const result = await service.getApplicationsCount(filter);
+            const filter = new ApplicationFilter;
+            const result = await service.getApplicationsCount(filter);
 
-			expect(indexerService.getApplicationCount)
-				.toHaveBeenCalledWith(filter);
-			expect(indexerService.getApplicationCount)
-				.toHaveBeenCalledTimes(1);
+            expect(indexerService.getApplicationCount)
+                .toHaveBeenCalledWith(filter);
+            expect(indexerService.getApplicationCount)
+                .toHaveBeenCalledTimes(1);
 
-			expect(result).toEqual(2);
-		});
-	});
+            expect(result).toEqual(2);
+        });
+    });
 });
