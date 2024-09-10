224baeef596b8902c817cbcfee1bd4ea9323317d
Catalin Faur <52102171+cfaur09@users.noreply.github.com>
2024-09-10 10:14:36 +0300

    fetch token price from mx-assets priceSource.url (#1290)
    * fetch token price from mx-assets priceSource.url
    
    * update extractData methods
    
    * use apiService.get to fetch result data
    
    * update list of providers
    
    * update key
    
    * review & adjustments
    
    * removed v2 from allEsdtTokens
    
    ---------
    
    Co-authored-by: tanghel <tanghel@live.com>

--- src/common/assets/entities/token.assets.price.source.ts
+++ src/common/assets/entities/token.assets.price.source.ts
@@ -7,4 +7,12 @@ export class TokenAssetsPriceSource {
   @Field(() => String, { description: 'Type of price source', nullable: true })
   @ApiProperty({ type: TokenAssetsPriceSourceType, nullable: true })
   type: TokenAssetsPriceSourceType | undefined = undefined;
+
+  @Field(() => String, { description: 'URL of price source in case of customUrl type', nullable: true })
+  @ApiProperty({ type: String, nullable: true })
+  url: string | undefined = undefined;
+
+  @Field(() => String, { description: '(Optional) path to fetch the price info in case of customUrl type', nullable: true })
+  @ApiProperty({ type: String, nullable: true })
+  path: string | undefined = undefined;
 }
