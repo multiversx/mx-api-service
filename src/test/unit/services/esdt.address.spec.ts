c7af1d58b8d053e6f9f9f4f0a76586fc6da51fbc
cfaur09 <catalinfaurpaul@gmail.com>
2024-09-10 12:32:29 +0300

    fix specs

--- src/test/unit/services/esdt.address.spec.ts
+++ src/test/unit/services/esdt.address.spec.ts
@@ -1,28 +1,21 @@
 import { CacheService } from "@multiversx/sdk-nestjs-cache";
 import { MetricsService } from "@multiversx/sdk-nestjs-monitoring";
-import { BadRequestException } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
 import { AssetsService } from "src/common/assets/assets.service";
-import { QueryPagination } from "src/common/entities/query.pagination";
 import { GatewayService } from "src/common/gateway/gateway.service";
 import { IndexerService } from "src/common/indexer/indexer.service";
 import { ProtocolService } from "src/common/protocol/protocol.service";
 import { CollectionService } from "src/endpoints/collections/collection.service";
 import { CollectionFilter } from "src/endpoints/collections/entities/collection.filter";
-import { NftCollection } from "src/endpoints/collections/entities/nft.collection";
 import { EsdtAddressService } from "src/endpoints/esdt/esdt.address.service";
 import { EsdtService } from "src/endpoints/esdt/esdt.service";
 import { NftFilter } from "src/endpoints/nfts/entities/nft.filter";
-import { NftType } from "src/endpoints/nfts/entities/nft.type";
 import { NftExtendedAttributesService } from "src/endpoints/nfts/nft.extendedattributes.service";
-import { TokenAssetStatus } from "src/endpoints/tokens/entities/token.asset.status";
 
 describe('EsdtAddressService', () => {
   let service: EsdtAddressService;
   let indexerService: IndexerService;
-  let apiConfigService: ApiConfigService;
-  let collectionService: CollectionService;
   let cacheService: CacheService;
   let metricsService: MetricsService;
   let protocolService: ProtocolService;
@@ -45,7 +38,6 @@ describe('EsdtAddressService', () => {
           useValue:
           {
             getExternalMediaUrl: jest.fn(),
-            getIsIndexerV3FlagActive: jest.fn(),
           },
         },
         {
@@ -107,8 +99,6 @@ describe('EsdtAddressService', () => {
 
     service = moduleRef.get<EsdtAddressService>(EsdtAddressService);
     indexerService = moduleRef.get<IndexerService>(IndexerService);
-    apiConfigService = moduleRef.get<ApiConfigService>(ApiConfigService);
-    collectionService = moduleRef.get<CollectionService>(CollectionService);
     cacheService = moduleRef.get<CacheService>(CacheService);
     metricsService = moduleRef.get<MetricsService>(MetricsService);
     protocolService = moduleRef.get<ProtocolService>(ProtocolService);
@@ -212,129 +202,6 @@ describe('EsdtAddressService', () => {
   });
 
   describe('EsdtAddressService - getCollectionsForAddress', () => {
-    const indexerCollectionMock = {
-      _id: 'XDAY23TEAM-f7a346',
-      name: 'XDAY2023TEAM',
-      ticker: 'XDAY23TEAM',
-      token: 'XDAY23TEAM-f7a346',
-      issuer: 'erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp',
-      currentOwner: 'erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp',
-      numDecimals: 0,
-      type: 'NonFungibleESDT',
-      timestamp: 1696923978,
-      ownersHistory: [
-        {
-          address: 'erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp',
-          timestamp: 1696923978,
-        },
-      ],
-      properties: {
-        canMint: false,
-        canBurn: false,
-        canUpgrade: true,
-        canTransferNFTCreateRole: true,
-        canAddSpecialRoles: true,
-        canPause: true,
-        canFreeze: true,
-        canWipe: true,
-        canChangeOwner: false,
-        canCreateMultiShard: false,
-      },
-      nft_hasTraitSummary: true,
-      roles: {
-        ESDTRoleNFTCreate: ['erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp'],
-        ESDTTransferRole: ['erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp'],
-        ESDTRoleNFTBurn: ['erd178ah2z70a442g9hrt39w2ld67lav62jq72gzp3r9tu5egz4hr4cswr5unp'],
-      },
-      nft_hasRarities: false,
-      api_holderCount: 101,
-      api_isVerified: true,
-      api_nftCount: 131,
-    };
-
-    const propertiesToCollectionsMock: NftCollection = {
-      collection: 'XDAY23TEAM-f7a346',
-      type: NftType.NonFungibleESDT,
-      subType: undefined,
-      name: 'xPortalAchievements',
-      ticker: 'XDAY23TEAM',
-      owner: 'erd1lpc6wjh2hav6q50p8y6a44r2lhtnseqksygakjfgep6c9uduchkqphzu6t',
-      timestamp: 0,
-      canFreeze: true,
-      canWipe: true,
-      canPause: true,
-      canTransferNftCreateRole: true,
-      canChangeOwner: false,
-      canUpgrade: false,
-      canAddSpecialRoles: false,
-      decimals: undefined,
-      assets: {
-        website: 'https://xday.com',
-        description:
-          'Test description.',
-        status: TokenAssetStatus.active,
-        pngUrl: 'https://media.elrond.com/tokens/asset/XDAY23TEAM-f7a346/logo.png',
-        name: '',
-        svgUrl: 'https://media.elrond.com/tokens/asset/XDAY23TEAM-f7a346/logo.svg',
-        extraTokens: [''],
-        ledgerSignature: '',
-        priceSource: undefined,
-        preferredRankAlgorithm: undefined,
-        lockedAccounts: undefined,
-      },
-      scamInfo: undefined,
-      traits: [],
-      auctionStats: undefined,
-      isVerified: undefined,
-      holderCount: undefined,
-      nftCount: undefined,
-    };
-
-    it('should throw BadRequestException when IndexerV3Flag is inactive and specific filters are used', async () => {
-      jest.spyOn(apiConfigService, 'getIsIndexerV3FlagActive').mockReturnValue(false);
-
-      const address = 'erd1qga7ze0l03chfgru0a32wxqf2226nzrxnyhzer9lmudqhjgy7ycqjjyknz';
-      const filter = { canCreate: true };
-
-      await expect(service.getCollectionsForAddress(address, filter, new QueryPagination())).rejects.toThrow(BadRequestException);
-    });
-
-    it('should return collections for a given address with IndexerV3Flag active', async () => {
-      jest.spyOn(apiConfigService, 'getIsIndexerV3FlagActive').mockReturnValue(true);
-      jest.spyOn(indexerService, 'getNftCollections').mockResolvedValue([indexerCollectionMock]);
-      jest.spyOn(collectionService, 'applyPropertiesToCollections').mockResolvedValue([propertiesToCollectionsMock]);
-
-      const address = 'erd1qga7ze0l03chfgru0a32wxqf2226nzrxnyhzer9lmudqhjgy7ycqjjyknz';
-      const filter = new CollectionFilter();
-      const pagination = new QueryPagination();
-
-      const results = await service.getCollectionsForAddress(address, filter, pagination);
-
-      expect(results).toHaveLength(1);
-      expect(apiConfigService.getIsIndexerV3FlagActive).toHaveBeenCalled();
-      expect(apiConfigService.getIsIndexerV3FlagActive).toHaveBeenCalledTimes(2);
-      expect(indexerService.getNftCollections).toHaveBeenCalledWith(pagination, filter, address);
-      expect(indexerService.getNftCollections).toHaveBeenCalledTimes(1);
-      expect(collectionService.applyPropertiesToCollections).toHaveBeenCalled();
-    });
-
-    it('should correctly apply roles to collections for a given address with IndexerV3Flag active', async () => {
-      jest.spyOn(apiConfigService, 'getIsIndexerV3FlagActive').mockReturnValue(true);
-      jest.spyOn(indexerService, 'getNftCollections').mockResolvedValue([indexerCollectionMock]);
-      jest.spyOn(collectionService, 'applyPropertiesToCollections').mockResolvedValue([propertiesToCollectionsMock]);
-
-      const address = 'erd1qga7ze0l03chfgru0a32wxqf2226nzrxnyhzer9lmudqhjgy7ycqjjyknz';
-      const filter = new CollectionFilter();
-      const pagination = new QueryPagination();
-
-      const results = await service.getCollectionsForAddress(address, filter, pagination);
-
-      expect(results).toHaveLength(1);
-      const firstCollection = results[0];
-      expect(firstCollection.canTransfer).toBe(false);
-      expect(firstCollection.role.canBurn).toBe(false);
-    });
-
     it('should return cached ESDTs for a given address', async () => {
       const address = 'erd1qga7ze0l03chfgru0a32wxqf2226nzrxnyhzer9lmudqhjgy7ycqjjyknz';
       const cachedEsdts = {
