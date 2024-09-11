47ecb8c3a379a921276ca7d7a6708d9b6befae5c
cfaur09 <catalinfaurpaul@gmail.com>
2024-09-11 10:54:05 +0300

    Update preload.js

--- k6/preload.js
+++ k6/preload.js
@@ -3,8 +3,10 @@ import http from 'k6/http';
 const BASE_URL = 'http://localhost:3001';
 
 export default function preloadCache() {
-    const numberofTokens = http.get(`${BASE_URL}/tokens/count`);
-    http.get(`${BASE_URL}/tokens?size=${numberofTokens}`);
-    const numberofNodes = http.get(`${BASE_URL}/nodes/count`);
-    http.get(`${BASE_URL}/nodes?size=${numberofNodes}`);
+    const numberOfTokens = http.get(`${BASE_URL}/tokens/count`);
+    http.get(`${BASE_URL}/tokens?size=${numberOfTokens}`);
+    const numberOfNodes = http.get(`${BASE_URL}/nodes/count`);
+    http.get(`${BASE_URL}/nodes?size=${numberOfNodes}`);
+    const numberOfTags = http.get(`${BASE_URL}/tags/count`);
+    http.get(`${BASE_URL}/tags?size=${numberOfTags}`);
 }
