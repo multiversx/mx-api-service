363c5400360dbe0262c1b2f3e055b45b3405f311
cfaur09 <catalinfaurpaul@gmail.com>
2024-10-29 18:38:37 +0200

    add events support

---
+++ src/endpoints/events/events.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { IndexerService } from '../../common/indexer/indexer.service';
+import { QueryPagination } from '../../common/entities/query.pagination';
+import { Events } from './entities/events';
+import { EventsFilter } from './entities/events.filter';
+
+@Injectable()
+export class EventsService {
+  constructor(
+    private readonly indexerService: IndexerService,
+  ) { }
+
+  async getEvents(pagination: QueryPagination, filter: EventsFilter): Promise<Events[]> {
+    const indexResults = await this.indexerService.getEvents(pagination, filter);
+
+    if (!indexResults) {
+      return [];
+    }
+
+    return indexResults;
+  }
+
+  async getEvent(txHash: string): Promise<Events | undefined> {
+    const result = await this.indexerService.getEvent(txHash);
+    return result ? result : undefined;
+  }
+
+  async getEventsCount(filter: EventsFilter): Promise<number> {
+    return await this.indexerService.getEventsCount(filter);
+  }
+}
