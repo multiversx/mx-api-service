8621be4f98cbc21031657933f96dd279904d715b
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2024-11-14 13:20:20 +0200

    API-70: remove module import

--- src/endpoints/pool/pool.module.ts
+++ src/endpoints/pool/pool.module.ts
@@ -1,11 +1,7 @@
 import { Module } from "@nestjs/common";
 import { PoolService } from "./pool.service";
-import { ProtocolModule } from "../../common/protocol/protocol.module";
 
 @Module({
-  imports: [
-    ProtocolModule,
-  ],
   providers: [
     PoolService,
   ],
