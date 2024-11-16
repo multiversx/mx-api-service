fce09a06e952d11a5b8d49fb60a40462a719c5d9
cfaur09 <catalinfaurpaul@gmail.com>
2024-11-16 21:37:26 +0200

    Update tokens.cs-e2e.ts

--- src/test/chain-simulator/tokens.cs-e2e.ts
+++ src/test/chain-simulator/tokens.cs-e2e.ts
@@ -48,7 +48,6 @@ describe('Tokens e2e tests with chain simulator', () => {
     it('should return status code 200 and a list of tokens', async () => {
       const response = await axios.get(`${API_SERVICE_URL}/tokens`);
       const tokens = response.data;
-      console.log(tokens);
 
       expect(response.status).toBe(200);
       expect(tokens).toBeInstanceOf(Array);
@@ -60,6 +59,7 @@ describe('Tokens e2e tests with chain simulator', () => {
         `${API_SERVICE_URL}/tokens?name=${tokenName}`,
       );
       const tokens = response.data;
+
       expect(response.status).toBe(200);
       for (const token of tokens) {
         expect(token.name).toBe(tokenName);
@@ -73,6 +73,7 @@ describe('Tokens e2e tests with chain simulator', () => {
         `${API_SERVICE_URL}/tokens?identifier=${tokenIdentifier}`,
       );
       const tokens = response.data;
+
       expect(response.status).toBe(200);
       for (const token of tokens) {
         expect(token.identifier).toBe(tokenIdentifier);
@@ -82,9 +83,150 @@ describe('Tokens e2e tests with chain simulator', () => {
     it('should support pagination and return 2 tokens', async () => {
       const response = await axios.get(`${API_SERVICE_URL}/tokens?size=2`);
       const tokens = response.data;
+
       expect(response.status).toBe(200);
       expect(tokens.length).toBe(2);
       expect(Array.isArray(tokens)).toBe(true);
+    });
+
+    it('should return filtered tokens by type', async () => {
+      const tokenType = 'FungibleESDT';
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens?type=${tokenType}`,
+      );
+      const tokens = response.data;
+
+      expect(response.status).toBe(200);
+      for (const token of tokens) {
+        expect(token.type).toBe(tokenType);
+      }
+    });
+
+    it('should return filtered tokens by multiple identifiers', async () => {
+      const fetchTokens = await axios.get(`${API_SERVICE_URL}/tokens`);
+      const identifiers = fetchTokens.data.map(
+        (token: any) => token.identifier,
+      );
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens?identifiers=${identifiers.join(',')}`,
+      );
+      const tokens = response.data;
+
+      expect(response.status).toBe(200);
+      for (const token of tokens) {
+        expect(identifiers).toContain(token.identifier);
+      }
+    });
+
+    it('should return filtered tokens by search term', async () => {
+      const searchTerm = 'Token1';
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens?search=${searchTerm}`,
+      );
+      const tokens = response.data;
+
+      expect(response.status).toBe(200);
+      for (const token of tokens) {
+        expect(token.name).toContain(searchTerm);
+      }
+    });
+  });
+
+  describe('GET /tokens/count', () => {
+    it('should return status code 200 and the total count of tokens', async () => {
+      const response = await axios.get(`${API_SERVICE_URL}/tokens/count`);
+      const count = response.data;
+
+      expect(response.status).toBe(200);
+      expect(count).toBeGreaterThanOrEqual(0);
+      expect(typeof count).toBe('number');
+    });
+
+    it('should return filtered token count by name', async () => {
+      const tokenName = 'Token1';
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens/count?name=${tokenName}`,
+      );
+      const count = response.data;
+
+      expect(response.status).toBe(200);
+      expect(typeof count).toBe('number');
+    });
+
+    it('should return filtered token count by search term', async () => {
+      const searchTerm = 'Token';
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens/count?search=${searchTerm}`,
+      );
+      const count = response.data;
+
+      expect(response.status).toBe(200);
+      expect(typeof count).toBe('number');
+    });
+  });
+
+  describe('GET /tokens/:identifier', () => {
+    it('should return status code 200 and token details', async () => {
+      const tokensResponse = await axios.get(
+        `${API_SERVICE_URL}/tokens?size=1`,
+      );
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens/${tokensResponse.data[0].identifier}`,
+      );
+      const token = response.data;
+
+      expect(response.status).toBe(200);
+      expect(token).toHaveProperty(
+        'identifier',
+        tokensResponse.data[0].identifier,
+      );
+    });
+
+    it('should return status code 400 for non-existent token', async () => {
+      const nonExistentTokenIdentifier = 'NON_EXISTENT_TOKEN';
+      try {
+        await axios.get(
+          `${API_SERVICE_URL}/tokens/${nonExistentTokenIdentifier}`,
+        );
+      } catch (error: any) {
+        expect(error.response.status).toBe(400);
+      }
+    });
+
+    it('should return token details with denominated supply (number)', async () => {
+      const tokensResponse = await axios.get(
+        `${API_SERVICE_URL}/tokens?size=1`,
+      );
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens/${tokensResponse.data[0].identifier}?denominated=true`,
+      );
+      const token = response.data;
+
+      expect(response.status).toBe(200);
+      expect(token).toHaveProperty(
+        'identifier',
+        tokensResponse.data[0].identifier,
+      );
+      expect(token).toHaveProperty('supply');
+      expect(typeof token.supply).toStrictEqual('number');
+    });
+
+    it('should return token details with supply (string)', async () => {
+      const tokensResponse = await axios.get(
+        `${API_SERVICE_URL}/tokens?size=1`,
+      );
+      const response = await axios.get(
+        `${API_SERVICE_URL}/tokens/${tokensResponse.data[0].identifier}?denominated=false`,
+      );
+      const token = response.data;
+
+      expect(response.status).toBe(200);
+      expect(token).toHaveProperty(
+        'identifier',
+        tokensResponse.data[0].identifier,
+      );
+      expect(token).toHaveProperty('supply');
+      expect(typeof token.supply).toStrictEqual('string');
     });
   });
 });
