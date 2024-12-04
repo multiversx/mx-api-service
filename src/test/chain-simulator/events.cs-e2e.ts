176a3a4020f415647150e3c9d4a6f2fdd0959bea
cfaur09 <catalinfaurpaul@gmail.com>
2024-12-04 21:47:38 +0200

    Create events.cs-e2e.ts

---
+++ src/test/chain-simulator/events.cs-e2e.ts
@@ -0,0 +1,105 @@
+import { config } from "./config/env.config";
+import { ChainSimulatorUtils } from "./utils/test.utils";
+import axios from "axios";
+
+describe('Events e2e tests with chain simulator', () => {
+  beforeAll(async () => {
+    await ChainSimulatorUtils.waitForEpoch(2);
+  });
+
+  describe('GET /events', () => {
+    it('should return status code 200 and a list of events', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+    });
+
+    it('should support pagination', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events?from=0&size=2`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+      expect(response.data.length).toStrictEqual(2);
+    });
+
+    it('should return events with expected properties', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events`);
+      const event = response.data[0];
+      expect(response.status).toBe(200);
+
+      const requiredProps = [
+        'txHash',
+        'logAddress',
+        'identifier',
+        'address',
+        'topics',
+        'shardID',
+        'txOrder',
+        'order',
+        'timestamp',
+      ];
+
+      for (const prop of requiredProps) {
+        expect(event).toHaveProperty(prop);
+      }
+
+      expect(event.topics).toBeInstanceOf(Array);
+    });
+
+    it('should support filtering by address', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events?address=${config.aliceAddress}`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+      expect(response.data.length).toBeGreaterThan(1);
+    });
+
+    it('should support filtering by identifier', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events?identifier=completedTxEvent`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+      expect(response.data.length).toBeGreaterThan(1);
+    });
+
+    // TODO: fix this
+    it.skip('should support filtering by txHash', async () => {
+      const events = await axios.get(`${config.apiServiceUrl}/events`);
+      const txHash = events.data[0].txHash;
+      const response = await axios.get(`${config.apiServiceUrl}/events?txHash=${txHash}`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+      expect(response.data.length).toBeGreaterThan(1);
+    });
+
+    it('should support filtering by shard', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events?shard=0`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeInstanceOf(Array);
+      expect(response.data.length).toBeGreaterThan(1);
+    });
+  });
+
+  describe('GET /events/count', () => {
+    it('should return status code 200 and the number of events', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events/count`);
+      expect(response.status).toBe(200);
+      expect(typeof response.data).toBe('number');
+    });
+
+    it('should return the number of events with the given address', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events/count?address=${config.aliceAddress}`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeGreaterThan(1);
+    });
+
+    it('should return the number of events with the given identifier', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events/count?identifier=completedTxEvent`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeGreaterThan(1);
+    });
+
+    it('should return the number of events with the given shard', async () => {
+      const response = await axios.get(`${config.apiServiceUrl}/events/count?shard=0`);
+      expect(response.status).toBe(200);
+      expect(response.data).toBeGreaterThan(1);
+    });
+  });
+});
