0937b6570d73d9fcce18d0dc48f7884c586cdc1e
Catalin Faur <52102171+cfaur09@users.noreply.github.com>
2025-01-08 16:56:24 +0200

    websockets transactions events metrics (#1429)
    * Enhance ApiMetricsService with new transaction and batch update counters.
    
    * Updated WebSocketPublisherController to emit metrics events after processing transactions and batch updates.
    
    * Update web.socket controller spec

--- src/test/unit/controllers/web.socket.publiser.controller.spec.ts
+++ src/test/unit/controllers/web.socket.publiser.controller.spec.ts
@@ -1,11 +1,14 @@
 import { ShardTransaction } from "@elrondnetwork/transaction-processor";
 import { TestingModule, Test } from "@nestjs/testing";
+import { EventEmitter2 } from "@nestjs/event-emitter";
 import { WebSocketPublisherController } from "src/common/websockets/web-socket-publisher-controller";
 import { WebSocketPublisherService } from "src/common/websockets/web-socket-publisher-service";
+import { MetricsEvents } from "src/utils/metrics-events.constants";
 
 describe('WebSocketPublisherController', () => {
   let controller: WebSocketPublisherController;
   let webSocketPublisherService: WebSocketPublisherService;
+  let eventEmitter: EventEmitter2;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,23 +22,32 @@ describe('WebSocketPublisherController', () => {
             onBatchUpdated: jest.fn(),
           },
         },
+        {
+          provide: EventEmitter2,
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     controller = module.get<WebSocketPublisherController>(WebSocketPublisherController);
     webSocketPublisherService = module.get<WebSocketPublisherService>(WebSocketPublisherService);
+    eventEmitter = module.get<EventEmitter2>(EventEmitter2);
   });
 
   it('should handle transactionsCompleted event', async () => {
     const mockTransactions = [{}, {}] as ShardTransaction[];
     await controller.transactionsCompleted(mockTransactions);
     expect(webSocketPublisherService.onTransactionCompleted).toHaveBeenCalledTimes(mockTransactions.length);
+    expect(eventEmitter.emit).toHaveBeenCalledWith(MetricsEvents.SetTransactionsCompleted, { transactions: mockTransactions });
   });
 
   it('should handle transactionsPendingResults event', async () => {
     const mockTransactions = [{}, {}] as ShardTransaction[];
     await controller.transactionsPendingResults(mockTransactions);
     expect(webSocketPublisherService.onTransactionPendingResults).toHaveBeenCalledTimes(mockTransactions.length);
+    expect(eventEmitter.emit).toHaveBeenCalledWith(MetricsEvents.SetTransactionsPendingResults, { transactions: mockTransactions });
   });
 
   it('should handle onBatchUpdated event', () => {
@@ -43,5 +55,6 @@ describe('WebSocketPublisherController', () => {
     controller.onBatchUpdated(mockPayload);
     expect(webSocketPublisherService.onBatchUpdated).toHaveBeenCalledWith(mockPayload.address, mockPayload.batchId, mockPayload.txHashes);
     expect(webSocketPublisherService.onBatchUpdated).toHaveBeenCalledTimes(1);
+    expect(eventEmitter.emit).toHaveBeenCalledWith(MetricsEvents.SetBatchUpdated);
   });
 });
