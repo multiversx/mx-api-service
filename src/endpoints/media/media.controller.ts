27fcb4322463b18a450d457b9cf67fe6b744fb86
Gabriel Matei <gabrielvmatei@gmail.com>
2025-03-14 16:08:05 +0200

    stream files

--- src/endpoints/media/media.controller.ts
+++ src/endpoints/media/media.controller.ts
@@ -1,31 +1,61 @@
-import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
+import { Controller, Get, InternalServerErrorException, NotFoundException, Param, Req, Res } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
-import { Response } from "express";
+import { Request, Response } from "express";
 import { MediaService } from "./media.service";
+import { ApiService } from "@multiversx/sdk-nestjs-http";
+import { OriginLogger } from "@multiversx/sdk-nestjs-common";
 
 @Controller()
 @ApiTags('media')
 export class MediaController {
+  private readonly logger = new OriginLogger(MediaController.name);
+
   constructor(
     private readonly mediaService: MediaService,
+    private readonly apiService: ApiService,
   ) { }
 
   @Get("/media/:uri(*)")
   async redirectToMediaUri(
     @Param('uri') uri: string,
-    @Res() response: Response
+    @Req() req: Request,
+    @Res() res: Response
   ) {
     const redirectUrl = await this.mediaService.getRedirectUrl(uri);
     if (!redirectUrl) {
       throw new NotFoundException('Not found');
     }
 
-    response.statusMessage = 'Found';
-    response.setHeader('location', redirectUrl);
-    response.setHeader('cache-control', 'max-age=60');
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Content-Security-Policy', "default-src 'self'; img-src 'self' https://*.amazonaws.com; connect-src 'self' https://*.amazonaws.com; frame-src https://*.amazonaws.com; navigate-to https://*.amazonaws.com;");
+    try {
+      const response = await this.apiService.get(redirectUrl, {
+        // @ts-ignore
+        responseType: 'stream',
+        timeout: 60_000, // 60 seconds timeout
+      });
 
-    return response.redirect(301, redirectUrl);
+      res.setHeader('content-type', response.headers['content-type']);
+      res.setHeader('content-length', response.headers['content-length']);
+      res.setHeader('cache-control', 'max-age=60');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      response.data.pipe(res);
+
+      response.data.on('error', (error: any) => {
+        this.logger?.error(`Error streaming the resource: ${redirectUrl}`);
+        this.logger?.error(error);
+
+        throw new InternalServerErrorException('Failed to fetch URL');
+      });
+
+      req.on('close', () => {
+        response.data?.destroy();
+      });
+
+      return;
+    } catch (error) {
+      this.logger.error(`Failed to fetch URL: ${redirectUrl}`);
+
+      throw new InternalServerErrorException('Failed to fetch URL');
+    }
   }
 }
