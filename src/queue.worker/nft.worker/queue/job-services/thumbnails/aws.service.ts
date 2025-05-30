39f3b66b76989a5ac381638be3a7c7ebf26abb71
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2025-05-30 16:11:48 +0300

    extra logs

--- src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
+++ src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
@@ -1,9 +1,12 @@
 import { S3 } from "@aws-sdk/client-s3";
 import { Injectable } from "@nestjs/common";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
+import { OriginLogger } from '@multiversx/sdk-nestjs-common';
 
 @Injectable()
 export class AWSService {
+  private readonly logger = new OriginLogger(AWSService.name);
+
   constructor(
     private readonly apiConfigService: ApiConfigService,
   ) { }
@@ -17,13 +20,21 @@ export class AWSService {
       region: this.apiConfigService.getAwsS3Region(),
     });
 
-    await s3.putObject({
+    const awsResponse = await s3.putObject({
       Bucket: this.apiConfigService.getAwsS3Bucket(),
       Key: path,
       Body: buffer,
       ContentType: type,
       ACL: 'public-read',
     });
+
+    this.logger.log(`Uploaded ${path} to S3. Response: ${JSON.stringify(awsResponse)} (empty means no error)`);
+
+    const head = await s3.headObject({
+      Bucket: this.apiConfigService.getAwsS3Bucket(),
+      Key: path,
+    });
+    this.logger.log(`S3 ${path} HeadObject result: ${JSON.stringify(head)}`);
 
     return this.getItemPath(path);
   }
