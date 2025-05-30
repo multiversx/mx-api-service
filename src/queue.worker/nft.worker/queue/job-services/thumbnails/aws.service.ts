bd3fb46a32fbba66b037907e4382cbd7f80f0993
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2025-05-30 16:50:45 +0300

    fix aws service

--- src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
+++ src/queue.worker/nft.worker/queue/job-services/thumbnails/aws.service.ts
@@ -1,12 +1,9 @@
 import { S3 } from "@aws-sdk/client-s3";
 import { Injectable } from "@nestjs/common";
 import { ApiConfigService } from "src/common/api-config/api.config.service";
-import { OriginLogger } from '@multiversx/sdk-nestjs-common';
 
 @Injectable()
 export class AWSService {
-  private readonly logger = new OriginLogger(AWSService.name);
-
   constructor(
     private readonly apiConfigService: ApiConfigService,
   ) { }
@@ -18,23 +15,16 @@ export class AWSService {
         secretAccessKey: this.apiConfigService.getAwsS3Secret(),
       },
       region: this.apiConfigService.getAwsS3Region(),
+      endpoint: this.apiConfigService.getAwsS3Endpoint(),
     });
 
-    const awsResponse = await s3.putObject({
+    await s3.putObject({
       Bucket: this.apiConfigService.getAwsS3Bucket(),
       Key: path,
       Body: buffer,
       ContentType: type,
       ACL: 'public-read',
     });
-
-    this.logger.log(`Uploaded ${path} to S3. Response: ${JSON.stringify(awsResponse)} (empty means no error)`);
-
-    const head = await s3.headObject({
-      Bucket: this.apiConfigService.getAwsS3Bucket(),
-      Key: path,
-    });
-    this.logger.log(`S3 ${path} HeadObject result: ${JSON.stringify(head)}`);
 
     return this.getItemPath(path);
   }
