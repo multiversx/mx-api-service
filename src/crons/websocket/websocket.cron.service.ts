62974c754b96fe8ff53e09a49df1b8f1589accad
GuticaStefan <stefan.gutica@gmail.com>
2025-08-22 11:21:33 +0300

    add lock on crons

--- src/crons/websocket/websocket.cron.service.ts
+++ src/crons/websocket/websocket.cron.service.ts
@@ -3,7 +3,7 @@ import { Cron } from '@nestjs/schedule';
 import { TransactionsGateway } from '../../endpoints/transactions/transaction.gateway';
 import { BlocksGateway } from 'src/endpoints/blocks/blocks.gateway';
 import { NetworkGateway } from 'src/endpoints/network/network.gateway';
-
+import { Lock } from "@multiversx/sdk-nestjs-common";
 @Injectable()
 export class WebsocketCronService {
     constructor(
@@ -13,16 +13,19 @@ export class WebsocketCronService {
     ) { }
 
     @Cron('*/6 * * * * *')
+    @Lock({ name: 'Push transactions to subscribers', verbose: true })
     async handleTransactionsUpdate() {
         await this.transactionsGateway.pushTransactions();
     }
 
     @Cron('*/6 * * * * *')
+    @Lock({ name: 'Push blocks to subscribers', verbose: true })
     async handleBlocksUpdate() {
         await this.blocksGateway.pushBlocks();
     }
 
     @Cron('*/6 * * * * *')
+    @Lock({ name: 'Push stats to subscribers', verbose: true })
     async handleStatsUpdate() {
         await this.networkGateway.pushStats();
     }
