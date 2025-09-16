0e1b93dd31f2d3e239f23a7bfd64da2b2ab6141b
Catalin Faur <52102171+cfaur09@users.noreply.github.com>
2025-09-16 11:42:18 +0300

    performance improvements  (#1534)
    * improve getBlocks
    
    * add concurrency utils
    
    * update collection assets
    
    * batchApplyAssetsAndTicker update
    
    * update token service
    
    * fix eslint
    
    * improve elastisc transfers sorting
    
    * update spec
    
    * update mocks

--- src/endpoints/nfts/nft.service.ts
+++ src/endpoints/nfts/nft.service.ts
@@ -93,22 +93,20 @@ export class NftService {
       return;
     }
 
+    const allAssets = await this.assetsService.getAllTokenAssets();
     if (queryOptions?.withAssets === false) {
       return;
     }
 
-    await Promise.all(
-      nfts.map(async (nft) => {
-        nft.assets = await this.assetsService.getTokenAssets(nft.identifier) ??
-          await this.assetsService.getTokenAssets(nft.collection);
+    for (const nft of nfts) {
+      nft.assets = allAssets[nft.identifier] ?? allAssets[nft.collection];
 
-        if (nft.assets) {
-          nft.ticker = nft.collection.split('-')[0];
-        } else {
-          nft.ticker = nft.collection;
-        }
-      })
-    );
+      if (nft.assets) {
+        nft.ticker = nft.collection.split('-')[0];
+      } else {
+        nft.ticker = nft.collection;
+      }
+    }
   }
 
   private async conditionallyApplyOwners(nfts: Nft[], queryOptions?: NftQueryOptions): Promise<void> {
