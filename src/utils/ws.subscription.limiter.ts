6b8194038bae78c891e8650f4d2d83ac83d58348
GuticaStefan <stefan.gutica@gmail.com>
2025-12-08 13:48:20 +0200

    add global subscription limiter

--- src/utils/ws.subscription.limiter.ts
+++ src/utils/ws.subscription.limiter.ts
@@ -5,14 +5,19 @@ import { WsException } from '@nestjs/websockets';
 
 @Injectable()
 export class WsSubscriptionLimiterGuard implements CanActivate {
-  static MAX_ROOMS = 5 + 1; // TODO: adjust limit (5 subscriptions + 1 default room client id)
+  static MAX_ROOMS_PER_CLIENT = 5 + 1; // TODO: adjust limit (5 subscriptions + 1 default room client id)
+  static MAX_ROOMS_GLOBAL = 10_000; // TODO: adjust
 
   canActivate(context: ExecutionContext): boolean {
     const client: Socket = context.switchToWs().getClient();
+    const totalRoomsGlobal = client.nsp.server.sockets.adapter.rooms.size;
+    const totalClientRooms = client.rooms.size;
+    if (totalRoomsGlobal >= WsSubscriptionLimiterGuard.MAX_ROOMS_GLOBAL) {
+      throw new WsException(`Maximum number of ${WsSubscriptionLimiterGuard.MAX_ROOMS_GLOBAL} global subscriptions accepted by server reached!`);
+    }
 
-    //TODO: check for ip, not for each socket
-    if (client.rooms.size >= WsSubscriptionLimiterGuard.MAX_ROOMS) {
-      throw new WsException(`Maximum number of ${WsSubscriptionLimiterGuard.MAX_ROOMS} subscriptions reached!`);
+    if (totalClientRooms >= WsSubscriptionLimiterGuard.MAX_ROOMS_PER_CLIENT) {
+      throw new WsException(`Maximum number of ${WsSubscriptionLimiterGuard.MAX_ROOMS_PER_CLIENT} subscriptions per client reached!`);
     }
 
     return true;
