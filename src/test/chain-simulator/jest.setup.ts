732017e9f5ea812c8e112eb3b04ccf613e9a7712
GuticaStefan <stefan.gutica@gmail.com>
2026-05-29 10:25:25 +0300

    add log

--- src/test/chain-simulator/jest.setup.ts
+++ src/test/chain-simulator/jest.setup.ts
@@ -2,3 +2,4 @@ import axios from "axios";
 
 axios.defaults.adapter = 'fetch';
 axios.defaults.headers.common['Connection'] = 'close';
+console.log('Axios default adapter set to fetch and Connection header set to close');
