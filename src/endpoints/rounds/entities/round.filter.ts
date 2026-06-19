9db59539a3d8429d886b8c9415f0a40244a7a7c2
GuticaStefan <stefan.gutica@gmail.com>
2026-06-19 11:44:10 +0300

    add validation for rounds filter

--- src/endpoints/rounds/entities/round.filter.ts
+++ src/endpoints/rounds/entities/round.filter.ts
@@ -1,10 +1,14 @@
 import { QueryConditionOptions } from "@multiversx/sdk-nestjs-elastic";
+import { BadRequestException } from "@nestjs/common";
 import { QueryPagination } from "src/common/entities/query.pagination";
 
 export class RoundFilter extends QueryPagination {
   constructor(init?: Partial<RoundFilter>) {
     super();
     Object.assign(this, init);
+    if (this.searchAfter !== undefined && this.from !== 0) {
+      throw new BadRequestException("'from' must be 0 when 'searchAfter' is provided.");
+    }
   }
 
   condition: QueryConditionOptions | undefined = QueryConditionOptions.must;
